24c0ccadc7838a2510bbc099d1825b456020c6e9
Marcin <Cichon@marcins-mbp.home>
2014-10-18 14:01:54 +0100

    started working on A-Z; built some basic JS functionality for populating sub-categories and links

--- wp-content/themes/govintranetpress/js/app.js
+++ wp-content/themes/govintranetpress/js/app.js
@@ -1,5 +1,6 @@
 /** This file will be broken down to separate modules and then concatenated
  * to app.js during a build process (which we don't have at the moment).
+ * The individual modules are already built to work on their own.
  */
 
 jQuery(function(){
@@ -13,18 +14,20 @@ jQuery(function(){
     "use strict";
 
     App.MobileMenu = function(){
+      this.$top = $('#masthead');
+      if(!this.$top.length){ return; }
+
       this.config = {
         menuToggleClass: 'mobile-menu-enabled'
       };
 
       this.init();
-      this.cacheEls();
-      this.bindEvents();
     };
 
     App.MobileMenu.prototype = {
       init: function(){
-        this.$top = $('#masthead');
+        this.cacheEls();
+        this.bindEvents();
       },
 
       cacheEls: function(){
@@ -32,7 +35,6 @@ jQuery(function(){
       },
 
       bindEvents: function(){
-        var _this = this;
         this.$top.on('click', 'button', $.proxy(this.toggleMenu, this));
       },
 
@@ -42,20 +44,22 @@ jQuery(function(){
     };
   }(window.jQuery));
 
-  /** Mobile menu
+  /** Sticky news
    */
   (function($){
     "use strict";
 
     App.StickyNews = function(){
+      this.$top = $('#need-to-know');
+      if(!this.$top.length){ return; }
+
       this.init();
-      this.cacheEls();
-      this.bindEvents();
     };
 
     App.StickyNews.prototype = {
       init: function(){
-        this.$top = $('#need-to-know');
+        this.cacheEls();
+        this.bindEvents();
       },
 
       cacheEls: function(){},
@@ -73,8 +77,82 @@ jQuery(function(){
     };
   }(window.jQuery));
 
+  /** A-Z
+   */
+  (function($){
+    "use strict";
+
+    App.PageIndex = function(){
+      this.$top = $('.a-z');
+      if(!this.$top.length){ return; }
+      this.init();
+    };
+
+    App.PageIndex.prototype = {
+      init: function(){
+        this.config = {
+          serviceUrl: 'http://localhost/mojintranet/service/children' //hard-coding for now
+        };
+
+        this.topPageId = this.$top.data('page-id');
+        this.itemTemplate = this.$top.find('template[data-name="a-z-category-item"]').html();
+
+        this.cacheEls();
+        this.bindEvents();
+
+        this.populateChildren(this.topPageId, 1);
+      },
+
+      cacheEls: function(){
+        this.$categoriesContainer = this.$top.find('.categories ul');
+        this.$subcategoriesContainer = this.$top.find('.subcategories');
+        this.$linksContainer = this.$top.find('.links');
+
+        this.$categories = this.$categoriesContainer.find('li');
+      },
+
+      bindEvents: function(){
+      },
+
+      categoryClick: function(parentId, level, e){
+        e.preventDefault();
+        this.populateChildren(parentId, level);
+      },
+
+      populateChildren: function(parentId, level){
+        var _this = this;
+        var $container = this.$top.find('.level-'+level);
+        var $el;
+
+        $.getJSON(this.config.serviceUrl+'/'+parentId, function(data){
+          $container.empty();
+
+          $.each(data.items, function(index, item){
+            $el = $(_this.itemTemplate);
+            $el.attr('data-page-id', item.id);
+            $el.attr('data-order', item.order); //the API doesnt supply
+            $el.find('a').html(item.title);
+            if(level<3){
+              $el.on('click', 'a', $.proxy(_this.categoryClick, _this, item.id, level+1));
+              $el.find('.description').html(item.excerpt);
+            }
+            $container.append($el);
+          });
+        });
+      },
+
+      getId: function($el){
+        $el = $($el);
+        var id = $el.data('page-id');
+
+        return id;
+      }
+    };
+  }(window.jQuery));
+
   /** init section - this should be in a separate file - init.js
    */
   var mobileMenu = new App.MobileMenu();
   var stickyNews = new App.StickyNews();
+  var pageIndex = new App.PageIndex();
 });
