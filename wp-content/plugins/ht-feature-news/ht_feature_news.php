8512db7c6774b521c957e4af31e884c41b18f03a
Marcin <Cichon@marcins-mbp.home>
2014-10-29 13:23:28 +0000

    removed stickies; made sure that multiple instances of the widget don't display the same news

--- wp-content/plugins/ht-feature-news/ht_feature_news.php
+++ wp-content/plugins/ht-feature-news/ht_feature_news.php
@@ -9,11 +9,13 @@ Author URI: http://www.helpfultechnology.com
 */
 
 class htFeatureNews extends WP_Widget {
-    function htFeatureNews() {
-        parent::WP_Widget(false, 'HT Feature news', array('description' => 'Display feature news stories'));
-}
+  static $alreadydone = array();
 
-function widget($args, $instance) {
+  function htFeatureNews() {
+    parent::WP_Widget(false, 'HT Feature news', array('description' => 'Display feature news stories'));
+  }
+
+  function widget($args, $instance) {
     extract( $args );
     $title = apply_filters('widget_title', $instance['title']);
     $largeitems = intval($instance['largeitems']);
@@ -24,19 +26,14 @@ function widget($args, $instance) {
     global $post;
     echo $before_widget;
     if ( $title ) echo $before_title . $title . $after_title;
-	echo "<div id='ht-feature-news'>";
+    echo "<div id='ht-feature-news'>";
 
-	//load manual sticky news stories
-	$hc = new Pod ('homepage_control');
-	$top_slot =  $hc->get_field('top_news_story');
+    //forumalate grid of news stories and formats
+    $totalstories =  $largeitems + $mediumitems + $thumbnailitems + $listitems;
 
+    $newsgrid=array();
 
-	//forumalate grid of news stories and formats
-	$totalstories =  $largeitems + $mediumitems + $thumbnailitems + $listitems;
-
-	$newsgrid=array();
-
-	for ($i = 1; $i <= $totalstories; $i++) {
+    for ($i = 1; $i <= $totalstories; $i++) {
 			if ($i <= $largeitems) {
 				$newsgrid[] = "L";
 			}
@@ -51,126 +48,34 @@ function widget($args, $instance) {
 			}
 		}
 
-	$siteurl = site_url();
+    $siteurl = site_url();
 
-	//manual override news stories
-	//display sticky top news stories
-
-	$num_top_slots = count($top_slot);
-	$to_fill = $totalstories - $num_top_slots;
-	$k=-1;
-	$alreadydone= array();
-
-	if ( $num_top_slots > 0 ){
-			foreach ((array)$top_slot as $slot){
-				$newspod = new Pod ( 'news' , $slot['ID'] );
-				if ($newspod->get_field('post_status')!='publish') {
-					continue;
-				}
-
-				if ($newsgrid[$k]=="L"){
-          $container_class = 'news-large';
-				}
-				elseif ($newsgrid[$k]=="M"){
-          $container_class = 'news-medium';
-				}
-				elseif ($newsgrid[$k]=="T"){
-          $container_class = 'news-thumbnail';
-				}
-
-        echo'<div class="news-item '.$container_class.'">';
-
-				$k++;
-				$alreadydone[] = $slot['ID'];
-				if (function_exists('get_video_thumbnail')){
-					$videostill = get_video_thumbnail( $slot['ID'] );
-				}
-
-				$thistitle = govintranetpress_custom_title($slot['post_title']);
-				$thisURL=$slot['post_name'];
-
-				if ($newsgrid[$k]=="L"){
-					$image_uri =  wp_get_attachment_image_src( get_post_thumbnail_id( $slot['ID'] ), 'newshead' );
-					if ($image_uri!="" && $videostill==''){
-						echo "<a href='".$siteurl."/news/".$slot['post_name']."/'><img class='img img-responsive' src='{$image_uri[0]}' width='{$image_uri[1]}' height='{$image_uri[2]}' alt='".govintranetpress_custom_title($slot)."' /></a>";
-					}
-				}
-
-				if ($newsgrid[$k]=="M"){
-					$image_uri =  wp_get_attachment_image_src( get_post_thumbnail_id( $slot['ID'] ), 'medium' );
-					if ($image_uri!="" && $videostill==''){
-						echo "<a href='".$siteurl."/news/".$slot['post_name']."/'><img class='img img-responsive' src='{$image_uri[0]}' width='{$image_uri[1]}' height='{$image_uri[2]}' alt='".govintranetpress_custom_title($slot)."' /></a>";
-					}
-				}
-
-				if ($newsgrid[$k]=="T"){
-					$image_uri = "<a href='".$siteurl."/news/".$slot['post_name']."/'>".get_the_post_thumbnail($slot['ID'], 'thumbnail', array('class' => 'media-object hidden-xs'))."</a>";
-					if ($image_uri!="" && $videostill==''){
-						$image_url = "<a href='".$siteurl."/news/".$slot['post_name']."/'>".$image_uri."</a>";
-					}
-				}
-
-        $thisdate= $slot['post_date'];
-        $post = get_post( $slot['ID'] );
-        setup_postdata( $post );
-        $thisexcerpt= get_the_excerpt();
-        $thisdate=date("j M Y",strtotime($thisdate));
-
-        if ($newsgrid[$k]=="T"){
-          echo "<div class='media'>".$image_url;
-        }
-
-        if ($newsgrid[$k]=="Li"){
-          echo "<p><span class='news_date'>".$thisdate."";
-          echo " <a class='more' href='{$thisURL}' title='{$thistitle}'>Read more</a></span></p>";
-        } else {
-          echo "<p class='news-date-wrapper'><span class='news_date'>".$thisdate."</span></p>";
-        }
-
-        echo "<h3 class='noborder'><a class='' href='".$thisURL."'>".$thistitle."</a></h3>";
-
-        echo "<div class='media-body'>";
-
-        if ($newsgrid[$k]!="Li"){
-          echo $thisexcerpt."<p class='news_date'>";
-          echo "<a class='more' href='{$thisURL}' title='{$thistitle}'>Read more</a></p>";
-        }
-
-        echo "</div>";
-
-        if ($newsgrid[$k]=="T"){
-          echo "</div>";
-        }
-
-        echo "<hr class='light' />\n";
-
-        echo'</div>';
-			}
-		} //end of stickies
-
-	//display remaining stories
-	$cquery = array(
+    //display remaining stories
+    $cquery = array(
 			'orderby' => 'post_date',
-		    'order' => 'DESC',
-		    'post_type' => 'news',
-		    'posts_per_page' => $totalstories,
-		    'meta_query'=>array(array(
-			    'key'=>'news_listing_type',
-			    'value'=>0,
-				))
-			);
+      'order' => 'DESC',
+      'post_type' => 'news',
+      'posts_per_page' => $totalstories,
+      'meta_query'=>array(array(
+        'key'=>'news_listing_type',
+        'value'=>0,
+      ))
+    );
 
 		$news =new WP_Query($cquery);
 		if ($news->post_count==0){
 			echo "Nothing to show.";
 		}
 
+    $k=-1;
 		while ($news->have_posts()) {
-				$news->the_post();
-			if (in_array($post->ID, $alreadydone )) { //don't show if already in stickies
+			$news->the_post();
+			if (in_array($post->ID, htFeatureNews::$alreadydone )) { //don't show if already in stickies
 				continue;
 			}
 			$k++;
+      htFeatureNews::$alreadydone[] = $post->ID;
+
 			if ($k >= $totalstories){
 				break;
 			}
@@ -195,14 +100,14 @@ function widget($args, $instance) {
 			if ($newsgrid[$k]=="L"){
 				$image_uri =  wp_get_attachment_image_src( get_post_thumbnail_id( $news->ID ), 'newshead' );
 				if ($image_uri!="" && $videostill==''){
-					echo "<a href='{$thisURL}'><img class='img img-responsive' src='{$image_uri[0]}' width='{$image_uri[1]}' height='{$image_uri[2]}' alt='".govintranetpress_custom_title($slot)."' /></a>";
+					echo "<a href='{$thisURL}'><img class='img img-responsive' src='{$image_uri[0]}' alt='".govintranetpress_custom_title($slot)."' /></a>";
 				}
 			}
 
 			if ($newsgrid[$k]=="M"){
 				$image_uri =  wp_get_attachment_image_src( get_post_thumbnail_id( $news->ID ), 'medium' );
 				if ($image_uri!="" && $videostill==''){
-					echo "<a href='{$thisURL}'><img class='img' src='{$image_uri[0]}' width='{$image_uri[1]}' height='{$image_uri[2]}' alt='".govintranetpress_custom_title($slot)."' /></a>";
+					echo "<a href='{$thisURL}'><img class='img' src='{$image_uri[0]}' alt='".govintranetpress_custom_title($slot)."' /></a>";
 				}
 			}
 
