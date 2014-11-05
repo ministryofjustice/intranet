8382352c39dac7c80cbde4c7e6995d27ba424159
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2014-11-05 13:42:40 +0000

    reorganised the scss files a bit; styled the filters on the news landing page

--- wp-content/themes/govintranetpress/mvc/views/pages/news.php
+++ wp-content/themes/govintranetpress/mvc/views/pages/news.php
@@ -1,12 +1,39 @@
 <?php if (!defined('ABSPATH')) die(); ?>
 
-<div class="page-news">
-  <div class="row">
-    <div class="col-md-4">
+<?php if ( have_posts() ) while ( have_posts() ) : the_post() ?>
 
+<div class="page-news">
+  <h1 class="page-title"><?php the_title() ?></h1>
+	<?php the_content() ?>
+
+  <div class="row">
+
+    <div class="col-md-4">
+      <form class="news-filters">
+        <div class="form-row">
+          <span class="label">Filter by:</span>
+        </div>
+        <div class="form-row">
+          <select name="category">
+            <option>All</option>
+            <option>Category 1</option>
+            <option>Category 2</option>
+          </select>
+        </div>
+        <div class="form-row">
+          <input type="text" placeholder="Keywords" name="keywords" />
+        </div>
+        <div class="form-row">
+          <input type="submit" value="Search" />
+        </div>
+      </form>
     </div>
+
     <div class="col-md-8">
       <?php dynamic_sidebar('newslanding-widget-area'); ?>
     </div>
+
   </div>
 </div>
+
+<?php endwhile ?>
