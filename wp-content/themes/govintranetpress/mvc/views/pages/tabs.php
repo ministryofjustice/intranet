663623a673da3bd5f16af4595f5e96c54417c2b6
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2014-11-06 11:31:18 +0000

    removed reduntant view file

--- wp-content/themes/govintranetpress/mvc/views/pages/tabs.php
+++
@@ -1,8 +0,0 @@
-<ul class="news-tabs">
-  <li class="<?=$selected=='news' ? 'selected' : ''?>">
-    <a href="#">Corporate updates</a>
-  </li>
-  <li class="<?=$selected=='events' ? 'selected' : ''?>">
-    <a href="#">Calendar</a>
-  </li>
-</ul>
