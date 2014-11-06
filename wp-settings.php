9055268571c2e139a43fb46383431a2dde1cf35e
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2014-11-06 13:58:16 +0000

    moved the mvc framework to a more sensible location

--- wp-settings.php
+++ wp-settings.php
@@ -154,7 +154,7 @@ require( ABSPATH . WPINC . '/nav-menu.php' );
 require( ABSPATH . WPINC . '/nav-menu-template.php' );
 require( ABSPATH . WPINC . '/admin-bar.php' );
 
-require( ABSPATH . '/vendor/init.php' );
+require( ABSPATH . '/custom/mvc/init.php' );
 
 // Load multisite-specific files.
 if ( is_multisite() ) {
