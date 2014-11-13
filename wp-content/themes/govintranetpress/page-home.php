94d51e99c12f071795b9cbd502899acb1814ec09
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2014-11-13 16:48:24 +0000

    fixed size of feeds on mobile bp

--- wp-content/themes/govintranetpress/page-home.php
+++ wp-content/themes/govintranetpress/page-home.php
@@ -101,16 +101,16 @@ get_header(); ?>
 
       <div class="col-lg-6 col-md-6 col-sm-12">
         <div class="grid feeds">
-          <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="col-lg-6 col-md-6 col-sm-6">
             <img src="<?=get_stylesheet_directory_uri()?>/images/feed_events.png" />
           </div>
-          <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="col-lg-6 col-md-6 col-sm-6">
             <img src="<?=get_stylesheet_directory_uri()?>/images/feed_stats.png" />
           </div>
-          <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="col-lg-6 col-md-6 col-sm-6">
             <img src="<?=get_stylesheet_directory_uri()?>/images/feed_twitter.png" />
           </div>
-          <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="col-lg-6 col-md-6 col-sm-6">
             <img src="<?=get_stylesheet_directory_uri()?>/images/feed_yammer.png" />
           </div>
         </div>
