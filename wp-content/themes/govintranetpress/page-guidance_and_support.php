619d206caffcd60defcaf7f6b3a6e4b22569b8fb
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-01-09 13:21:13 +0000

    created a guidance and support content page template

---
+++ wp-content/themes/govintranetpress/page-guidance_and_support.php
@@ -0,0 +1,22 @@
+<?php if (!defined('ABSPATH')) die();
+
+/**
+ * The Template for guidance and support pages
+ *
+ * Template name: Guidance & Support
+ */
+class Page_guidance_and_support extends MVC_controller {
+  function main(){
+    while(have_posts()){
+      get_header();
+      get_footer();
+    }
+  }
+
+  function get_data(){
+    return array(
+    );
+  }
+}
+
+new Page_guidance_and_support();
