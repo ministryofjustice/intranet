738223be4f61b043d79adf2afde21cd287311efb
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-01-13 09:47:43 +0000

    (WIP) prepared the code for the redirect script

---
+++ wp-content/themes/govintranetpress/mvc/views/pages/guidance_and_support_content/main.php
@@ -0,0 +1,4 @@
+<?php if (!defined('ABSPATH')) die(); ?>
+
+<div class="guidance-and-support-content" data-redirect-url="<?=$redirect_url?>" data-redirect-enabled="<?=$redirect_enabled?>">
+</div>
--- wp-content/themes/govintranetpress/page-guidance-and-support.php
+++ wp-content/themes/govintranetpress/page-guidance-and-support.php
@@ -8,13 +8,17 @@
 class Page_guidance_and_support extends MVC_controller {
   function main(){
     while(have_posts()){
+      the_post();
       get_header();
+      $this->view('pages/guidance_and_support_content/main', $this->get_data());
       get_footer();
     }
   }
 
   function get_data(){
     return array(
+      'redirect_url' => get_post_meta(get_the_ID(), 'redirect_url', true),
+      'redirect_enabled' => get_post_meta(get_the_ID(), 'redirect_enabled', true)
     );
   }
 }
