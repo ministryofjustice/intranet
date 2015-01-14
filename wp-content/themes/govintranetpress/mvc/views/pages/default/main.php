211d56226eb5d72ef43162b165c5426260de9a14
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-01-14 11:10:04 +0000

    cleaned up and converted to MVC the default template

---
+++ wp-content/themes/govintranetpress/mvc/views/pages/default/main.php
@@ -0,0 +1,12 @@
+<?php if (!defined('ABSPATH')) die(); ?>
+
+<div class="page-default">
+  <div class="grid">
+    <div class="col-lg-12">
+      <h1><?=$title?></h1>
+    </div>
+    <div class="col-lg-12">
+      <?=$content?>
+    </div>
+  </div>
+</div>
