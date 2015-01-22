ff67d4e09b188c7241b27e9d0a7e4d7351210891
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-01-22 15:19:47 +0000

    removed hammer.js for all IEs

--- wp-content/themes/govintranetpress/footer.php
+++ wp-content/themes/govintranetpress/footer.php
@@ -38,7 +38,7 @@
 <?php
 	wp_footer();
 ?>
-<!--[if (gte IE 8)|!(IE)]><!-->
+<!--[if !(IE)]><!-->
   <script type="text/javascript" src="<?=get_stylesheet_directory_uri()?>/js/hammer.min.js"></script>
   <script type="text/javascript" src="<?=get_stylesheet_directory_uri()?>/js/jquery.hammer.js"></script>
 <!--<![endif]-->
