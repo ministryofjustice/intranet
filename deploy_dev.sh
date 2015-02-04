93594a4ec89c7cbc48aba85a75d01ccaca8fd116
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-02-04 15:05:52 +0000

    updated rsync flags in deploy script

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -57,11 +57,11 @@ fi
 source $config_file
 
 # deploy all files
-rsync -rlptu --delete --progress --exclude='.htaccess' --exclude=deploy_dev.sh --exclude=deploy_dev.cfg --exclude=wp-config.php -e ssh ./ $user@$app-mojintranet.rhcloud.com:/var/lib/openshift/$user/app-root/runtime/repo/
+rsync -rlpt --delete --progress --exclude='.htaccess' --exclude=deploy_dev.sh --exclude=deploy_dev.cfg --exclude=wp-config.php -e ssh ./ $user@$app-mojintranet.rhcloud.com:/var/lib/openshift/$user/app-root/runtime/repo/
 
 # create and deploy mysql dump
 mysqldump -u $db_user --password=$db_pass $db_db >> deploy_dump.sql
-rsync -zu --progress -e ssh ./deploy_dump.sql $user@$app-mojintranet.rhcloud.com:/var/lib/openshift/$user/app-root/runtime/
+rsync -z --progress -e ssh ./deploy_dump.sql $user@$app-mojintranet.rhcloud.com:/var/lib/openshift/$user/app-root/runtime/
 
 # import the mysql dump on the server
 ssh $user@$app-mojintranet.rhcloud.com <<ENDSSH
