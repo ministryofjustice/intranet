e0f770bc5aa6e26bf9fc2680ec2be4b993fc95a1
Ryan Jarrett <ryan.jarrett@digital.justice.gov.uk>
2015-02-16 15:54:42 +0000

    Removed dashboard widgets

--- wp-content/themes/govintranetpress/functions.php
+++ wp-content/themes/govintranetpress/functions.php
@@ -1664,3 +1664,13 @@ function setup_js_wp_editor() {
 	}
 }
 add_action( 'init', 'setup_js_wp_editor',100);
+
+// Hide dashboard widgets
+function remove_dashboard_widgets() {
+	remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+	remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );
+	remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+	remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+	remove_action( 'welcome_panel', 'wp_welcome_panel' );
+}
+add_action('wp_dashboard_setup', 'remove_dashboard_widgets' );
