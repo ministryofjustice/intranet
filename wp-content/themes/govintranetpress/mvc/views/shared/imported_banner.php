de5760220f6fc93d713635dad632ee82f703310c
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-02-21 14:06:32 +0000

    created beta banner and 'imported' banner

---
+++ wp-content/themes/govintranetpress/mvc/views/shared/imported_banner.php
@@ -0,0 +1,13 @@
+<?php if (!defined('ABSPATH')) die(); ?>
+
+<div class="grid">
+  <div class="col-lg-12 col-md-12 col-sm-12">
+    <div class="imported-banner">
+      <p class="message">
+        The MOJ intranet is changing for the better. The information on this page is correct, but we'll soon be improving
+        the way it's presented. You can contact us to <a href="mailto:info@justice.gsi.gov.uk">info@justice.gsi.gov.uk</a>
+        if you have a query.
+      </p>
+    </div>
+  </div>
+</div>
