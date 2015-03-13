ed23faba5bd11f7cb725db448d421dce7dfe3fb8
Ryan Jarrett <ryan.jarrett@digital.justice.gov.uk>
2015-03-13 11:18:25 +0000

    Logs in user to subscriber account fullsite when visiting fullsite page

--- wp-content/themes/govintranetpress/functions.php
+++ wp-content/themes/govintranetpress/functions.php
@@ -1704,3 +1704,34 @@ function add_full_site_param($public_query_vars) {
   return $public_query_vars;
 }
 add_filter('query_vars','add_full_site_param');
+
+function auto_login() {
+	//change these 2 items
+	$loginpageid = 'fullsite'; //Page ID/slug of your login page
+	$loginusername = 'fullsite'; //username of the WordPress user account to impersonate
+
+    if (!is_user_logged_in()
+    	&& is_page($loginpageid)) { //only attempt to auto-login if at www.site.com/auto-login/ (i.e. www.site.com/?p=1234 )
+
+        //get user's ID
+        $user = get_user_by('login', $loginusername);
+        $user_id = $user->ID;
+
+        //login
+        wp_set_current_user($user_id, $loginusername);
+        wp_set_auth_cookie($user_id);
+        do_action('wp_login', $loginusername);
+
+        // Set fullsite session var
+        $_SESSION['full_site'] = true;
+
+        //redirect to home page after logging in (i.e. don't show content of www.site.com/?p=1234 )
+        wp_redirect( home_url() );
+        exit;
+    } elseif(is_page($loginpageid)) {
+        //prevent viewing of login page even if logged in
+        wp_redirect( home_url() );
+        exit;
+    } else {}
+}
+add_action('wp', 'auto_login');
