d2654d5a77fec89daee1fea1dc09706d48162e4f
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-03-19 14:26:50 +0000

    fixed the missing footer

--- wp-content/themes/govintranetpress/footer.php
+++ wp-content/themes/govintranetpress/footer.php
@@ -17,7 +17,7 @@
   <div class="grid">
     <div class="col-lg-12 col-md-12 col-sm-12">
       <div class="footer">
-        <?php if(is_active_sidebar('first-footer-widget-area')&&$is_moj_story==true): ?>
+        <?php if(is_active_sidebar('first-footer-widget-area') && $_SESSION['full_site']): ?>
           <?php dynamic_sidebar('first-footer-widget-area'); ?>
         <?php endif ?>
       </div>
