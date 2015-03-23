56aba2c0d19b61dff71ef121095f25d7c84c9d12
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-03-23 17:44:04 +0000

    made keyboard navigation on lemon tree more intuitive

--- wp-content/themes/govintranetpress/js/app.js
+++ wp-content/themes/govintranetpress/js/app.js
@@ -923,6 +923,8 @@
           paddingTop: $overviewPageLink.outerHeight() + 'px'
         });
       }
+
+      $thisLevelContainer.find('.item').first().find('a').focus();
     },
 
     buildOverviewPageLink: function(level, $selectedItem) {
