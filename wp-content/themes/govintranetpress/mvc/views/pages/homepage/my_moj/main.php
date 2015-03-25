c011a450bd763eda29e8286811f83c12d1d3f88d
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-03-25 15:36:37 +0000

    added titles for search form and department dropdown

--- wp-content/themes/govintranetpress/mvc/views/pages/homepage/my_moj/main.php
+++ wp-content/themes/govintranetpress/mvc/views/pages/homepage/my_moj/main.php
@@ -3,7 +3,7 @@
 <div class="my-moj">
   <div class="my-moj-top">
     <div data-department="select-department" class="department-dropdown-box">
-      <select class="department">
+      <select class="department" title="Select your intranet">
         <option data-department="select-department" value="">Choose your intranet</option>
         <?php foreach($departments as $department): ?>
           <option data-url="<?=$department['url']?>" data-department="<?=$department['name']?>"><?=$department['label']?></option>
--- wp-content/themes/govintranetpress/searchform.php
+++ wp-content/themes/govintranetpress/searchform.php
@@ -1,4 +1,4 @@
 <form role="search" class="search-form" name="search-form" action="<?=site_url()?>/search-results/" method="post">
-  <input class="keywords-field" type="text" placeholder="Search" name="s" id="s" value="<?=urldecode(get_query_var('search-string'))?>">
-  <input class="search-btn cta" type="submit" value="Search" />
+  <input class="keywords-field" type="text" placeholder="Search" name="s" id="s" value="<?=urldecode(get_query_var('search-string'))?>" title="Search">
+  <input class="search-btn cta" id="search-btn" type="submit" value="Search" />
 </form>
