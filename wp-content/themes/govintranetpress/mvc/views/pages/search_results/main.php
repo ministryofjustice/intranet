4981a2fa314f2f64ae7c24383ea23f58e3d9a451
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-03-31 12:38:41 +0100

    added aria labels for pagination

--- wp-content/themes/govintranetpress/mvc/views/pages/search_results/main.php
+++ wp-content/themes/govintranetpress/mvc/views/pages/search_results/main.php
@@ -48,8 +48,8 @@
 
       <ul class="content-nav grid">
         <li class="previous disabled col-lg-6 col-md-6 col-sm-6">
-          <a href="#">
-            <span class="nav-label">Previous page</span>
+          <a href="#" aria-labelledby="prev-page-label">
+            <span class="nav-label" id="prev-page-label">Previous page</span>
             <span class="page-info">
               <span class="prev-page"></span>
               of
@@ -60,8 +60,8 @@
         </li>
 
         <li class="next disabled col-lg-6 col-md-6 col-sm-6">
-          <a href="#">
-            <span class="nav-label">Next page</span>
+          <a href="#" aria-labelledby="next-page-label">
+            <span class="nav-label" id="next-page-label">Next page</span>
             <span class="page-info">
               <span class="next-page"></span>
               of
