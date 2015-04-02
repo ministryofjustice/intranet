b8d604ee4ba1872ad242e2d453cb9c9671fe6e1b
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-04-02 17:14:16 +0100

    finished the new navigation

--- wp-content/themes/govintranetpress/js/src/init.js
+++ wp-content/themes/govintranetpress/js/src/init.js
@@ -1,4 +1,4 @@
-jQuery(function($) {
+jQuery(function() {
   "use strict";
 
   var App = window.App;
@@ -19,4 +19,5 @@ jQuery(function($) {
   App.ins.departmentDropdown = new App.DepartmentDropdown();
   App.ins.feeds = new App.Feeds();
   App.ins.skipToContent = new App.SkipToContent();
+  App.ins.navigation = new App.Navigation();
 });
