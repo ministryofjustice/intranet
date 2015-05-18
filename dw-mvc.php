60cc68e46c9cebef22f00e7e971a8c2ea92fecd3
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-05-18 12:47:35 +0100

    made sure the template class is instantiated only if it exists

--- dw-mvc.php
+++ dw-mvc.php
@@ -54,8 +54,11 @@ function mmvc_init($template) {
     $controller_name = ucfirst(basename($template));
     $controller_name = preg_replace('/\.[^.]+$/', '', $controller_name);
     $controller_name = str_replace('-', '_', $controller_name);
-    $obj  = new $controller_name();
-    $obj->main();
+
+    if(class_exists($controller_name)) {
+      $obj  = new $controller_name();
+      $obj->main();
+    }
   }
 }
 
