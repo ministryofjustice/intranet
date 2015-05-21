17eb9a2ba99fc7619d579da633407c5301b7af56
Ryan Jarrett <ryan.jarrett@digital.justice.gov.uk>
2015-05-21 15:17:55 +0100

    Moved anonymizeIp line

--- wp-content/themes/govintranetpress/mvc/views/modules/head.php
+++ wp-content/themes/govintranetpress/mvc/views/modules/head.php
@@ -33,9 +33,9 @@
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+    ga('set', 'anonymizeIp', true);
     ga('create', '%s', 'auto');
     ga('send', 'pageview');
-    ga('set', 'anonymizeIp', true);
 
   </script>";
   $gis = "general_intranet_track_homepage";
