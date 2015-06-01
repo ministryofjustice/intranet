f761fb7ac45d058ae658e10ab3642d1ff8426e4b
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-06-01 17:40:28 +0100

    deleted redundant frontend files from the theme

--- wp-content/themes/govintranetpress/mvc/views/modules/body_bottom.php
+++ wp-content/themes/govintranetpress/mvc/views/modules/body_bottom.php
@@ -8,14 +8,6 @@
   <script type="text/javascript" src="<?=get_template_directory_uri()?>/js/respond.min.js"></script>
 <![endif]-->
 
-<?php
-/* Hammer is not needed anymore but might still come in handy soon when we optimise the site for mobile
-<!--[if !(IE)]><!-->
-  <script type="text/javascript" src="<?=get_template_directory_uri()?>/js/hammer.min.js"></script>
-  <script type="text/javascript" src="<?=get_template_directory_uri()?>/js/jquery.hammer.js"></script>
-<!--<![endif]-->
-*/
-?>
 <script type="text/javascript" src="<?=get_template_directory_uri()?>/js/app.js?<?=add_checksum_param('js/app.js')?>"></script>
 
 <?php wp_footer(); ?>
