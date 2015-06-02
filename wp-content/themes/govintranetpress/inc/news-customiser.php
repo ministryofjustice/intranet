e8aea7e70c5cae33a9dc81daa414e97f123bc561
Ryan Jarrett <ryan.jarrett@digital.justice.gov.uk>
2015-06-02 12:02:44 +0100

    Moved customizer.js

--- wp-content/themes/govintranetpress/inc/news-customiser.php
+++ wp-content/themes/govintranetpress/inc/news-customiser.php
@@ -24,7 +24,7 @@
 
       public function load_autocomplete() {
         wp_enqueue_script('jquery-ui-autocomplete');
-        wp_enqueue_script('customizer',get_template_directory_uri()."/js/customizer.js",array('jquery-ui-datepicker'),null,true);
+        wp_enqueue_script('customizer',get_template_directory_uri()."/admin/js/customizer.js",array('jquery-ui-datepicker'),null,true);
         wp_enqueue_style('jquery-admin-ui-css', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/themes/overcast/jquery-ui.css', false, 0.1, false);
       }
 
