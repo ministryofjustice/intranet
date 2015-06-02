efda968f50fa9317dafd0822857ae9de7e1b7c27
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-06-02 15:25:10 +0100

    improved the structure of frontend-related dirs

--- wp-content/themes/govintranetpress/views/modules/head.php
+++ wp-content/themes/govintranetpress/views/modules/head.php
@@ -5,15 +5,15 @@
 <meta charset="<?php bloginfo('charset'); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<link rel="stylesheet" type="text/css" media="all" href="<?=get_template_directory_uri()?>/css/style.css?<?=add_checksum_param('css/style.css')?>" />
-<link rel="stylesheet" type="text/css" media="print" href="<?=get_template_directory_uri()?>/css/print.css" />
+<link rel="stylesheet" type="text/css" media="all" href="<?=get_template_directory_uri()?>/assets/css/style.css?<?=add_checksum_param('css/style.css')?>" />
+<link rel="stylesheet" type="text/css" media="print" href="<?=get_template_directory_uri()?>/assets/css/print.css" />
 
 <!--[if !IE]><!-->
-  <link rel="stylesheet" type="text/css" media="all" href="<?=get_template_directory_uri()?>/css/fonts.css" />
+  <link rel="stylesheet" type="text/css" media="all" href="<?=get_template_directory_uri()?>/assets/css/fonts.css" />
 <!--<![endif]-->
 
 <!--[if lte IE 9]>
-  <link rel="stylesheet" type="text/css" media="screen" href="<?=get_template_directory_uri()?>/css/ie.css?<?=add_checksum_param('css/ie.css')?>" />
+  <link rel="stylesheet" type="text/css" media="screen" href="<?=get_template_directory_uri()?>/assets/css/ie.css?<?=add_checksum_param('css/ie.css')?>" />
 <![endif]-->
 
 <!--[if lt IE 9]>
