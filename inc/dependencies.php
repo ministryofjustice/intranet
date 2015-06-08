e52d273f5e98cfd4480e75bbf8be91da94fbe2ee
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-06-08 16:10:24 +0100

    removed loading useless dependencies

--- inc/dependencies.php
+++ inc/dependencies.php
@@ -5,15 +5,8 @@ function enqueueThemeScripts() {
   global $wp_scripts;
 
   wp_enqueue_script( 'jquery' );
-  wp_enqueue_script( 'jquery-ui' );
-
-  wp_register_script( 'ht-scripts', get_stylesheet_directory_uri() . "/js/ht-scripts.js");
-  wp_enqueue_script( 'ht-scripts' );
-
-  wp_register_script( 'jquery.ht-timediff', get_stylesheet_directory_uri() . "/js/jquery.ht-timediff.js");
-  wp_enqueue_script( 'jquery.ht-timediff',90 );
-
 }
+
 add_action('wp_enqueue_scripts','enqueueThemeScripts');
 
 // Adds custom styling to the login page
