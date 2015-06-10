a99851e77102464795829352333bec38510a76bf
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-06-10 13:45:38 +0100

    made the pagination scroll to top of results on page change

--- views/pages/search_results/main.php
+++ views/pages/search_results/main.php
@@ -48,7 +48,7 @@
 
       <ul class="content-nav grid">
         <li class="previous disabled col-lg-6 col-md-6 col-sm-6">
-          <a href="#" aria-labelledby="prev-page-label">
+          <a href="#content" aria-labelledby="prev-page-label">
             <span class="nav-label" id="prev-page-label">Previous page</span>
             <span class="page-info">
               <span class="prev-page"></span>
@@ -60,7 +60,7 @@
         </li>
 
         <li class="next disabled col-lg-6 col-md-6 col-sm-6">
-          <a href="#" aria-labelledby="next-page-label">
+          <a href="#content" aria-labelledby="next-page-label">
             <span class="nav-label" id="next-page-label">Next page</span>
             <span class="page-info">
               <span class="next-page"></span>
