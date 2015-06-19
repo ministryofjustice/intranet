fe8cca147edebaf0606acd8adcf7a50284f30064
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-06-19 19:48:41 +0100

    added GA tracking for search refinements

--- assets/js/src/modules/search_results.js
+++ assets/js/src/modules/search_results.js
@@ -13,23 +13,28 @@
 
   App.SearchResults.prototype = {
     init: function() {
+      this.settings = {
+        updateGATimeout: 2000, //timeout for google analytics for search refinements
+        minKeywordLength: 2, //not implemented yet
+        months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+      };
+
       this.applicationUrl = $('head').data('application-url');
       this.serviceUrl = this.applicationUrl+'/service/search';
       this.pageBase = this.applicationUrl+'/'+this.$top.data('top-level-slug');
-
       this.itemTemplate = this.$top.find('.template-partial[data-name="search-item"]').html();
       this.resultsPageTitleTemplate = this.$top.find('.template-partial[data-name="search-results-page-title"]').html();
       this.filteredResultsTitleTemplate = this.$top.find('.template-partial[data-name="search-filtered-results-title"]').html();
       this.serviceXHR = null;
-      this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      this.updateGATimeoutHandle = null;
       this.currentPage = null;
-      this.minKeywordLength = 2;
+      this.finishedInitialLoad = false;
+      this.lastSearchUrl = "";
 
       this.cacheEls();
       this.bindEvents();
 
       this.$keywordsInput.focus();
-
       this.setFilters();
 
       this.loadResults();
@@ -151,8 +156,6 @@
         dataArray.push(value);
       });
 
-
-
       /* use the timeout for dev/debugging purposes */
       //**/window.setTimeout(function() {
         _this.serviceXHR = $.getJSON(_this.serviceUrl+'/'+dataArray.join('/'), $.proxy(_this.displayResults, _this));
@@ -166,6 +169,7 @@
     displayResults: function(data) {
       var _this = this;
       var $searchItem;
+      var newUrl;
 
       this.clearResults();
       this.setResultsHeading(data);
@@ -178,6 +182,25 @@
       this.updatePagination(data);
       this.updateUrl();
       this.stopLoadingResults();
+
+      newUrl = this.getNewUrl(true); //must be set after updateUrl
+
+      if(!this.finishedInitialLoad) {
+        this.finishedInitialLoad = true;
+        this.lastSearchUrl = newUrl;
+      }
+
+      if(this.lastSearchUrl !== newUrl) {
+        if(this.updateGATimeoutHandle) {
+          window.clearTimeout(this.updateGATimeoutHandle);
+        }
+
+        this.updateGATimeoutHandle = window.setTimeout($.proxy(this.updateGA, this), this.settings.updateGATimeout);
+      }
+      else {
+        window.clearTimeout(this.updateGATimeoutHandle);
+        this.updateGATimeoutHandle = null;
+      }
     },
 
     setResultsHeading: function(data) {
@@ -204,7 +227,6 @@
       var keywordsArray = keywords.split(' ');
 
       if(!keywords.length){ return false; }
-
 
       return true;
     },
@@ -278,7 +300,7 @@
     },
 
     formatDate: function(dateObject) {
-      return dateObject.getDate()+' '+this.months[dateObject.getMonth()]+' '+dateObject.getFullYear();
+      return dateObject.getDate()+' '+this.settings.months[dateObject.getMonth()]+' '+dateObject.getFullYear();
     },
 
     updatePagination: function(data) {
@@ -313,6 +335,23 @@
     /** Updates the url based on user selections
      */
     updateUrl: function() {
+      if(history.pushState) {
+        history.pushState({}, "", this.getNewUrl());
+      }
+    },
+
+    updateGA: function() {
+      this.updateGATimeoutHandle = null;
+      this.lastSearchUrl = this.getNewUrl(true);
+
+      window.ga('send', 'pageview', this.getNewUrl(true));
+    },
+
+    /** Creates and returns as a string a new urls based on current filters
+     * @param {Boolean} rootRelative Will only return a root-relative url (omitting the domain)
+     * @returns {String} The new url
+     */
+    getNewUrl: function(rootRelative) {
       var urlParts = [this.pageBase];
       var keywords = this.getSanitizedKeywords();
 
@@ -327,9 +366,13 @@
       //page number
       urlParts.push(this.currentPage);
 
-      if(history.pushState) {
-        history.pushState({}, "", urlParts.join('/')+'/');
+      if(rootRelative) {
+        urlParts.shift();
+        urlParts.unshift(this.$top.data('top-level-slug'));
+        urlParts.unshift(''); //will have a leading slash on the final string (from join)
       }
+
+      return urlParts.join('/')+'/';
     }
   };
 }(jQuery));
