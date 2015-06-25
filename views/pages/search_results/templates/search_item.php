251c04d0c725777940df475e7786a28e60439331
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-06-25 18:52:50 +0100

    cleanup: moved template partials to separate files

---
+++ views/pages/search_results/templates/search_item.php
@@ -0,0 +1,25 @@
+<?php if (!defined('ABSPATH')) die(); ?>
+
+<div class="template-partial" data-name="search-item">
+  <li class="search-item">
+    <h3 class="title">
+      <a href="" class="search-link"></a>
+    </h3>
+    <div class="meta grid">
+      <div class="col-lg-3">
+        <span class="date"></span>
+      </div>
+      <div class="col-lg-3">
+        <span class="type">Guidance</span>
+      </div>
+    </div>
+    <p class="excerpt"></p>
+    <p class="file">
+      Download
+      <a class="file-link" href=""></a>,
+      <span class="file-size"></span>,
+      <span class="file-length"></span>
+      Pages
+    </p>
+  </li>
+</div>
---
+++ views/pages/search_results/templates/search_results_page_title.php
@@ -0,0 +1,5 @@
+<?php if (!defined('ABSPATH')) die(); ?>
+
+<div class="template-partial" data-name="search-results-page-title">
+  <h3 class="search-results-page-title search-results-title">Latest</h3>
+</div>
