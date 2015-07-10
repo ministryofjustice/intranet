d7c9b9dac514d30a8cea1b15e9fb429717f49697
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-07-10 12:03:18 +0100

    updated the IE6 message and decreased the font size

--- wp-content/themes/mojintranet/views/modules/ie6_message.php
+++ wp-content/themes/mojintranet/views/modules/ie6_message.php
@@ -3,7 +3,7 @@
 <div class="grid">
   <div class="col-lg-12 col-md-12 col-sm-12">
     <p class="ie6-message">
-      The site is not optimised for IE6. Please use firefox.
+      Internet Explorer 6 is not supported. Please use a different browser.
     </p>
   </div>
 </div>
