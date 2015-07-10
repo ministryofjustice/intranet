c211f7d169b2e8963f5be70657f93d0c868e8dc9
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-07-10 09:24:01 +0100

    added the IE6 banner

---
+++ wp-content/themes/mojintranet/views/modules/ie6_message.php
@@ -0,0 +1,9 @@
+<?php if (!defined('ABSPATH')) die(); ?>
+
+<div class="grid">
+  <div class="col-lg-12 col-md-12 col-sm-12">
+    <p class="ie6-message">
+      The site is not optimised for IE6. Please use firefox.
+    </p>
+  </div>
+</div>
