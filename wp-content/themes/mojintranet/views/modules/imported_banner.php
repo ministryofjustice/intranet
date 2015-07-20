0b690c3500276aa26fa531fa2dcf85560fc6d3ef
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-07-20 17:21:09 +0100

    Updated the site to Beta

--- wp-content/themes/mojintranet/views/modules/imported_banner.php
+++ wp-content/themes/mojintranet/views/modules/imported_banner.php
@@ -4,7 +4,7 @@
   <div class="col-lg-12 col-md-12 col-sm-12">
     <div class="imported-banner">
       <p class="message">
-        This intranet is an Alpha version and is still in test mode. While it's in development the information may not be up to date so please don't rely on it at present.
+        This intranet is an Beta version and is still in test mode. While it's in development the information may not be up to date so please don't rely on it at present.
       </p>
     </div>
   </div>
