600afa4c4c03301588f3cc804f9c0e2521465648
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-07-21 13:43:08 +0100

    updated the imported content banner message

--- wp-content/themes/mojintranet/views/modules/imported_banner.php
+++ wp-content/themes/mojintranet/views/modules/imported_banner.php
@@ -4,7 +4,7 @@
   <div class="col-lg-12 col-md-12 col-sm-12">
     <div class="imported-banner">
       <p class="message">
-        This intranet is an Beta version and is still in test mode. While it's in development the information may not be up to date so please don't rely on it at present.
+        This intranet is in Beta and is still being tested. This page may not be up to date.
       </p>
     </div>
   </div>
