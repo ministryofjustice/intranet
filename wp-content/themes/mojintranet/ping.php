984454f09fb1d33a353cf465df5c5690e095c25c
Ryan Jarrett <ryan.jarrett@digital.justice.gov.uk>
2015-08-05 12:22:20 +0100

    Add headers to prevent browser/Varnish caching to ping.json

--- wp-content/themes/mojintranet/ping.php
+++ wp-content/themes/mojintranet/ping.php
@@ -1,5 +1,8 @@
 <?php
 
+header('Cache-Control: max-age=0, no-cache, no-store, must-revalidate');
+header('Expires: Thu, 01 Jan 1970 00:00:00 GMT');
+header('Pragma: no-cache');
 header('Content-Type: application/json');
 
 $json_string = file_get_contents('build.json');
