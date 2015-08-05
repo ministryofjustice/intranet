a6780cc764e896a7d8927a8e9f6be45288da089e
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-08-05 11:46:04 +0100

    changed the search POST 'page' location

--- wp-content/themes/mojintranet/views/modules/search_form.php
+++ wp-content/themes/mojintranet/views/modules/search_form.php
@@ -1,4 +1,4 @@
-<form role="search" class="search-form" name="search-form" action="<?=site_url()?>/search-results/" method="post">
+<form role="search" class="search-form" name="search-form" action="<?=site_url()?>/search" method="post">
   <input class="keywords-field" type="text" placeholder="Search" name="s" id="s" value="<?=htmlentities(urldecode(get_query_var('search-string')))?>" title="Search">
   <input class="search-btn cta" id="search-btn" type="submit" value="Search" />
 </form>
