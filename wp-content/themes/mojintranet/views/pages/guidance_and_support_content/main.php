29fad0437dfa4b9ef248f45b321bf8ff0a42a67f
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2015-08-18 15:12:41 +0100

    change tabs so they use ARIA and the dynamic content is more accessible

--- wp-content/themes/mojintranet/views/pages/guidance_and_support_content/main.php
+++ wp-content/themes/mojintranet/views/pages/guidance_and_support_content/main.php
@@ -25,64 +25,63 @@
         </div>
       </div>
 
-      <ul class="content-tabs <?=$tab_count >= 3 ? 'small-tabs' : ''?> <?=$tab_count <= 1 ? 'hidden' : ''?>">
+      <ul role="tablist" class="content-tabs <?=$tab_count >= 3 ? 'small-tabs' : ''?> <?=$tab_count <= 1 ? 'hidden' : ''?>">
         <?php foreach($tab_array as $tab_row): ?>
-          <li data-content="<?=$tab_row['name']?>">
-            <a href=""><?=$tab_row['title']?></a>
+          <li role="presentation">
+            <a id="tab-<?=$tab_row['name']?>" role="tab" aria-selected="false" aria-controls="panel-<?=$tab_row['name']?>" href=""><?=$tab_row['title']?></a>
           </li>
         <?php endforeach ?>
       </ul>
 
-      <div class="tab-content editable"></div>
-      <span class="date-updated">Last updated: <time><?=$human_date?></time></span>
-    </div>
-  </div>
+      <?php $tab_no=1; ?>
+      <?php foreach($tab_array as $tab_number=>$tab_row): ?>
+        <div id="panel-<?=$tab_row['name']?>" class="template-partial editable" role="tabpanel" aria-labelled-by="tab-<?='tab-'.$tab_row['name']?>">
+          <?php foreach($tab_row['sections'] as $section): ?>
+            <h2><?=$section['title']?></h2>
+            <?=$section['content']?>
+          <?php endforeach ?>
 
-  <?php $tab_no=1; ?>
-  <?php foreach($tab_array as $tab_number=>$tab_row): ?>
-    <div class="template-partial" data-template-type="tab-content" data-content-name="<?=$tab_row['name']?>">
-      <?php foreach($tab_row['sections'] as $section): ?>
-        <h2><?=$section['title']?></h2>
-        <?=$section['content']?>
+          <?php if(count($link_array->tabs[$tab_number])): ?>
+            <?php if($autoheadings): ?>
+              <h2><?=$links_title?></h2>
+            <?php endif ?>
+            <ul>
+              <?php foreach($link_array->tabs[$tab_number] as $link_row): ?>
+                <?php if($link_row['heading']): ?>
+                  </ul>
+                  <h2><?=$link_row['linktext']?></h2>
+                  <ul>
+                <?php else: ?>
+                  <li>
+                    <a href="<?=$link_row['linkurl']?>"><?=$link_row['linktext']?></a>
+                  </li>
+                <?php endif ?>
+              <?php endforeach ?>
+            </ul>
+            </div>
+          <?php endif ?>
+        </div>
+        <?php $tab_no++; ?>
       <?php endforeach ?>
 
-      <?php if(count($link_array->tabs[$tab_number])): ?>
-        <?php if($autoheadings): ?>
-          <h2><?=$links_title?></h2>
-        <?php endif ?>
-        <ul>
-          <?php foreach($link_array->tabs[$tab_number] as $link_row): ?>
-            <?php if($link_row['heading']): ?>
-              </ul>
-              <h2><?=$link_row['linktext']?></h2>
-              <ul>
-            <?php else: ?>
-              <li>
-                <a href="<?=$link_row['linkurl']?>"><?=$link_row['linktext']?></a>
-              </li>
-            <?php endif ?>
-          <?php endforeach ?>
-        </ul>
-        </div>
-      <?php endif ?>
-    </div>
-    <?php $tab_no++; ?>
-  <?php endforeach ?>
-
-  <div class="template-partial" data-name="menu-item">
-    <li class="menu-item">
-      <div class="menu-item-container">
-        <a href="" class="menu-item-link"></a>
-        <button href="#" class="dropdown-button">Expand</button>
+      <div class="template-partial" data-name="menu-item">
+        <li class="menu-item">
+          <div class="menu-item-container">
+            <a href="" class="menu-item-link"></a>
+            <button href="#" class="dropdown-button">Expand</button>
+          </div>
+          <ul class="children-list">
+          </ul>
+        </li>
       </div>
-      <ul class="children-list">
-      </ul>
-    </li>
-  </div>
 
-  <div class="template-partial" data-name="child-item">
-    <li class="child-item">
-      <a href="" class="child-item-link"></a>
-    </li>
+      <div class="template-partial" data-name="child-item">
+        <li class="child-item">
+          <a href="" class="child-item-link"></a>
+        </li>
+      </div>
+
+      <span class="date-updated">Last updated: <time><?=$human_date?></time></span>
+    </div>
   </div>
 </div>
