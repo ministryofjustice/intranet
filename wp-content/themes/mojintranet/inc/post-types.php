baa953ca1ba38251bacfa962998d4704897927f6
Ryan Jarrett <ryan.jarrett@digital.justice.gov.uk>
2015-09-07 15:05:09 +0100

    Add webchat custom post type declaration

--- wp-content/themes/mojintranet/inc/post-types.php
+++ wp-content/themes/mojintranet/inc/post-types.php
@@ -3,6 +3,38 @@
 // Controls post-types (custom and built-in)
 // ----------------------------------------
 
+// CPT DEFINITIONS
+// ---------------
+
+// Define webchat post type
+function define_webchat_post_type() {
+  register_post_type( 'webchat',
+    array(
+      'labels'        => array(
+        'name'               => __('Webchats'),
+        'singular_name'      => __('Webchat'),
+        'add_new_item'       => __('Add New Webchat'),
+        'edit_item'          => __('Edit Webchat'),
+        'new_item'           => __('New Webchat'),
+        'view_item'          => __('View Webchat'),
+        'search_items'       => __('Search Webchats'),
+        'not_found'          => __('No webchats found'),
+        'not_found_in_trash' => __('No webchats found in Trash')
+      ),
+      'description'   => __('Contains details of webchats'),
+      'public'        => true,
+      'menu_position' => 20,
+      'menu_icon'     => 'dashicons-phone',
+      'supports'      => array('title','editor','thumbnail','excerpt'),
+      'has_archive'   => 'webchats'
+    )
+  );
+}
+add_action( 'init', define_webchat_post_type);
+
+// CPT MODIFICATIONS
+// -----------------
+
 // Adds excerpts to pages
 function add_page_excerpts() {
   add_post_type_support('page','excerpt' );
