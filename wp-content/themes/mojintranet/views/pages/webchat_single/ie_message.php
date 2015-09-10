11126d49fca40c03890b049db603f2be24a7fcde
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-09-10 14:24:06 +0100

    updated the IE message

--- wp-content/themes/mojintranet/views/pages/webchat_single/ie_message.php
+++ wp-content/themes/mojintranet/views/pages/webchat_single/ie_message.php
@@ -1,5 +1,5 @@
 <?php if (!defined('ABSPATH')) die(); ?>
 
 <p class="compatibility-message">
-  Webchats are not supported in Internet Explorer. Use <a href="/about-firefox">Firefox</a> instead.
+  Webchats are not fully supported in Internet Explorer. Use <a href="/about-firefox">Firefox</a> instead.
 </p>
