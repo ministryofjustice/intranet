d5c2a146c5b7f7b2e65249c84e05eaca18590423
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2015-11-03 11:05:35 +0000

    (WIP) finished the function gathering client data

--- wp-content/themes/mojintranet/assets/js/src/modules/page_feedback.js
+++ wp-content/themes/mojintranet/assets/js/src/modules/page_feedback.js
@@ -1,54 +1,3 @@
-/** Tabbed content
- */
-//(function($) {
-//  "use strict";
-//
-//  var App = window.App;
-//
-//  App.PageFeedback = function() {
-//    this.$link = $('.page-feedback-link');
-//    if(!this.$link.length) { return; }
-//    this.init();
-//  };
-//
-//  App.PageFeedback.prototype = {
-//    init: function() {
-//      this.cacheEls();
-//      this.bindEvents();
-//    },
-//
-//    cacheEls: function() {
-//    },
-//
-//    bindEvents: function() {
-//      this.$link.click($.proxy(this.prepareEmail, this));
-//    },
-//
-//    prepareEmail: function(e) {
-//      var email = this.$link.attr('href');
-//      var id = 'T' + new Date().getTime();
-//      var subject = 'Page feedback - ' + $('title').text() + ' [' + id + ']';
-//      var body = [];
-//      var nl = '\n';
-//
-//      e.preventDefault();
-//
-//      body.push(new Array(71).join('-'));
-//      body.push('This information will help us a lot with resolving the issue. Please do not delete.');
-//      body.push('Page URL: ' + window.location.href);
-//      body.push('User agent: ' + window.navigator.userAgent);
-//      body.push('Screen resolution: ' + window.screen.availWidth + 'x' + window.screen.availHeight);
-//      body.push(new Array(71).join('-'));
-//
-//      body = nl + nl + body.join(nl) + nl;
-//
-//      window.location.href = email + '?subject='+encodeURIComponent(subject)+'&body='+encodeURIComponent(body);
-//    }
-//  };
-//}(jQuery));
-
-
-
 (function($) {
   "use strict";
 
@@ -84,7 +33,7 @@
 
     getClientData: function() {
       var dwTag = $('.template-container').data('dw-tag');
-      var email = dwTag === 'search-results' ? this.$top.data('alt-email') : this.$top.attr('email');
+      var email = dwTag === 'search-results' ? this.$top.data('alt-email') : this.$top.data('email');
       var id = 'T' + new Date().getTime();
 
       return {
@@ -92,11 +41,13 @@
         userAgent: window.navigator.userAgent,
         resolution: window.screen.availWidth + 'x' + window.screen.availHeight,
         subject: 'Page feedback - ' + $('title').text() + ' [' + id + ']',
-        email: email
+        email: email,
+        description: this.$top.find('.feedback-field').val()
       };
     },
 
-    sendForm: function() {
+    sendForm: function(e) {
+      e.preventDefault();
     }
   };
 }(jQuery));
