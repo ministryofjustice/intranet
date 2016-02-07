68f39175e23a512ed7e8d9f1397ab24c697a5c7f
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-02-07 14:54:57 +0000

    added validation framework

---
+++ wp-content/themes/mojintranet/views/modules/validation/validation.php
@@ -0,0 +1,18 @@
+<?php if (!defined('ABSPATH')) die(); ?>
+
+<script class="template-partial" data-name="validation-message" type="text/x-partial-template">
+  <p class="validation-message"></p>
+</script>
+
+<script class="template-partial" data-name="validation-summary-item" type="text/x-partial-template">
+  <li class="field">
+    <a href="#"></a>
+  </li>
+</script>
+
+<script class="template-partial" data-name="validation-summary" type="text/x-partial-template">
+  <div class="validation-summary">
+    <p class="please-fix">Please fix the following:</p>
+    <ul class="errors"></ul>
+  </div>
+</script>
