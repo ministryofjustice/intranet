7248d6923d3a560aa1f391f534a0051955805c8d
Ryan Jarrett <ryan.jarrett@digital.justice.gov.uk>
2016-02-11 08:13:25 +0000

    Alphapbetise require and include statements in functions.php

--- wp-content/themes/mojintranet/functions.php
+++ wp-content/themes/mojintranet/functions.php
@@ -1,31 +1,31 @@
 <?php
 // Includes and requires
-require_once('inc/security.php');                         // Security functions
-require_once('inc/searching.php');                        // Functions to enhance searching (using Relevanssi)
-require_once('inc/utility.php');                          // Utility functions
-require_once('inc/post-types.php');                       // Controls post-types (custom and built-in)
-require_once('inc/news-customiser.php');                  // Setup news customiser
-require_once('inc/customiser-controls.php');              // Extra customiser controls
-require_once('inc/redirects.php');                        // Site redirects
-require_once('inc/query-vars.php');                       // Register custom query variables
-require_once('inc/images.php');                           // Images sizes and functions
-require_once('inc/uploads.php');                          // File uploads
-require_once('inc/dependencies.php');                     // CSS/JS dependency enqueing
-require_once('inc/sidebars.php');                         // Register sidebars
-require_once('inc/titles.php');                           // Title filters
-require_once('inc/excerpts.php');                         // Excerpt filters
-require_once('inc/tidy-up.php');                          // Tidy up CMS
-require_once('inc/languages.php');                        // Controls the site language(s)
-require_once('inc/cache.php');                            // Amend the cache headers
-require_once('inc/documents.php');                        // Control how documents are handled
 require_once('inc/authors.php');                          // Additional author information
+require_once('inc/cache.php');                            // Amend the cache headers
+require_once('inc/customiser-controls.php');              // Extra customiser controls
+require_once('inc/dependencies.php');                     // CSS/JS dependency enqueing
+require_once('inc/documents.php');                        // Control how documents are handled
+require_once('inc/excerpts.php');                         // Excerpt filters
+require_once('inc/images.php');                           // Images sizes and functions
+require_once('inc/languages.php');                        // Controls the site language(s)
+require_once('inc/news-customiser.php');                  // Setup news customiser
+require_once('inc/post-types.php');                       // Controls post-types (custom and built-in)
+require_once('inc/query-vars.php');                       // Register custom query variables
+require_once('inc/redirects.php');                        // Site redirects
+require_once('inc/searching.php');                        // Functions to enhance searching (using Relevanssi)
+require_once('inc/security.php');                         // Security functions
+require_once('inc/sidebars.php');                         // Register sidebars
+require_once('inc/tidy-up.php');                          // Tidy up CMS
+require_once('inc/titles.php');                           // Title filters
+require_once('inc/uploads.php');                          // File uploads
+require_once('inc/utility.php');                          // Utility functions
 
 require_once('admin/templates/template-functions.php');   // Customises page editor based on template
 require_once('admin/editor-enhancements.php');            // Adds enhancements to post/page editor screen
 require_once('admin/errors.php');                         // Displays errors in admin
 
-include     ('helpers/debug.php');                        // Debug tool
 include     ('helpers/cachebuster.php');                  // Ensures updated CSS and JS are served to client
+include     ('helpers/debug.php');                        // Debug tool
 include     ('helpers/taggr.php');                        // Tool for retrieving pages by their dw-tag
 
 add_action( 'after_setup_theme', 'mojintranet_setup' );
