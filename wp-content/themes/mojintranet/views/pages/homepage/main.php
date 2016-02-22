6c39de928e217f46883cf6172a6decaf13036b32
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-02-22 14:37:40 +0000

    updated the social widget

--- wp-content/themes/mojintranet/views/pages/homepage/main.php
+++ wp-content/themes/mojintranet/views/pages/homepage/main.php
@@ -10,11 +10,20 @@
 
     <div class="col-lg-4 col-md-6 col-sm-12">
       <?php $this->view('pages/homepage/my_moj/main', $my_moj) ?>
+      <?php $this->view('pages/homepage/social') ?>
     </div>
+
 
     <?php #dynamic_sidebar('home-widget-area0'); ?>
     <?php #$this->view('widgets/posts', array('posts' => $posts, 'see_all_posts_url' => $see_all_posts_url)) ?>
     <?php #$this->view('widgets/events', array('events' => $events, 'see_all_events_url' => $see_all_events_url)) ?>
-    <?php #$this->view('pages/homepage/social') ?>
+  </div>
+
+  <div class="grid">
+    <div class="col-lg-8 col-md-6 col-sm-12">
+    </div>
+    <div class="col-lg-4 col-md-6 col-sm-12">
+    </div>
+
   </div>
 </div>
