17c1fd57eca9a9cddd8e12167329bf583cdea843
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-02-25 15:51:31 +0000

    created the new site header (no my-moj and no dept dropdown)

--- wp-content/themes/mojintranet/views/modules/header.php
+++ wp-content/themes/mojintranet/views/modules/header.php
@@ -6,6 +6,34 @@
       <a href="#content">Skip to main content</a>
     </div>
   </div>
+
+  <div class="grid header-top">
+    <div class="site-logo col-lg-6 col-lg-6 col-lg-12">
+      <a href="<?=WP_SITEURL?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+        <img src="<?=get_template_directory_uri()?>/assets/images/moj_logo.png" alt="Ministry of Justice logo" />
+      </a>
+    </div>
+    <div class="user-bar col-lg-6 col-lg-6 col-lg-12">
+    </div>
+  </div>
+
+  <div class="header-search">
+    <div class="grid">
+      <div class="col-lg-12 col-lg-12 col-lg-12">
+        <?php $this->view('modules/search_form') ?>
+      </div>
+    </div>
+  </div>
+
+  <div class="header-menu">
+    <div class="grid">
+      <div class="col-lg-12 col-lg-12 col-lg-12">
+        <?php dynamic_sidebar('main-menu') ?>
+      </div>
+    </div>
+  </div>
+
+  <?php /*
   <div class="grid header-top">
     <div class="col-lg-8 col-md-8 col-sm-10">
       <div class="site-logo">
@@ -40,5 +68,5 @@
     <div class="col-lg-4 col-md-4 col-sm-12">
       <div class="my-moj-trigger">My MoJ menu <span class="arrow">▼</span></div>
     </div>
-  </div>
+  </div> */ ?>
 </div>
