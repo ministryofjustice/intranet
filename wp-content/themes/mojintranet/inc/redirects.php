898a8ac7ee132100c1efd0746ac8d5bb8cb7057c
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-03-21 15:23:23 +0000

    updated redirects to login and register pages

--- wp-content/themes/mojintranet/inc/redirects.php
+++ wp-content/themes/mojintranet/inc/redirects.php
@@ -68,7 +68,7 @@ function dw_rewrite_rules() {
   add_rewrite_rule($regex, $redirect, 'top');
 
   //Custom controllers
-  $regex = '^(service|password|register|login)/?(.*)';
+  $regex = '^(service|password|create-an-account|sign-in)/?(.*)';
   $redirect = 'index.php?controller=$matches[1]&param_string=$matches[2]';
   add_rewrite_rule($regex, $redirect, 'top');
 }
