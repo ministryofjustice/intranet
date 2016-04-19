1d7330dc4dfce847fa6b6f98f669f18aabb7faa5
Robert Lowe <robertlowe@Admins-MacBook-Pro-7.local>
2016-04-19 15:05:36 +0100

    Meta Rewrite

--- wp-content/themes/mojintranet/inc/post-fork.php
+++ wp-content/themes/mojintranet/inc/post-fork.php
@@ -25,7 +25,6 @@ function dw_fork_post_as_draft(){
   $current_user = wp_get_current_user();
   $new_post_author = $current_user->ID;
 
-
   /*
    * if post data exists, create the post duplicate
    */
@@ -53,7 +52,7 @@ function dw_fork_post_as_draft(){
     /*
      * insert the post by wp_insert_post() function
      */
-    $new_post_id = wp_insert_post( $args );
+    $new_post_id = wp_insert_post($args);
 
     /*
      * get all current post terms ad set them to the new post draft
@@ -61,7 +60,7 @@ function dw_fork_post_as_draft(){
     $taxonomies = get_object_taxonomies($post->post_type); // returns array of taxonomy names for post type, ex array("category", "post_tag");
     foreach ($taxonomies as $taxonomy) {
 
-      if($taxonomy != 'agency' && $taxonomy != 'author') {
+      if ($taxonomy != 'agency' && $taxonomy != 'author') {
         $post_terms = wp_get_object_terms($post_id, $taxonomy, array('fields' => 'slugs'));
         wp_set_object_terms($new_post_id, $post_terms, $taxonomy, false);
       }
@@ -74,7 +73,7 @@ function dw_fork_post_as_draft(){
      */
     $post_agencies = wp_get_object_terms($post_id, 'agency', array('fields' => 'slugs'));
 
-    if(in_array($context,$post_agencies)){
+    if (in_array($context,$post_agencies) && in_array('hq',$post_agencies)) {
       $post_agencies = array_diff($post_agencies, array($context));
       wp_set_object_terms($post_id, $post_agencies, 'agency', false);
     }
@@ -87,17 +86,18 @@ function dw_fork_post_as_draft(){
     /*
      * duplicate all post meta just in two SQL queries
      */
-    $post_meta_infos = $wpdb->get_results("SELECT meta_key, meta_value FROM $wpdb->postmeta WHERE post_id=$post_id");
+    $post_meta_ary = get_post_meta($post_id);
 
-    if (count($post_meta_infos)!=0) {
-      $sql_query = "INSERT INTO $wpdb->postmeta (post_id, meta_key, meta_value) ";
-      foreach ($post_meta_infos as $meta_info) {
-        $meta_key = $meta_info->meta_key;
-        $meta_value = addslashes($meta_info->meta_value);
-        $sql_query_sel[]= "SELECT $new_post_id, '$meta_key', '$meta_value'";
+    if (count($post_meta_ary)!=0) {
+
+      foreach ($post_meta_ary as $key => $post_meta) {
+
+        if (is_array($post_meta) && count($post_meta) > 0) {
+          add_post_meta($new_post_id, $key, $post_meta[0]);
+        }
+
       }
-      $sql_query.= implode(" UNION ALL ", $sql_query_sel);
-      $wpdb->query($sql_query);
+
     }
 
     /*
@@ -109,13 +109,13 @@ function dw_fork_post_as_draft(){
     /*
      * finally, redirect to the edit post screen for the new draft
      */
-    wp_redirect( admin_url( 'post.php?action=edit&post=' . $new_post_id ) );
+    wp_redirect(admin_url( 'post.php?action=edit&post=' . $new_post_id ));
     exit;
   } else {
     wp_die('Post creation failed, could not find original post: ' . $post_id);
   }
 }
-add_action( 'admin_action_dw_fork_post_as_draft', 'dw_fork_post_as_draft' );
+add_action('admin_action_dw_fork_post_as_draft', 'dw_fork_post_as_draft');
 
 /*
  * Add the duplicate link to action list for post_row_actions
@@ -130,5 +130,5 @@ function dw_fork_post_link( $actions, $post ) {
   return $actions;
 }
 
-add_filter( 'post_row_actions', 'dw_fork_post_link', 10, 2 );
+add_filter('post_row_actions', 'dw_fork_post_link', 10, 2);
 add_filter('page_row_actions', 'dw_fork_post_link', 10, 2);
