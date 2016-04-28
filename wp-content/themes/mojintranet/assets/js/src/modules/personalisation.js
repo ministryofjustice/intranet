e3d5954519b844bb1e86d04c7fde3c40e47f5e33
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-04-28 18:57:32 +0100

    added functionality to hide selected modules for agencies

--- wp-content/themes/mojintranet/assets/js/src/modules/personalisation.js
+++ wp-content/themes/mojintranet/assets/js/src/modules/personalisation.js
@@ -9,10 +9,20 @@
 
   App.Personalisation.prototype = {
     init: function() {
+      this.settings = {
+        hideForAgency: {
+          'hmcts': ['.events-widget', '.events-menu-item']
+        }
+      };
+
+      this.agency = window.App.tools.helpers.agency.get();
+
+
       this.cacheEls();
       this.bindEvents();
 
       this.update();
+      this.hideContent();
     },
 
     cacheEls: function() {
@@ -23,23 +33,30 @@
     },
 
     update: function() {
-      var agency = window.App.tools.helpers.agency.get();
       var $homepage = $('.template-home');
       var $departmentDropdown = $('.department-list');
       var name;
 
-      if(App.tools.helpers.agency.isIntegrated(agency)) {
-        name = $departmentDropdown.find('[data-department="' + agency + '"]').text();
+      if(App.tools.helpers.agency.isIntegrated(this.agency)) {
+        name = $departmentDropdown.find('[data-department="' + this.agency + '"]').text();
       }
       else {
         name = $departmentDropdown.find('[data-department="hq"]').text();
       }
 
-      $('html').attr('data-agency', agency);
+      $('html').attr('data-agency', this.agency);
 
       if($homepage.length) {
         $('.template-home h1').text(name);
       }
+    },
+
+    hideContent: function() {
+      var selectorsToHide = this.settings.hideForAgency[this.agency] || [];
+
+      $.each(selectorsToHide, function(index, selector) {
+        $(selector).addClass('agency-hidden');
+      });
     }
   };
 }(window.jQuery));
