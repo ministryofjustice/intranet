db00d1dae25a77f7b5f20c6afcd02a1b0283fa45
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2016-04-29 14:15:47 +0100

    Change tab style to 2 spaces.

--- wp-content/themes/mojintranet/assets/js/src/modules/set_agency_from_url.js
+++ wp-content/themes/mojintranet/assets/js/src/modules/set_agency_from_url.js
@@ -1,54 +1,54 @@
-(function($) {
-    "use strict";
+(function ($) {
+  "use strict";
 
-    var App = window.App;
+  var App = window.App;
 
-    App.SetAgencyFromUrl = function() {
-        this.init();
-    };
+  App.SetAgencyFromUrl = function () {
+    this.init();
+  };
 
-    App.SetAgencyFromUrl.prototype = {
-        init: function() {
-            var agency = App.tools.getUrlParam('agency');
-            if (typeof agency === 'string') {
-                this.setAgency(agency);
-                this.removeFromUrl();
-            }
-        },
+  App.SetAgencyFromUrl.prototype = {
+    init: function () {
+      var agency = App.tools.getUrlParam('agency');
+      if (typeof agency === 'string') {
+        this.setAgency(agency);
+        this.removeFromUrl();
+      }
+    },
 
-        setAgency: function(agency) {
-            App.tools.helpers.agency.set(agency);
-        },
+    setAgency: function (agency) {
+      App.tools.helpers.agency.set(agency);
+    },
 
-        removeFromUrl: function() {
-            if(!window.history.replaceState) {
-                return;
-            }
+    removeFromUrl: function () {
+      if (!window.history.replaceState) {
+        return;
+      }
 
-            var urlParts = window.location.href.split('?');
-            var url = urlParts[0];
-            var query = urlParts[1].split('&');
+      var urlParts = window.location.href.split('?');
+      var url = urlParts[0];
+      var query = urlParts[1].split('&');
 
-            var newUrl = url;
-            var newQuery = [];
+      var newUrl = url;
+      var newQuery = [];
 
-            var a, length;
-            var pair, key;
+      var a, length;
+      var pair, key;
 
-            for (a = 0, length = query.length; a < length; a++) {
-                pair = query[a].split('=');
-                key = decodeURIComponent(pair.shift());
+      for (a = 0, length = query.length; a < length; a++) {
+        pair = query[a].split('=');
+        key = decodeURIComponent(pair.shift());
 
-                if (key !== 'agency') {
-                    newQuery.push(query[a]);
-                }
-            }
-
-            if (newQuery.length > 0) {
-                newUrl += '?' + newQuery.join('&');
-            }
-
-            window.history.replaceState(null, null, newUrl);
+        if (key !== 'agency') {
+          newQuery.push(query[a]);
         }
-    };
+      }
+
+      if (newQuery.length > 0) {
+        newUrl += '?' + newQuery.join('&');
+      }
+
+      window.history.replaceState(null, null, newUrl);
+    }
+  };
 }(jQuery));
