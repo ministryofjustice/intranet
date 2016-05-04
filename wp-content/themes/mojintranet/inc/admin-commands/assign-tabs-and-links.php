dcfdfbd61a6adbb78196b655363f267cd795651d
Robert Lowe <robertlowe@Admins-MacBook-Pro-7.local>
2016-05-04 15:21:27 +0100

    Guidance Tbas already set check

--- wp-content/themes/mojintranet/inc/admin-commands/assign-tabs-and-links.php
+++ wp-content/themes/mojintranet/inc/admin-commands/assign-tabs-and-links.php
@@ -33,95 +33,99 @@ class Assign_Tabs_And_Links extends Admin_Command {
         echo '<ul>';
         foreach ($pages as $page){
             if (get_post_meta($page->ID,'_wp_page_template',true) == 'page_guidance_and_support.php') {
-                //Find Links
-                $found_links_check = true;
-                $tab_links = array();
-                $count = 1;
-                while ($found_links_check == true) { //Presumed theres always text as there is no link count
-                    if (metadata_exists('post', $page->ID, "_quick_links-link-text" . $count)) {
-                        $type = 'link';
-                        $show_tab1 = false;
-                        $show_tab2 = false;
-                        $url = '';
 
-                        if (metadata_exists('post', $page->ID, "_quick_links-url" . $count)) {
-                            $url = get_post_meta($page->ID, '_quick_links-url' . $count, true);
-                        }
+                if (!metadata_exists('post', $page->ID, "guidance_tabs") ||  get_post_meta($page->ID, "guidance_tabs", true) == '0') { //check if tabs have been defined
+                    //Find Links
+                    $found_links_check = true;
+                    $tab_links = array();
+                    $count = 1;
+                    while ($found_links_check == true) { //Presumed theres always text as there is no link count
+                        if (metadata_exists('post', $page->ID, "_quick_links-link-text" . $count)) {
+                            $type = 'link';
+                            $show_tab1 = false;
+                            $show_tab2 = false;
+                            $url = '';
 
-                        if (metadata_exists('post', $page->ID, "_quick_links-heading" . $count) && get_post_meta($page->ID, '_quick_links-heading' . $count, true) == 'on' ) {
-                            $type = 'heading';
-                        }
+                            if (metadata_exists('post', $page->ID, "_quick_links-url" . $count)) {
+                                $url = get_post_meta($page->ID, '_quick_links-url' . $count, true);
+                            }
 
-                        if (metadata_exists('post', $page->ID, "_quick_links-firsttab" . $count) && get_post_meta($page->ID, '_quick_links-firsttab' . $count, true) == 'on' ) {
-                            $show_tab1 = true;
-                        }
+                            if (metadata_exists('post', $page->ID, "_quick_links-heading" . $count) && get_post_meta($page->ID, '_quick_links-heading' . $count, true) == 'on') {
+                                $type = 'heading';
+                            }
 
-                        if (metadata_exists('post', $page->ID, "_quick_links-secondtab" . $count) && get_post_meta($page->ID, '_quick_links-secondtab' . $count, true) == 'on' ) {
-                            $show_tab2 = true;
-                        }
+                            if (metadata_exists('post', $page->ID, "_quick_links-firsttab" . $count) && get_post_meta($page->ID, '_quick_links-firsttab' . $count, true) == 'on') {
+                                $show_tab1 = true;
+                            }
 
-                        $tab_link = array(
-                            'link_title' => get_post_meta($page->ID, '_quick_links-link-text' . $count, true),
-                            'link_url' => $url,
-                            'link_type' => $type,
-                        );
+                            if (metadata_exists('post', $page->ID, "_quick_links-secondtab" . $count) && get_post_meta($page->ID, '_quick_links-secondtab' . $count, true) == 'on') {
+                                $show_tab2 = true;
+                            }
 
-                        if($show_tab1) {
-                            $tab_links[0][] = $tab_link;
-                        }
-                        if($show_tab2) {
-                            $tab_links[1][] = $tab_link;
-                        }
-
-                    }
-                    else {
-                        $found_links_check = false;
-                    }
-                    $count++;
-                }
-                if(count($tab_links) > 0){
-                    foreach($tab_links as $key => $tab) {
-                        echo '<li>' . count($tab) . ' Links (Tab:' . ($key + 1) . ') found for page: "' . $page->post_title . '" (' . $page->ID . ')</li>';
-                    }
-                }
-
-                //Find Tabs
-                $tab_count = get_post_meta($page->ID, '_content_tabs-tab-count', true);
-                if ($tab_count > 0) {
-                    $guidance_tabs = array();
-
-                    for ($current_tab = 1; $current_tab <= $tab_count; $current_tab++) {
-                        $new_tab = array();
-                        $section_count = get_post_meta($page->ID, '_content_tabs-tab-'.$current_tab.'-section-count', true);
-                        $new_tab['sections'] = array();
-                        $new_tab['tab_title'] = get_post_meta($page->ID, '_content_tabs-tab-'.$current_tab.'-title', true);
-
-                        for ($current_section = 1; $current_section <= $section_count; $current_section++) {
-                            $section_title = get_post_meta($page->ID, '_content_tabs-tab-' . $current_tab . '-section-' . $current_section . '-title', true);
-                            $section_content = get_post_meta($page->ID, '_content_tabs-tab-' . $current_tab . '-section-' . $current_section . '-content', true);
-                            $new_tab['sections'][] = array(
-                                'section_title' => $section_title,
-                                'section_content' => $section_content,
-                                'section_html_content' => ''
+                            $tab_link = array(
+                                'link_title' => get_post_meta($page->ID, '_quick_links-link-text' . $count, true),
+                                'link_url' => $url,
+                                'link_type' => $type,
                             );
+
+                            if ($show_tab1) {
+                                $tab_links[0][] = $tab_link;
+                            }
+                            if ($show_tab2) {
+                                $tab_links[1][] = $tab_link;
+                            }
+
+                        } else {
+                            $found_links_check = false;
                         }
-
-                        $new_tab['links'] = array();
-
-                        if (is_array($tab_links[$current_tab - 1]) && count($tab_links[$current_tab - 1]) > 0) {
-                            $new_tab['links'] = $tab_links[$current_tab - 1];
+                        $count++;
+                    }
+                    if (count($tab_links) > 0) {
+                        foreach ($tab_links as $key => $tab) {
+                            echo '<li>' . count($tab) . ' Links (Tab:' . ($key + 1) . ') found for page: "' . $page->post_title . '" (' . $page->ID . ')</li>';
                         }
-
-                        $guidance_tabs[] = $new_tab;
                     }
 
+                    //Find Tabs
+                    $tab_count = get_post_meta($page->ID, '_content_tabs-tab-count', true);
+                    if ($tab_count > 0) {
+                        $guidance_tabs = array();
+
+                        for ($current_tab = 1; $current_tab <= $tab_count; $current_tab++) {
+                            $new_tab = array();
+                            $section_count = get_post_meta($page->ID, '_content_tabs-tab-' . $current_tab . '-section-count', true);
+                            $new_tab['sections'] = array();
+                            $new_tab['tab_title'] = get_post_meta($page->ID, '_content_tabs-tab-' . $current_tab . '-title', true);
+
+                            for ($current_section = 1; $current_section <= $section_count; $current_section++) {
+                                $section_title = get_post_meta($page->ID, '_content_tabs-tab-' . $current_tab . '-section-' . $current_section . '-title', true);
+                                $section_content = get_post_meta($page->ID, '_content_tabs-tab-' . $current_tab . '-section-' . $current_section . '-content', true);
+                                $new_tab['sections'][] = array(
+                                    'section_title' => $section_title,
+                                    'section_content' => $section_content,
+                                    'section_html_content' => ''
+                                );
+                            }
+
+                            $new_tab['links'] = array();
+
+                            if (is_array($tab_links[$current_tab - 1]) && count($tab_links[$current_tab - 1]) > 0) {
+                                $new_tab['links'] = $tab_links[$current_tab - 1];
+                            }
+
+                            $guidance_tabs[] = $new_tab;
+                        }
 
 
-                    update_field('field_572320a8bc14c', $guidance_tabs,  $page->ID);
+                        update_field('field_572320a8bc14c', $guidance_tabs, $page->ID);
 
-                    echo '<li>Assigned tabs for page: "' . $page->post_title . '" (' . $page->ID . ')</li>';
-                } else {
-                    echo '<li>Skipped Page: "' . $page->post_title . '" (' . $page->ID . ')</li>';
+                        echo '<li>Assigned tabs for page: "' . $page->post_title . '" (' . $page->ID . ')</li>';
+                    } else {
+                        echo '<li>Skipped Page (No Tabs Found): "' . $page->post_title . '" (' . $page->ID . ')</li>';
+                    }
+                }
+                else {
+                    echo '<li>Skipped Page (Tabs already defined): "' . $page->post_title . '" (' . $page->ID . ')</li>';
                 }
             }
         }
