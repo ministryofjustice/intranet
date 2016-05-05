02f1aa750787d5f871fdadc750aaed2bee4569cc
Robert Lowe <robertlowe@Admins-MacBook-Pro-7.local>
2016-05-05 11:40:13 +0100

    Remove Old Tabs and Quicklinks

--- wp-content/themes/mojintranet/admin/templates/template-config.php
+++ wp-content/themes/mojintranet/admin/templates/template-config.php
@@ -6,21 +6,9 @@ $template_options = array(
   'guidance-and-support' => array(
     'add'       => array(),
     'del'       => array('editor'),
-    'js'        => array('jquery','jquery-ui-draggable','jquery-ui-tabs','jquery-ui-accordion','jquery-ui-dialog'),
-    'css'       => array('wp-jquery-ui-dialog','jquery-admin-ui-css'),
+    'js'        => array(),
+    'css'       => array(),
     'metaboxes' => array(
-      array(
-        'id'       => 'quick_links',
-        'title'    => 'Quick Links',
-        'context'  => 'normal',
-        'priority' => 'core'
-      ),
-      array(
-        'id'       => 'content_tabs',
-        'title'    => 'Content Tabs',
-        'context'  => 'normal',
-        'priority' => 'core'
-      ),
       array(
         'id' => 'left_hand_nav',
         'title' => 'Left Hand Navigation',
