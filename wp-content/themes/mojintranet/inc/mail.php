624254c8bcc16ecf0fa967aab3344ec31140c23c
Robert Lowe <robertlowe@Admins-MacBook-Pro-52.local>
2016-05-12 15:24:28 +0100

    Remove Blog Name from emails

--- wp-content/themes/mojintranet/inc/mail.php
+++ wp-content/themes/mojintranet/inc/mail.php
@@ -8,5 +8,14 @@ function dw_mail_from($old) {
 
 add_filter('wp_mail_from_name', 'dw_mail_from_name');
 function dw_mail_from_name($old) {
-  return 'newintranet';
+  return 'Intranet';
+}
+
+//remove sitename from email subject
+add_filter('wp_mail', 'email_subject_remove_sitename');
+function email_subject_remove_sitename($email) {
+  $blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
+  $email['subject'] = str_replace("[".$blogname."] - ", "", $email['subject']);
+  $email['subject'] = str_replace("[".$blogname."]", "", $email['subject']);
+  return $email;
 }
