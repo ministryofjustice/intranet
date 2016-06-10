254b3fa5ee588e181e577513f10cc7dca60ab466
Robert Lowe <robertlowe@Admins-MacBook-Pro-52.local>
2016-06-10 11:45:35 +0100

    Walker base class

--- wp-content/themes/mojintranet/inc/taxonomies/agency-terms-walker.php
+++ wp-content/themes/mojintranet/inc/taxonomies/agency-terms-walker.php
@@ -1,41 +1,9 @@
 <?php
 
-class Walker_Agency_Terms extends Walker {
-    public $tree_type = 'category';
-    public $db_fields = array ('parent' => 'parent', 'id' => 'term_id'); //TODO: decouple this
+// Include Walker_Category_Checklist class
+require_once( ABSPATH . 'wp-admin/includes/class-walker-category-checklist.php' );
 
-    /**
-     * Starts the list before the elements are added.
-     *
-     * @see Walker:start_lvl()
-     *
-     * @since 2.5.1
-     *
-     * @param string $output Passed by reference. Used to append additional content.
-     * @param int    $depth  Depth of category. Used for tab indentation.
-     * @param array  $args   An array of arguments. @see wp_terms_checklist()
-     */
-    public function start_lvl( &$output, $depth = 0, $args = array() ) {
-        $indent = str_repeat("\t", $depth);
-        $output .= "$indent<ul class='children'>\n";
-    }
-
-    /**
-     * Ends the list of after the elements are added.
-     *
-     * @see Walker::end_lvl()
-     *
-     * @since 2.5.1
-     *
-     * @param string $output Passed by reference. Used to append additional content.
-     * @param int    $depth  Depth of category. Used for tab indentation.
-     * @param array  $args   An array of arguments. @see wp_terms_checklist()
-     */
-    public function end_lvl( &$output, $depth = 0, $args = array() ) {
-        $indent = str_repeat("\t", $depth);
-        $output .= "$indent</ul>\n";
-    }
-
+class Walker_Agency_Terms extends Walker_Category_Checklist {
     /**
      * Start the element output.
      *
