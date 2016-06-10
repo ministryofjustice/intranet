8316e3d58058ef43429ea01a4cd1635d45e2699b
Robert Lowe <robertlowe@Admins-MacBook-Pro-52.local>
2016-06-10 12:15:21 +0100

    Document functions

--- wp-content/themes/mojintranet/inc/taxonomies/content-category.php
+++ wp-content/themes/mojintranet/inc/taxonomies/content-category.php
@@ -5,7 +5,6 @@ namespace MOJ_Intranet\Taxonomies;
 use Agency_Context;
 
 class Content_Category extends Taxonomy {
-
     public function __construct() {
         parent::__construct();
 
@@ -15,6 +14,9 @@ class Content_Category extends Taxonomy {
         add_action('add_meta_boxes', array($this, 'add_custom_category_meta_box'));
     }
 
+    /**
+     * Set Category Permissions for Adminstrator, Global Editor and Agency Editor
+     */
     public function set_role_permissions() {
         $administrator = get_role('administrator');
 
@@ -31,28 +33,39 @@ class Content_Category extends Taxonomy {
             $agency_editor->add_cap($this->args['capabilities']['assign_terms']);
         }
     }
+
+    /**
+     * Check if any terms in the category relate to the current Agency context
+     *
+     * @return bool
+     */
     public function context_has_terms() {
         $terms = get_terms($this->name, array('hide_empty' => 0));
         $context = Agency_Context::get_agency_context('term_id');
-        $has_terms = false;
 
         foreach ($terms as $term){
             $term_agencies = get_field('term_used_by', $this->name . '_' . $term->term_id);
 
             if (in_array($context, $term_agencies)) {
-                $has_terms = true;
+                return true;
             }
         }
 
-        return $has_terms;
+        return false;
     }
 
+    /**
+     * Remove the default category metabox
+     */
     public function remove_default_meta_box() {
         foreach ($this->object_types as $type) {
             remove_meta_box($this->name . 'div', $type, 'normal');
         }
     }
 
+    /**
+     * Adds the custom category metabox
+     */
     public function add_custom_category_meta_box() {
         if ($this->context_has_terms()) {
             foreach ($this->object_types as $type) {
@@ -61,6 +74,9 @@ class Content_Category extends Taxonomy {
         }
     }
 
+    /**
+     * Show the custom category metabox. Which uses the Agency Terms Walker.
+     */
     public function show_custom_category_meta_box($post) {
         echo '<div id="' . $this->name . '-all" class="tabs-panel categorydiv">';
             echo '<ul id="' . $this->name . 'checklist" class="list:' . $this->name . ' categorychecklist form-no-clear">';
