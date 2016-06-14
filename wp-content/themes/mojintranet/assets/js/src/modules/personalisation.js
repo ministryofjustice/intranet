6a600c660b0fe046926b5e19aaf0aed5dc5528d7
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-06-14 12:35:43 +0100

    fixed the missing default value of agency cookie

--- wp-content/themes/mojintranet/assets/js/src/modules/personalisation.js
+++ wp-content/themes/mojintranet/assets/js/src/modules/personalisation.js
@@ -21,10 +21,11 @@
 
       this.cacheEls();
 
-      this.updateAgencyFromUrl();
+      this.setAgency();
       this.addAgencyAttribute();
       this.initializeMenu();
       this.updateLogo();
+
       this.updateHomepageHeading();
       this.updateSearchPlaceholder();
       this.hideContent();
@@ -113,14 +114,22 @@
       }
     },
 
-    updateAgencyFromUrl: function() {
+    setAgency: function() {
       var agency = App.tools.getUrlParam('agency');
+      var agencyTools = App.tools.helpers.agency;
 
+      //set agency from url
       if (typeof agency === 'string') {
-        App.tools.helpers.agency.set(agency);
+        agencyTools.set(agency);
         this.removeAgencyFromUrl();
-        this.agency = window.App.tools.helpers.agency.get();
       }
+
+      //set default agency in cookie if missing
+      if(!agencyTools.getCookie()) {
+        agencyTools.set(agencyTools.getForContent());
+      }
+
+      this.agency = window.App.tools.helpers.agency.get();
     },
 
     removeAgencyFromUrl: function() {
