b69d7a93247fe588fbb02672e36841728f224888
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-06-21 13:05:59 +0100

    made breadcrumbs model use hierarchy model

--- wp-content/themes/mojintranet/models/breadcrumbs.php
+++ wp-content/themes/mojintranet/models/breadcrumbs.php
@@ -4,42 +4,25 @@ class Breadcrumbs_model extends MVC_model {
   function get_data() {
     wp_reset_query();
 
-    $breadcrumbs = array();
+    $breadcrumbs = [];
     $id = get_the_ID();
-    $type = get_post_type();
-    $parents = get_post_ancestors($id);
-    $home_page_id = Taggr::get_id('homepage');
-    $landing_page_id = null;
+    $ancestor_ids = $this->model->hierarchy->get_ancestor_ids($id);
+    $homepage_id = Taggr::get_id('homepage');
 
-    $parents = array_reverse($parents);
-    array_push($parents, $id);
-
-    if($type == 'news') {
-      $landing_page_id = Taggr::get_id('news-landing');
-    }
-    elseif($type == 'post') {
-      $landing_page_id = Taggr::get_id('blog-landing');
-    }
-    elseif($type == 'event') {
-      $landing_page_id = Taggr::get_id('events-landing');
-    }
-    elseif($type == 'webchat') {
-      $landing_page_id = Taggr::get_id('webchats-landing');
+    if ($ancestor_ids[0] != $homepage_id) {
+      array_unshift($ancestor_ids, $homepage_id);
     }
 
-    if($landing_page_id) array_unshift($parents, $landing_page_id);
-    if($home_page_id && $home_page_id != $id) array_unshift($parents, $home_page_id);
-
-    //convert the ids array to useful data
-    foreach($parents as $page_id) {
-      $breadcrumbs[] = array(
-        'title' => get_the_title($page_id),
-        'url' => get_permalink($page_id)
-      );
+    //convert the ids to useful data
+    foreach ($ancestor_ids as $post_id) {
+      $breadcrumbs[] = [
+        'title' => get_the_title($post_id),
+        'url' => get_permalink($post_id)
+      ];
     }
 
     $breadcrumbs[count($breadcrumbs) - 1]['last'] = true;
 
-    return is_404() ? array() : $breadcrumbs;
+    return is_404() ? [] : $breadcrumbs;
   }
 }
