b82a5c263278a21d82b1d60a4bc810d70a143181
Robert Lowe <robertlowe@Admins-MacBook-Pro-52.local>
2016-07-04 09:52:47 +0100

    Space and comma fix

--- wp-content/themes/mojintranet/inc/admin-commands.php
+++ wp-content/themes/mojintranet/inc/admin-commands.php
@@ -13,7 +13,7 @@ $load_commands = array(
     // filename => Class_Name
     'assign-agency-terms-to-posts' => 'Assign_Agency_Terms_To_Posts',
     'remove-old-home-page-options' => 'Remove_Old_Home_Page_Options',
-     'reset-all-pages-menu-order' => 'Reset_All_Pages_Menu_Order'
+    'reset-all-pages-menu-order' => 'Reset_All_Pages_Menu_Order',
 );
 
 $admin_commands = array();
