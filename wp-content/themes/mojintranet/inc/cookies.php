4984d0aaf49de222f6941da981d108a5f007e4e6
Robert Lowe <robertlowe@Admins-MacBook-Pro-52.local>
2016-07-06 15:59:08 +0100

    Prefix on cookie

--- wp-content/themes/mojintranet/inc/cookies.php
+++ wp-content/themes/mojintranet/inc/cookies.php
@@ -3,7 +3,7 @@ function dw_set_cache_cookie() {
 
   $timeout = current_user_can('edit_posts')? 7 * DAYS_IN_SECONDS : 1;
 
-  setcookie('disable_cache', 1, $timeout, COOKIEPATH, COOKIE_DOMAIN);
+  setcookie('dw_disable_cache', 1, $timeout, COOKIEPATH, COOKIE_DOMAIN);
 
 }
 add_action('init', 'dw_set_cache_cookie', 10, 3);
