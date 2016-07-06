8336f6f34e1b5b4cb22f1a0b8184da7db4e40837
Robert Lowe <robertlowe@Admins-MacBook-Pro-52.local>
2016-07-06 16:27:00 +0100

    Cookie name

--- wp-content/themes/mojintranet/inc/cookies.php
+++ wp-content/themes/mojintranet/inc/cookies.php
@@ -3,7 +3,7 @@ function dw_set_cache_cookie() {
 
   $timeout = current_user_can('edit_posts')? 7 * DAYS_IN_SECONDS : 1;
 
-  setcookie('dw_disable_cache', 1, $timeout, COOKIEPATH, COOKIE_DOMAIN);
+  setcookie('dw_can_edit_posts', 1, $timeout, COOKIEPATH, COOKIE_DOMAIN);
 
 }
 add_action('init', 'dw_set_cache_cookie', 10, 3);
