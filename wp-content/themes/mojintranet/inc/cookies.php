3b2be7d6a382acbfac435c03cb8e888cb4e8c927
Robert Lowe <robertlowe@Admins-MacBook-Pro-52.local>
2016-07-13 17:16:07 +0100

    Cookie Fix

--- wp-content/themes/mojintranet/inc/cookies.php
+++ wp-content/themes/mojintranet/inc/cookies.php
@@ -1,9 +1,7 @@
 <?php
 function dw_set_cache_cookie() {
 
-  $timeout = current_user_can('edit_posts')? 7 * DAYS_IN_SECONDS : 1;
-
-  setcookie('dw_can_edit_posts', 1, $timeout, COOKIEPATH, COOKIE_DOMAIN);
+  setcookie('dw_can_edit_posts',  current_user_can('edit_posts')? 1 : 0 , 7 * DAYS_IN_SECONDS, COOKIEPATH, COOKIE_DOMAIN);
 
 }
 add_action('init', 'dw_set_cache_cookie', 10, 3);
