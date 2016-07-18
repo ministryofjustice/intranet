c619ee69330d859e8db7d7dea1d1da4d5e377310
Robert Lowe <robertlowe@Admins-MacBook-Pro-52.local>
2016-07-18 10:34:24 +0100

    Cookie Unset

--- wp-content/themes/mojintranet/inc/cookies.php
+++ wp-content/themes/mojintranet/inc/cookies.php
@@ -1,7 +1,7 @@
 <?php
 function dw_set_cache_cookie() {
 
-  setcookie('dw_can_edit_posts',  current_user_can('edit_posts')? 1 : 0 , 7 * DAYS_IN_SECONDS, COOKIEPATH, COOKIE_DOMAIN);
+  setcookie('dw_can_edit_posts',  current_user_can('edit_posts')? 1 : 0 , current_user_can('edit_posts')? 7 * DAYS_IN_SECONDS : 1, COOKIEPATH, COOKIE_DOMAIN);
 
 }
 add_action('init', 'dw_set_cache_cookie', 10, 3);
