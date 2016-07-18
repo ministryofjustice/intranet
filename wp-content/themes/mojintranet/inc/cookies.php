7b1fb287a7aecec4402aa6506ce904d540d12e6d
Robert Lowe <robertlowe@Admins-MacBook-Pro-52.local>
2016-07-18 11:04:26 +0100

    Test

--- wp-content/themes/mojintranet/inc/cookies.php
+++ wp-content/themes/mojintranet/inc/cookies.php
@@ -3,5 +3,6 @@ function dw_set_cache_cookie() {
 
   setcookie('dw_can_edit_posts',  current_user_can('edit_posts')? 1 : 0 , current_user_can('edit_posts')? 7 * DAYS_IN_SECONDS : 1, COOKIEPATH, COOKIE_DOMAIN);
 
+  
 }
 add_action('init', 'dw_set_cache_cookie', 10, 3);
