13f24ab8e8ca3e7337a8890f0823391b8629b5fe
Robert Lowe <robertlowe@Admins-MacBook-Pro-52.local>
2016-07-25 16:04:09 +0100

    Merge dashboard widget functions

--- wp-content/themes/mojintranet/admin/dashboard.php
+++ wp-content/themes/mojintranet/admin/dashboard.php
@@ -1,11 +1,12 @@
 <?php
 
+// Hide dashboard widgets
 function dw_remove_dashboard_widgets() {
-  global $wp_meta_boxes;
-
-  unset($wp_meta_boxes['dashboard']['normal']['core']['recently-edited-content']);
+  remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+  remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );
+  remove_meta_box( 'recently-edited-content', 'dashboard', 'normal' );
+  remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+  remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+  remove_action( 'welcome_panel', 'wp_welcome_panel' );
 }
-
 add_action('wp_dashboard_setup', 'dw_remove_dashboard_widgets' );
-
-
