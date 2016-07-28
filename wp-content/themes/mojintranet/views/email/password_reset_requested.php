77118d7ff8ea0898422d0fd91627d603b7eb4896
Robert Lowe <robertlowe@Admins-MacBook-Pro-52.local>
2016-07-28 11:18:57 +0100

    Email address links

--- wp-content/themes/mojintranet/views/email/password_reset_requested.php
+++ wp-content/themes/mojintranet/views/email/password_reset_requested.php
@@ -7,7 +7,7 @@
     <p>To change your password click on the link below and follow the prompts:</p>
     <a href="<?=$reset_password_url?>">Change your password</a>
     <p>If you haven’t requested a new password, contact us at:</p>
-    <p>newintranet@digital.justice.gov.uk</p>
+    <p><a>newintranet@digital.justice.gov.uk</a></p>
     <p>The MoJ Intranet Team</p>
   </body>
 </html>
