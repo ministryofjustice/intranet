d24d893a7017252dcc11c0708afeba6ba358bf74
Robert Lowe <robertlowe@admins-mbp-52.ws.local>
2016-08-17 10:01:27 +0100

    Comments on Listing Functions

--- wp-content/themes/mojintranet/admin/listing.php
+++ wp-content/themes/mojintranet/admin/listing.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Remove Categories and Tags columns from the post listing page.
+ * Filter: manage_posts_columns
+ *
+ * @param array $columns
+ * @return array
+ */
 function dw_edit_posts_columns($columns) {
   unset($columns['categories']);
   unset($columns['tags']);
@@ -6,6 +13,13 @@ function dw_edit_posts_columns($columns) {
 }
 add_filter('manage_posts_columns' , 'dw_edit_posts_columns');
 
+/**
+ * Remove Comments column from the pages listing page.
+ * Filter: manage_pages_columns
+ *
+ * @param array $columns
+ * @return array
+ */
 function dw_edit_page_columns($columns) {
   unset($columns['comments']);
   return $columns;
