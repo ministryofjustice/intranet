91cc6718c6e3880838ed7c40f2ffde11e9d3b226
Robert Lowe <robertlowe@admins-mbp-52.ws.local>
2016-08-25 15:09:39 +0100

    Agency Logic Fix

--- wp-content/themes/mojintranet/inc/admin-commands/news-permissions-reset.php
+++ wp-content/themes/mojintranet/inc/admin-commands/news-permissions-reset.php
@@ -23,31 +23,21 @@ class News_Permissions_Reset extends Admin_Command {
      * @return void
      */
     public function execute() {
-
-
         $roles = ['administrator', 'agency-editor', 'regional-editor', 'editor'];
 
         $caps = [ 
             'edit_news',
             'edit_others_news',
-            'edit_private_news',
-            'edit_published_news',
             'read_news',
-            'read_document_revisions',
             'read_private_news',
             'delete_news',
-            'delete_others_news',
-            'delete_private_news',
-            'delete_published_news',
             'publish_news',
         ];
 
         foreach ($roles as $role) {
             $wpRole = get_role($role);
             foreach ($caps as $cap) {
-                
-                    $wpRole->add_cap($cap);
-
+               $wpRole->add_cap($cap);
             }
         }
         echo '<p>News Permissions reset</p>';
