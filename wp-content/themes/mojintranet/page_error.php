51db71569b809df81394172551f6ffdf0ced725f
Robert Lowe <robertlowe@Admins-MacBook-Pro-52.local>
2016-08-26 15:51:12 +0100

    500 error fix

--- wp-content/themes/mojintranet/page_error.php
+++ wp-content/themes/mojintranet/page_error.php
@@ -11,13 +11,19 @@ class Page_error extends MVC_controller {
   }
 
   function main() {
-    if(have_posts()) the_post();
-    $this->view('layouts/default', $this->get_data());
+    $this->view('layouts/default', $this->get_data('main'));
   }
 
-  private function get_data() {
+  function error404() {
+    $this->view('layouts/default', $this->get_data('error404'));
+  }
+
+  function error500() {
+    $this->view('layouts/default', $this->get_data('error500'));
+  }
+  private function get_data($view) {
     return array(
-      'page' => 'pages/error/main',
+      'page' => 'pages/error/' . $view,
       'template_class' => 'error',
       'cache_timeout' => 60 * 60 * 24 /* 1 day */
     );
