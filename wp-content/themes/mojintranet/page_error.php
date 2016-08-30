cabc0807b97ae10fdd1ecbecb2abd24e48b608bc
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-08-30 16:05:46 +0100

    fixed the missing controller path

--- wp-content/themes/mojintranet/page_error.php
+++ wp-content/themes/mojintranet/page_error.php
@@ -20,7 +20,7 @@ class Page_error extends MVC_controller {
 
   function error500() {
     $this->view('layouts/default', $this->get_data('500'));
-    trigger_error('Controller not found: ' . $controller_path . '. Entry point: ' . $_SERVER['REQUEST_URI'], E_USER_ERROR);
+    trigger_error('Controller not found: ' . $this->original_controller_path . '. Entry point: ' . $_SERVER['REQUEST_URI'], E_USER_ERROR);
   }
   private function get_data($view) {
     return array(
