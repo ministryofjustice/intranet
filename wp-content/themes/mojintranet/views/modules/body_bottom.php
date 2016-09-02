85da6a317815a79126ce6c30e513df2744f5ddeb
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-09-02 13:14:42 +0100

    moved the base.js below the IE scripts

--- wp-content/themes/mojintranet/views/modules/body_bottom.php
+++ wp-content/themes/mojintranet/views/modules/body_bottom.php
@@ -1,6 +1,5 @@
 <?php if (!defined('ABSPATH')) die(); ?>
 
-<script type="text/javascript" src="<?=get_template_directory_uri()?>/assets/js/base.js?<?=add_checksum_param('js/base.js')?>"></script>
 
 <!--[if lte IE 7]>
   <script type="text/javascript" src="<?=get_template_directory_uri()?>/assets/js/json3.min.js"></script>
@@ -10,6 +9,8 @@
   <script type="text/javascript" src="<?=get_template_directory_uri()?>/assets/js/html5-shiv.min.js"></script>
   <script type="text/javascript" src="<?=get_template_directory_uri()?>/assets/js/respond.min.js"></script>
 <![endif]-->
+
+<script type="text/javascript" src="<?=get_template_directory_uri()?>/assets/js/base.js?<?=add_checksum_param('js/base.js')?>"></script>
 
 <script type="text/javascript" src="<?=get_template_directory_uri()?>/assets/js/moment.min.js"></script>
 
