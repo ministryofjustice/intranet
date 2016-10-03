2052f45cd2bfc4f7d0c22a12934c1f70a97e5bc2
Robert Lowe <robertlowe@admins-mbp-52.ws.local>
2016-10-03 12:01:19 +0100

    Set cookie moved to seperate function

--- wp-content/themes/mojintranet/inc/cookies.php
+++ wp-content/themes/mojintranet/inc/cookies.php
@@ -1,21 +1,28 @@
 <?php
 function dw_set_cache_cookie() {
-  $cookie_url = preg_replace('#^https?://#', '', get_site_url());
-
   if (current_user_can('edit_posts')) {
-    setcookie('dw_can_edit_posts', 1, time() + (7 * DAY_IN_SECONDS), COOKIEPATH, $cookie_url);
+    dw_set_edit_posts_cookie(true);
   }
   elseif (isset($_COOKIE['dw_can_edit_posts'])) {
-    setcookie('dw_can_edit_posts', 0, 1, COOKIEPATH, $cookie_url);
+    dw_set_edit_posts_cookie(false);
   }
 }
 add_action('init', 'dw_set_cache_cookie', 10, 3);
 
 function dw_set_login_cookie($user_login, $user) {
-  $cookie_url = preg_replace('#^https?://#', '', get_site_url());
-
   if(isset($user->allcaps['edit_posts']) && $user->allcaps['edit_posts'] == true) {
-    setcookie('dw_can_edit_posts', 1, time() + (7 * DAY_IN_SECONDS), COOKIEPATH, $cookie_url);
+    dw_set_edit_posts_cookie(true);
   }
 }
 add_action('wp_login', 'dw_set_login_cookie', 10, 2);
+
+function dw_set_edit_posts_cookie($active) {
+  $cookie_url = preg_replace('#^https?://#', '', get_site_url());
+
+  if ($active == true) {
+    setcookie('dw_can_edit_posts', 1, time() + (7 * DAY_IN_SECONDS), COOKIEPATH, $cookie_url);
+  }
+  else {
+    setcookie('dw_can_edit_posts', 0, 1, COOKIEPATH, $cookie_url);
+  }
+}
