5c51ca7ee862c835546656bc99f8654d8c6e08b3
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2016-10-03 15:57:52 +0100

    Refactor code and add docblocks.

--- wp-content/themes/mojintranet/inc/cookies.php
+++ wp-content/themes/mojintranet/inc/cookies.php
@@ -1,4 +1,11 @@
 <?php
+
+/**
+ * Check if the user has permission to edit posts, and set or delete the
+ * 'edit posts' cookie accordingly.
+ *
+ * Action: init
+ */
 function dw_set_cache_cookie() {
   if (current_user_can('edit_posts')) {
     dw_set_edit_posts_cookie(true);
@@ -7,20 +14,39 @@ function dw_set_cache_cookie() {
     dw_set_edit_posts_cookie(false);
   }
 }
-add_action('init', 'dw_set_cache_cookie', 10, 3);
+add_action('init', 'dw_set_cache_cookie', 10);
 
-function dw_set_login_cookie($user_login, $user) {
-  if(isset($user->allcaps['edit_posts']) && $user->allcaps['edit_posts'] == true) {
+/**
+ * Check if the user has permission to edit posts, and set or delete the
+ * 'edit posts' cookie accordingly.
+ *
+ * This runs after wp_login, at which point current_user_can() cannot be used.
+ *
+ * Action: wp_login
+ *
+ * @param string $user_login
+ * @param WP_User $user
+ */
+function dw_set_login_cookie($user_login, WP_User $user) {
+  if (!empty($user->allcaps['edit_posts'])) {
     dw_set_edit_posts_cookie(true);
+  }
+  else {
+    dw_set_edit_posts_cookie(false);
   }
 }
 add_action('wp_login', 'dw_set_login_cookie', 10, 2);
 
+/**
+ * Set or delete the 'edit posts' cookie.
+ *
+ * @param bool $active To set the cookie, or not to set the cookie. That is the question.
+ */
 function dw_set_edit_posts_cookie($active) {
   $cookie_url = preg_replace('#^https?://#', '', get_site_url());
 
-  if ($active == true) {
-    setcookie('dw_can_edit_posts', 1, time() + (7 * DAY_IN_SECONDS), COOKIEPATH, $cookie_url);
+  if ($active) {
+    setcookie('dw_can_edit_posts', 1, strtotime('+7 days'), COOKIEPATH, $cookie_url);
   }
   else {
     setcookie('dw_can_edit_posts', 0, 1, COOKIEPATH, $cookie_url);
