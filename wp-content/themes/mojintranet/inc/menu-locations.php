ad696917ca1258287c499db477a54e5ea59a7821
Robert Lowe <robertlowe@admins-mbp-52.ws.local>
2016-10-04 10:10:36 +0100

    Remove Agency Menu Locations

--- wp-content/themes/mojintranet/inc/menu-locations.php
+++ wp-content/themes/mojintranet/inc/menu-locations.php
@@ -5,23 +5,6 @@
  */
 
 function register_taxonomy_menus() {
-    // This theme uses wp_nav_menu() in one location.
-    $terms = get_terms('agency', array(
-        'hide_empty' => false,
-    ));
-
-    $menus = array();
-
-    foreach ($terms as $term) {
-        $menu_slug = $term->slug . '-quick-links';
-        $menu_name = $term->name . ' Quick Links';
-        $menus[$menu_slug] = $menu_name;
-
-        $menu_slug = $term->slug . '-guidance-most-visited';
-        $menu_name = $term->name . ' Guidance Most Visited';
-        $menus[$menu_slug] = $menu_name;
-    }
-
     $menus['main-menu'] = 'Main Menu';
 
     register_nav_menus($menus);
