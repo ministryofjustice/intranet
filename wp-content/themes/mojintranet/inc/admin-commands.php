481b4ec6990b8cb7a1eeedc612bc4448198e5146
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-10-05 14:24:33 +0100

    added admin cmd to hide bylines

--- wp-content/themes/mojintranet/inc/admin-commands.php
+++ wp-content/themes/mojintranet/inc/admin-commands.php
@@ -12,6 +12,7 @@ if (!current_user_can('administrator')) {
 $load_commands = array(
     // filename => Class_Name
     'assign-agency-terms-to-posts' => 'Assign_Agency_Terms_To_Posts',
+    'hide-page-bylines' => 'Hide_Page_Bylines',
     'pb-guidance-optin' => 'PB_Guidance_Optin',
     'remove-old-tabs-and-links' => 'Remove_Old_Tabs_And_Links',
 );
