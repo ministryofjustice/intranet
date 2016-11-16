8735741033576c408506d9c7a625acc6905f8b4c
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-11-16 16:22:13 +0000

    fixed a post-merge bug

--- wp-content/themes/mojintranet/page_search_results.php
+++ wp-content/themes/mojintranet/page_search_results.php
@@ -10,9 +10,9 @@ class Page_search_results extends MVC_controller {
   private $post;
 
   function __construct($param_string, $post_id) {
-    $this->model('taxonomy');
     $this->post = get_post();
     parent::__construct($param_string, $post_id);
+    $this->model('taxonomy');
   }
 
   function main() {
