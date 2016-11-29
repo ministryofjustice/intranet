232ed04415bdb0d177f0ba7ad23fcbb7193a6ff3
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-11-29 11:22:42 +0000

    fixed the regions on LHS menu being limited to 5

--- wp-content/themes/mojintranet/models/page_tree.php
+++ wp-content/themes/mojintranet/models/page_tree.php
@@ -38,7 +38,8 @@ class Page_tree_model extends MVC_model {
       $children = get_posts([
         'meta_key' => 'dw_regional_template',
         'meta_value' => 'page_regional_landing.php',
-        'post_type' => 'any'
+        'post_type' => 'any',
+        'posts_per_page' => -1
       ]);
 
       $data['children'] = [];
