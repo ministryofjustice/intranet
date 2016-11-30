75b94401e2ddc0317bfb6e40e26a569c287480d9
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-11-30 17:33:16 +0000

    small code cleanup

--- wp-content/themes/mojintranet/page_news.php
+++ wp-content/themes/mojintranet/page_news.php
@@ -11,27 +11,25 @@ class Page_news extends MVC_controller {
     parent::__construct($param_string, $post_id);
 
     $this->model('taxonomy');
-    $this->post = get_post();
   }
 
   function main() {
+    $this->post = get_post();
     $this->view('layouts/default', $this->get_data());
   }
 
   function get_data() {
-    $top_slug = $this->post->post_name;
-
-    return array(
+    return [
       'page' => 'pages/news_landing/main',
       'template_class' => 'news-landing',
       'breadcrumbs' => true,
       'cache_timeout' => 60 * 60 * 24, /* 1 day */
-      'page_data' => array(
+      'page_data' => [
         'page_base_url' => rtrim(get_permalink($this->post_id), '/'),
         'news_categories' => htmlspecialchars(json_encode($this->model->taxonomy->get([
           'taxonomy' => 'news_category'
         ])))
-      )
-    );
+      ]
+    ];
   }
 }
