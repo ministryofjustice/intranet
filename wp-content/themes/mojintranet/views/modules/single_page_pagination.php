fce3335340f95901a5ee65d63af47f607565089c
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-12-02 18:30:33 +0000

    moved single page pagination view to a separate file

---
+++ wp-content/themes/mojintranet/views/modules/single_page_pagination.php
@@ -0,0 +1,19 @@
+<?php if (!defined('ABSPATH')) die(); ?>
+
+<ul class="content-nav nav-hidden grid">
+  <li class="previous col-lg-6 col-md-6 col-sm-6">
+    <a href="" aria-labelledby="prev-page-label">
+      <span class="nav-label"  id="prev-page-label">
+        Previous
+      </span>
+    </a>
+  </li>
+
+  <li class="next col-lg-6 col-md-6 col-sm-6">
+    <a href="" aria-labelledby="next-page-label">
+      <span class="nav-label" id="next-page-label">
+        Next
+      </span>
+    </a>
+  </li>
+</ul>
--- wp-content/themes/mojintranet/views/pages/blog_post/main.php
+++ wp-content/themes/mojintranet/views/pages/blog_post/main.php
@@ -32,27 +32,9 @@
         <?=$content?>
       </div>
 
-      <div class="item-row">
+      <div class="item-row"></div>
 
-      </div>
-
-      <ul class="content-nav nav-hidden grid">
-        <li class="previous col-lg-6 col-md-6 col-sm-6">
-          <a href="" aria-labelledby="prev-page-label">
-            <span class="nav-label"  id="prev-page-label">
-              Previous
-            </span>
-          </a>
-        </li>
-
-        <li class="next col-lg-6 col-md-6 col-sm-6">
-          <a href="" aria-labelledby="next-page-label">
-            <span class="nav-label" id="next-page-label">
-              Next
-            </span>
-          </a>
-        </li>
-      </ul>
+      <?php $this->view('modules/single_page_pagination') ?>
     </div>
   </div>
 
