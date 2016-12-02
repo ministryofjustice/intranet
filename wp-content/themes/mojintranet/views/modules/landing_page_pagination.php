8ae7965bc097f01d0b0e557876a14342f4e8f21d
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-12-02 18:30:52 +0000

    moved landing page pagination view to a separate file

---
+++ wp-content/themes/mojintranet/views/modules/landing_page_pagination.php
@@ -0,0 +1,26 @@
+<?php if (!defined('ABSPATH')) die(); ?>
+
+<ul class="content-nav grid">
+  <li class="previous disabled col-lg-6 col-md-6 col-sm-6">
+    <a href="" aria-labelledby="prev-page-label">
+      <span class="nav-label" id="prev-page-label">Previous page</span>
+      <span class="page-info">
+        <span class="prev-page"></span>
+        of
+        <span class="total-pages"></span>
+      </span>
+    </a>
+
+  </li>
+
+  <li class="next disabled col-lg-6 col-md-6 col-sm-6">
+    <a href="" aria-labelledby="next-page-label">
+      <span class="nav-label" id="next-page-label">Next page</span>
+      <span class="page-info">
+        <span class="next-page"></span>
+        of
+        <span class="total-pages"></span>
+      </span>
+    </a>
+  </li>
+</ul>
--- wp-content/themes/mojintranet/views/pages/events_landing/main.php
+++ wp-content/themes/mojintranet/views/pages/events_landing/main.php
@@ -35,30 +35,7 @@
     <div class="col-lg-8 col-md-8 col-sm-12">
       <ul class="results"></ul>
 
-      <ul class="content-nav grid">
-        <li class="previous disabled col-lg-6 col-md-6 col-sm-6">
-          <a href="" aria-labelledby="prev-page-label">
-            <span class="nav-label" id="prev-page-label">Previous page</span>
-            <span class="page-info">
-              <span class="prev-page"></span>
-              of
-              <span class="total-pages"></span>
-            </span>
-          </a>
-
-        </li>
-
-        <li class="next disabled col-lg-6 col-md-6 col-sm-6">
-          <a href="" aria-labelledby="next-page-label">
-            <span class="nav-label" id="next-page-label">Next page</span>
-            <span class="page-info">
-              <span class="next-page"></span>
-              of
-              <span class="total-pages"></span>
-            </span>
-          </a>
-        </li>
-      </ul>
+      <?php $this->view('modules/landing_page_pagination') ?>
     </div>
   </div>
 
