d70c08cd02c0d247ec4fa47c458146765f844842
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2016-12-07 16:33:37 +0000

    added debug mode

--- wp-content/themes/mojintranet/purge-cache.php
+++ wp-content/themes/mojintranet/purge-cache.php
@@ -3,6 +3,7 @@
 class Purge_cache extends MVC_Controller {
   function __construct($param_string, $post_id) {
     parent::__construct($param_string, $post_id);
+    $this->debug = isset($_GET['debug']) && $_GET['debug'] ? true : false;
     $this->settings = [
       'node_count' => 5,
       'max_tries' => 50, //maximum number of attempts to hit all the nodes
@@ -16,18 +17,26 @@ class Purge_cache extends MVC_Controller {
     $url = base64_decode($url);
     $nodes = [];
 
+    if ($this->debug) {
+      Debug::full($url);
+    }
+
     for ($count = 1; $count <= $this->settings['max_tries']; $count++) {
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL, $url);
       curl_setopt($ch, CURLOPT_HTTPHEADER, $this->_get_headers());
       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PURGE');
       curl_setopt($ch, CURLOPT_HEADER, true);
-      curl_setopt($ch, CURLOPT_NOBODY, true);
+      curl_setopt($ch, CURLOPT_NOBODY, !$this->debug);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 2000);
 
       $response = curl_exec($ch);
       curl_close($ch);
+
+      if ($this->debug) {
+        Debug::full($response);
+      }
 
       preg_match('/X-Served-By:\s+([0-9a-fA-F]+)/', $response, $matches);
 
@@ -53,12 +62,18 @@ class Purge_cache extends MVC_Controller {
       $message = '(attempts: ' . $count . ')';
     }
 
-    header('Content-Type: application/json');
     http_response_code(200);
-    echo json_encode([
-      'status' => $status,
-      'message' => $message
-    ]);
+
+    if ($this->debug) {
+      Debug::full([$status, $message]);
+    }
+    else {
+      header('Content-Type: application/json');
+      echo json_encode([
+        'status' => $status,
+        'message' => $message
+      ]);
+    }
   }
 
   private function _get_headers() {
