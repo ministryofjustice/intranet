7637bee372e65fb2e9542abfc489db011b429569
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2017-01-13 11:56:25 +0000

    update and format request activation link email

--- wp-content/themes/mojintranet/user.php
+++ wp-content/themes/mojintranet/user.php
@@ -57,6 +57,7 @@ class User extends MVC_controller {
         //send email to user
         $data = array(
           'name' => $display_name,
+          'site_url' => site_url(),
           'activation_url' => network_site_url("/user/auth/?key=".$key."&login=" . rawurlencode($email) . "&screen_name=" . $display_name . "&redirect_url=" . $redirect_url, 'login')
         );
 
