186a9569bf90661a292aeb2fbeba922f07c057ac
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2017-01-16 14:21:30 +0000

    update confirmation message and activation email content

--- wp-content/themes/mojintranet/views/email/activate_account.php
+++ wp-content/themes/mojintranet/views/email/activate_account.php
@@ -14,17 +14,34 @@
       </tr>
       <tr>
         <td>
-          <p style="padding: 20px; font-size: 16px; font-family: Arial, sans-serif;">Hello,</p>
-          <p style="padding: 20px; font-size: 16px; font-family: Arial, sans-serif;">Activation link:</p>
-          <p style="padding: 20px; font-size: 16px; font-family: Arial, sans-serif;">
-            <a href="<?=$activation_url?>"><?=$activation_url?></a>
+          <p style="padding: 5px 0; font-size: 19px; font-family: Arial, sans-serif;">Hello,</p>
+          <p style="padding: 5px 0; font-size: 19px; font-family: Arial, sans-serif;">Thank you for signing up to comment on the MoJ intranet. To begin commenting, click the button below.</p>
+          <p>
+             <a style="
+              position: relative;
+              display: inline-block;
+              padding: 8px 15px 5px;
+              background-color: #00823b;
+              box-shadow: 0 2px 0 #003518;
+              color: #ffffff;
+              cursor: pointer;
+              font-size: 19px;
+              line-height: 25px;
+              text-decoration: none;
+              vertical-align: top;
+              "
+              href="<?=$activation_url?>">Begin commenting</a>
           </p>
+          <p style="padding: 5px 0; font-size: 19px; font-family: Arial, sans-serif;">For security reasons this email will be active for 3 hours only.</p>
+          <p style="padding: 5px 0; font-size: 19px; font-family: Arial, sans-serif;">If you did not sign up to comment on the MoJ intranet, please ignore this email.</p>
+          <p style="padding: 5px 0; font-size: 19px; font-family: Arial, sans-serif;">The Intranet Team.</p>
+          <p style="padding: 5px 0; font-size: 19px; font-family: Arial, sans-serif;">This email is generated automatically. Do not reply.</p>
         </td>
       </tr>
       <tr>
         <td>
           <div style="background-color: #dee0e2">
-            <p style="padding: 20px; font-size: 16px; font-family: Arial, sans-serif;">
+            <p style="padding: 20px; font-size: 19px; font-family: Arial, sans-serif;">
               If you're unsure an email is from MoJ, forward it to <a href="mailto:phishing@digital.justice.gov.uk">phishing@digital.justice.gov.uk</a>.
             </p>
           </div>
--- wp-content/themes/mojintranet/views/pages/blog_post/partials/confirmation.php
+++ wp-content/themes/mojintranet/views/pages/blog_post/partials/confirmation.php
@@ -7,7 +7,7 @@
         <h1>Please check your email</h1>
         <p class="confirmation-message">We’re just emailing you a link so you can start commenting. This can take up to 5 minutes.</p>
         <p class="confirmation-message">When it arrives, click on the link (which is active for 3 hours). This will log you in to the intranet and enable you to add your comments.</p>
-        <p class="confirmation-message">Please check your inbox for this email from <a href="???@digital.justice.gov.uk">???@digital.justice.gov.uk</a>.</p>
+        <p class="confirmation-message">Please check your inbox for this email from <a href="newintranet@digital.justice.gov.uk">newintranet@digital.justice.gov.uk</a>.</p>
         <p class="confirmation-message">If you can’t find it, check your junk folder and add our address to your safe list.</p>
         <div class="back-link-box">
           <a class="back-link" href="">< Back to blog post</a>
