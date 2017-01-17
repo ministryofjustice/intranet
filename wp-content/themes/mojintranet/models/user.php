5636b671751d448d1210fa0e792a42180930af47
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2017-01-17 16:02:10 +0000

    update whitelist of email domains

--- wp-content/themes/mojintranet/models/user.php
+++ wp-content/themes/mojintranet/models/user.php
@@ -2,13 +2,36 @@
 
 class User_model extends MVC_model {
   private $valid_domains = array(
-    'publicguardian.gsi.gov.uk',
+    'cafcass.gsi.gov.uk',
+    'ccrc.x.gsi.gov.uk',
+    'cica.gsi.gov.uk',
+    'cjs.gsi.gov.uk',
+    'crowncommercial.gov.uk',
     'digital.justice.gov.uk',
-    'legalaid.gsi.gov.uk',
-    'justice.gsi.gov.uk',
-    'justice.gov.uk',
+    'hmcourts-service.gsi.gov.uk',
     'hmcts.gsi.gov.uk',
-    'noms.gsi.gov.uk'
+    'hmiprisons.gsi.gov.uk',
+    'hmiprobation.gsi.gov.uk',
+    'hmps.gsi.gov.uk',
+    'homeoffice.gsi.gov.uk',
+    'ips.gsi.gov.uk',
+    'jac.gsi.gov.uk',
+    'jaco.gsi.gov.uk',
+    'judiciary.gsi.gov.uk',
+    'justice.gov.uk',
+    'justice.gsi.gov.uk',
+    'lawcommission.gsi.gov.uk',
+    'legalaid.gsi.gov.uk',
+    'legalombudsman.org.uk',
+    'legalservicesboard.org.uk',
+    'noms.gsi.gov.uk',
+    'offsol.gsi.gov.uk',
+    'paroleboard.gsi.gov.uk',
+    'ppo.gsi.gov.uk',
+    'probation.gsi.gov.uk',
+    'publicguardian.gsi.gov.uk',
+    'sentencingcouncil.gsi.gov.uk',
+    'yjb.gsi.gov.uk'
   );
 
   /** creates a new user
