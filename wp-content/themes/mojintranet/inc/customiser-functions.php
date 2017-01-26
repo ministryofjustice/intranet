a99a883f647ba0b1c2fbb90a12a144e197402b96
Robert Lowe <robertlowe@admins-mbp-52.ws.local>
2017-01-26 14:10:52 +0000

    Customise message

--- wp-content/themes/mojintranet/inc/customiser-functions.php
+++ wp-content/themes/mojintranet/inc/customiser-functions.php
@@ -2,7 +2,7 @@
 
 function alter_customiser_info_text($translated, $original) {
   if($original == 'The Customizer allows you to preview changes to your site before publishing them. You can also navigate to different pages on your site to preview them.') {
-    return 'Here';
+    return 'The preview pane on this page won’t display any changes you make. To view your changes, wait 60 seconds after saving the page and then view the homepage on the live site. If the live page hasn’t updated after 60 seconds, contact newintranet@digital.justice.gov.uk.';
   }
   return $translated;
 }
