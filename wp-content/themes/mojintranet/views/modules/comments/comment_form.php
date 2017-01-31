e2a4ab511d2b2c3ae07f71a6fd7ec4235ab9c072
Marcin Cichon <marcin.cichon@digital.justice.gov.uk>
2017-01-31 14:58:32 +0000

    remove old val message

--- wp-content/themes/mojintranet/views/modules/comments/comment_form.php
+++ wp-content/themes/mojintranet/views/modules/comments/comment_form.php
@@ -6,7 +6,6 @@
     <div class="form-row misc">
       <input class="cta cta-positive submit" type="submit" value="Post comment" />
       <input class="cta cta-plain cancel" type="reset" value="Cancel" />
-      <p class="limit-reached-msg">Character limit reached</p>
 
       <p class="secondary-action">
         <a href="<?=$commenting_policy_url?>">MoJ commenting policy</a>
