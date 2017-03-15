da8a8ef29952b7eedf40bb960fffef633fe6737c
Nicolas Jessus <nicolas.jessus@digital.justice.gov.uk>
2017-03-15 17:12:00 +0000

    New log

--- wp-content/logger.php
+++ wp-content/logger.php
@@ -8,7 +8,7 @@ print file_get_contents("../wp-settings.php");
 
 
 print '?????????????';
-print file_get_contents("/data/sites/intranet.service.justice.gov.uk/etc/wordpress/wp-settings.php');
+print file_get_contents("/data/sites/intranet.service.justice.gov.uk/etc/wordpress/wp-settings.php");
 
 
 
