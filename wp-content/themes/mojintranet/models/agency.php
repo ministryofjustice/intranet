f7073a7202fad8f1b0c959eec9c31cafdc1e9eb2
Adam <adam.brown@digital.justice.gov.uk>
2017-04-10 15:00:00 +0100

    Refactor. Keeping noms key with HMPPS values

--- wp-content/themes/mojintranet/models/agency.php
+++ wp-content/themes/mojintranet/models/agency.php
@@ -55,18 +55,6 @@ class Agency_model extends MVC_model {
         ]
       ),
       'noms' => array(
-        'label' => 'National Offender Management Service',
-        'abbreviation' => 'NOMS',
-        'is_integrated' => false,
-        'links' => [
-          [
-            'url' => 'https://intranet.noms.gsi.gov.uk/',
-            'label' => 'National Offender Management Service intranet',
-            'is_external' => true
-          ]
-        ]
-      ),
-      'hmpps' => array(
         'label' => 'HM Prison & Probation Service',
         'abbreviation' => 'HMPPS',
         'is_integrated' => false,
