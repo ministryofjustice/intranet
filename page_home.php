5cd45be054ff775b03ee37ad61b06b29c836c316
Alex Foxleigh <alex@volcaniccreations.com>
2017-05-23 12:47:16 +0100

    added emergency banner and also put a placeholder image into the full width banner

--- page_home.php
+++ page_home.php
@@ -5,6 +5,7 @@ error_reporting(0);
 //ToDo: Change name to home.php when database changed ?>
 <?php get_component('c-global-header'); ?>
   <div id="maincontent" class="u-wrapper l-main">
+    <?php get_component('c-emergency-banner'); ?>
     <?php get_component('c-full-width-banner'); ?>
     <h1 class="o-title o-title--page">Ministry of Justice HQ</h1>
     <?php get_component('c-home-page-primary'); ?>
---
+++ src/components/c-emergency-banner/view.php
@@ -0,0 +1,11 @@
+<!-- c-emergency-banner starts here -->
+<section class="c-emergency-banner c-emergency-banner--emergency">
+  <div class="c-emergency-banner__meta">
+    <h1>Example headline</h1>
+    <time datetime="2016-12-22">22nd December 2016</time>
+  </div>
+  <div class="c-emergency-banner__content">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates beatae impedit, commodi laboriosam, tempora similique cupiditate facilis, reiciendis saepe dolor sint? Iste dolore soluta autem dolor. Eum, velit magni nostrum.</p>
+  </div>
+</section>
+<!-- c-emergency-banner ends here -->
