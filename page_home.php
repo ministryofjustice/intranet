68368a017dae520ffa7dfe2c56b4546cbc8241d5
Alex Foxleigh <alex@volcaniccreations.com>
2017-05-23 10:29:01 +0100

    added the secondary component

--- page_home.php
+++ page_home.php
@@ -6,16 +6,7 @@ error_reporting(0);
 <?php get_component('c-global-header'); ?>
   <div id="maincontent" class="u-wrapper l-main">
     <h1 class="o-title o-title--page">Ministry of Justice HQ</h1>
-    <div class="l-primary" role="main">
-      <?php get_component('c-news-container'); ?>
-      <?php get_component('c-need-to-know'); ?>
-      <?php get_component('c-events-container'); ?>
-    </div>
-    <div class="l-secondary" role="complementary">
-      <?php get_component('c-my-moj'); ?>
-      <?php get_component('c-quick-links'); ?>
-      <?php get_component('c-blog-summary'); ?>
-      <?php get_component('c-social-links'); ?>
-    </div>
+    <?php get_component('c-home-page-primary'); ?>
+    <?php get_component('c-home-page-secondary'); ?>
   </div>
 <?php get_component('c-global-footer'); ?>
