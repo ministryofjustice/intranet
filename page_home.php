f82ba74eb65e102f3ce4ab1a69e03db9c8185f9a
Irune Itoiz <irune.itoiz@gmail.com>
2017-05-29 10:46:25 +0100

    Added agency labels to header and sections
    Added dynamic labels to homepage header and sidebar My $agency

--- page_home.php
+++ page_home.php
@@ -2,12 +2,18 @@
 // Turned off error reporting for now.
 error_reporting(0);
 
+use MOJ\Intranet\Agency;
+$oAgency = new Agency();
+$activeAgency = $oAgency->getCurrentAgency();
+
+
+
 //ToDo: Change name to home.php when database changed ?>
 <?php get_component('c-global-header'); ?>
   <div id="maincontent" class="u-wrapper l-main">
     <?php get_component('c-emergency-banner'); ?>
     <?php get_component('c-full-width-banner'); ?>
-    <h1 class="o-title o-title--page">Ministry of Justice HQ</h1>
+    <h1 class="o-title o-title--page"><?php echo $activeAgency['label'];?></h1>
     <?php get_component('c-home-page-primary'); ?>
     <?php get_component('c-home-page-secondary'); ?>
   </div>
