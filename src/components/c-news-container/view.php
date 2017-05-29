2512c5c86de518555cbadca405f8a5a0b8589d75
Irune Itoiz <irune.itoiz@gmail.com>
2017-05-29 11:39:36 +0100

    MI-245 Added link to all news
    hardcoded to the news archive section as not dynamic

--- src/components/c-news-container/view.php
+++ src/components/c-news-container/view.php
@@ -3,5 +3,5 @@
   <?php get_component('c-featured-news-list'); ?>
   <h1 class="o-title o-title--section">News</h1>
   <?php get_component('c-news-list'); ?>
-  <a href="" class="o-see-all-link">See all news</a>
+  <a href="/newspage/" class="o-see-all-link">See all news</a>
 </section>
