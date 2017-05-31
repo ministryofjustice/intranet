27b8c26c8f74ac82124441aaab05a5b928f705b7
Irune Itoiz <irune.itoiz@gmail.com>
2017-05-31 15:28:00 +0100

    MI-406 Sidebar banner data
    using the full width banner component

--- src/components/c-home-page-secondary/view.php
+++ src/components/c-home-page-secondary/view.php
@@ -1,6 +1,13 @@
+<?php
+use MOJ\Intranet\HomepageBanners;
+$sidebarBanner = HomepageBanners::getSidebarBanner(get_intranet_code());
+?>
 <!-- c-home-page-secondary starts here -->
 <section class="c-home-page-secondary l-secondary" role="complementary">
   <?php get_component('c-my-moj'); ?>
+  <?php if ($sidebarBanner && $sidebarBanner['visible']) { ?>
+      <?php get_component('c-full-width-banner', $sidebarBanner); ?>
+  <?php } ?>
   <?php get_component('c-quick-links'); ?>
   <?php get_component('c-blog-summary'); ?>
   <?php get_component('c-social-links'); ?>
