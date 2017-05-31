8556455588834d8b23a65b9f4fd86808e22a7816
Irune Itoiz <irune.itoiz@gmail.com>
2017-05-31 15:21:24 +0100

    Moved data retrieval to the wrapping component
    so it’s possible to reuse the banners on other points of the template.

--- page_home.php
+++ page_home.php
@@ -3,16 +3,25 @@
 error_reporting(0);
 
 use MOJ\Intranet\Agency;
+use MOJ\Intranet\HomepageBanners;
+
 $oAgency = new Agency();
 $activeAgency = $oAgency->getCurrentAgency();
 
+$emergencyBanner = HomepageBanners::getEmergencyBanner(get_intranet_code());
+$fullWidthTopBanner = HomepageBanners::getTopBanner(get_intranet_code());
 
-
+print_r($fullWidthTopBanner);
 //ToDo: Change name to home.php when database changed ?>
 <?php get_component('c-global-header'); ?>
   <div id="maincontent" class="u-wrapper l-main">
-    <?php get_component('c-emergency-banner'); ?>
-    <?php get_component('c-full-width-banner'); ?>
+<?php
+if ($emergencyBanner && $emergencyBanner['visible']) { ?>
+    <?php get_component('c-emergency-banner', $emergencyBanner); ?>
+<?php } ?>
+<?php if ($fullWidthTopBanner && $fullWidthTopBanner['visible']) { ?>
+    <?php get_component('c-full-width-banner', $fullWidthTopBanner); ?>
+<?php } ?>
     <h1 class="o-title o-title--page"><?php echo $activeAgency['label'];?></h1>
     <?php get_component('c-home-page-primary'); ?>
     <?php get_component('c-home-page-secondary'); ?>
--- src/components/c-full-width-banner/view.php
+++ src/components/c-full-width-banner/view.php
@@ -1,15 +1,7 @@
-<?php
-use MOJ\Intranet\HomepageBanners;
-
-$homepageTopBanner = HomepageBanners::getTopBanner(get_intranet_code());
-
-if ($homepageTopBanner && $homepageTopBanner['visible']) { ?>
-
 <!-- c-full-width-banner starts here -->
 <section class="c-full-width-banner">
-  <a href="<?php echo $homepageTopBanner['url'];?>">
-      <img src="<?php echo $homepageTopBanner['image_url'];?>" alt="<?php echo $homepageTopBanner['alt'];?>">
+  <a href="<?php echo $data['url'];?>">
+      <img src="<?php echo $data['image_url'];?>" alt="<?php echo $data['alt'];?>">
   </a>
 </section>
 <!-- c-full-width-banner ends here -->
-<?php }
