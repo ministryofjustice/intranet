de66112ea95b734fca2bb7a34f57658516a517d2
Irune Itoiz <irune.itoiz@gmail.com>
2017-05-31 16:56:38 +0100

    MI-408-Social links display
    coming from the agency class instead of the follow_us module for consolidation

--- src/components/c-social-links/view.php
+++ src/components/c-social-links/view.php
@@ -1,7 +1,18 @@
-<section class="c-social-links">
-  <h1 class="o-title o-title--section">Follow us</h1>
-  <ul>
-    <li class="u-icon u-icon--twitter"><a href="">MoJ on Twitter</a></li>
-    <li class="u-icon u-icon--yammer"><a href="">MoJ on Yammer</a></li>
-  </ul>
-</section>
+<?php
+use MOJ\Intranet\Agency;
+
+$socialLinks = Agency::getSocialLinks(get_intranet_code());
+
+if (!empty($socialLinks)) {
+?>
+    <section class="c-social-links">
+        <h1 class="o-title o-title--section">Follow us</h1>
+        <ul>
+<?php foreach ($socialLinks as $link) { ?>
+            <li class="u-icon u-icon--<?php echo $link['name'];?>"><a href="<?php echo $link['url'];?>"><?php echo $link['label'];?></a></li>
+<?php } ?>
+        </ul>
+    </section>
+<?php }
+?>
+
