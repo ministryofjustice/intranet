dc756f7f2c4a2cbe7dc55510d512e5a24588cdd0
Alex Foxleigh <alex@volcaniccreations.com>
2017-06-12 16:03:38 +0100

    Created C news article and working on creating the news article page

--- page_news.php
+++ page_news.php
@@ -5,14 +5,6 @@ Template Name: Clarity - News
 ?>
 <?php get_component('c-global-header'); ?>
   <div id="maincontent" class="u-wrapper l-main" role="main">
-    <h1><a href="">News article title</a></h1>
-    <?php get_component('c-article-byline'); ?>
-    <?php get_component('c-article-excerpt'); ?>
-    <section class="l-secondary">
-      <?php get_component('c-article-featured-image'); ?>
-    </section>
-    <section class="l-primary">
-      <?php get_component('c-rich-text-block'); ?>
-    </section>
+    <?php get_component('c-news-article'); ?>
   </div>
 <?php get_component('c-global-footer'); ?>
