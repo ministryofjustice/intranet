6e690917f71cd6b29c3e3dfdce05eb29dd2e0487
Alex Foxleigh <alex@volcaniccreations.com>
2017-06-16 16:24:41 +0100

    build templates and related components for MI-9 and MI-261

---
+++ src/components/c-guidance/view.php
@@ -0,0 +1,11 @@
+<!-- c-guidance starts here -->
+<section class="c-guidance">
+  <h1 class="o-title o-title--page">Guidance and Forms</h1>
+  <p>Find information on policies and processes. All forms and templates are found on their relevant guidance pages.</p>
+  <div class="c-guidance__section">
+    <h2>All</h2>
+    <?php get_component('c-headed-link-list'); ?>
+  </div>
+
+</section>
+<!-- c-guidance ends here -->
