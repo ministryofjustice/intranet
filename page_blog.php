1e957568b4448c01715fa29a6c0682f3557ad551
Alex Foxleigh <alex@volcaniccreations.com>
2017-06-20 16:32:16 +0100

    Added structure for blog page. still need to style share components and add commenting - which is part of MI-10

---
+++ page_blog.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Template Name: Clarity - Blog
+*/
+?>
+<?php get_component('c-global-header'); ?>
+  <div id="maincontent" class="u-wrapper l-main" role="main">
+    <?php get_component('c-blog-article'); ?>
+  </div>
+<?php get_component('c-global-footer'); ?>
---
+++ src/components/c-blog-article/view.php
@@ -0,0 +1,9 @@
+<!-- c-blog-article starts here -->
+<article class="c-blog-article">
+  <h1 class="o-title o-title--headline">Blog article title</h1>
+  <?php get_component('c-article-byline'); ?>
+  <?php get_component('c-article-excerpt'); ?>
+  <?php get_component('c-rich-text-block'); ?>
+  <?php get_component('c-share-post'); ?>
+</article>
+<!-- c-blog-article ends here -->
