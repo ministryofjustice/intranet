0a5601a3be4a039e716ef171b129210366a32460
Adam <adam.brown@digital.justice.gov.uk>
2017-07-21 14:55:02 +0100

    Fixed photo gallery resizing. Added in JS and CSS from Magnific Popups. This gave us a lot better cross browser support. Still using my old popup for the videos until this can be refactored into the new popup code.

--- wp-content/themes/mojintranet/views/modules/body_bottom.php
+++ wp-content/themes/mojintranet/views/modules/body_bottom.php
@@ -15,3 +15,5 @@
 <script type="text/javascript" src="<?=get_template_directory_uri()?>/assets/js/moment.min.js"></script>
 
 <script type="text/javascript" src="<?=get_template_directory_uri()?>/assets/js/main.js?<?=add_checksum_param('js/main.js')?>"></script>
+
+<script type="text/javascript" src="<?=get_template_directory_uri()?>/assets/js/jquery.magnific-popup.js?<?=add_checksum_param('js/jquery.magnific-popup.js')?>"></script>
--- wp-content/themes/mojintranet/views/pages/media_grid/main.php
+++ wp-content/themes/mojintranet/views/pages/media_grid/main.php
@@ -60,7 +60,28 @@
         </div>
       </div>
 
-  <?php $this->view('pages/media_grid/lightbox') ?>
   <?php $this->view('modules/side_navigation') ?>
-
+  <?php $this->view('pages/media_grid/lightbox') ?>
+  
 </div>
+<script>
+// From jQuery object
+$(document).ready(function() {
+
+	$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: true,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+});
+</script>
