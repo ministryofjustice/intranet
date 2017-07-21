3b0860828851d929414b472b7ba47dae6e11a1e7
Adam <adam.brown@digital.justice.gov.uk>
2017-07-21 11:06:11 +0100

    Fixes to image resizing

--- wp-content/themes/mojintranet/views/pages/media_grid/lightbox.php
+++ wp-content/themes/mojintranet/views/pages/media_grid/lightbox.php
@@ -13,10 +13,9 @@
 <div class="lightbox-image-container">
  <div class="grid content-container lightbox-attr">
    <div class="btn-close" id="button"></div>
-   <div class="col-lg-12 col-md-12 col-sm-12 lightbox-center">
-
-     <img class="lightbox-img" alt="" data-state="">
-     <div class="lightbox-img_caption"><h4></h4></div>
+   <div class="grid lightbox-center">
+     <div class="col-lg-12 col-md-12 col-sm-12"><img class="lightbox-img" alt="" data-state=""></div>
+     <div class="col-lg-12 col-md-12 col-sm-12"><div class="lightbox-img_caption"><h4></h4></div></div>
    </div>
  </div>
 </div>
