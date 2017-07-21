9cd6ad29805b96da507c46e1c509e7db966936f6
Adam <adam.brown@digital.justice.gov.uk>
2017-07-21 14:55:02 +0100

    Fixed photo gallery resizing. Added in JS and CSS from Magnific Popups. This gave us a lot better cross browser support. Still using my old popup for the videos until this can be refactored into the new popup code.

--- wp-content/themes/mojintranet/views/pages/media_grid/photo_gallery.php
+++ wp-content/themes/mojintranet/views/pages/media_grid/photo_gallery.php
@@ -10,12 +10,32 @@
   $photo_gallery = get_sub_field('photo_gallery');
   if ($photo_gallery):
     foreach ($photo_gallery as $gallery): ?>
-    <div class="col-lg-2 col-md-3 col-sm-12 thumbnail" data-state="">
-      <a href="<?php echo $gallery['sizes']['large']; ?>">
-        <img src="<?php echo $gallery['sizes']['thumbnail']; ?>" alt="" title="<?php echo $gallery['caption']; ?>"/>
+    <div class="col-lg-2 col-md-3 col-sm-12">
+      <a class="image-popup-no-margins" href="<?php echo $gallery['sizes']['large']; ?>" title="<?php echo $gallery['caption']; ?>">
+      <img src="<?php echo $gallery['sizes']['thumbnail']; ?>" alt="" title="<?php echo $gallery['caption']; ?>"/>
       </a>
     </div>
-
   <?php endforeach; ?>
 <?php endif; ?>
 </div>
+<script>
+// This will eventually need to be refactored into the appropreate js area. Best to do when incorporating the video popup.
+$(document).ready(function() {
+
+	$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: true,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+});
+</script>
