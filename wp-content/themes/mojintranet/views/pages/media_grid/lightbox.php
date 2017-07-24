080654b4f3babd9f1fb5ebb7e01fc4a6c38e2d6a
Adam <adam.brown@digital.justice.gov.uk>
2017-07-24 13:01:02 +0100

    Resored css for video lightbox

--- wp-content/themes/mojintranet/views/pages/media_grid/lightbox.php
+++ wp-content/themes/mojintranet/views/pages/media_grid/lightbox.php
@@ -3,18 +3,16 @@
  <div class="grid content-container lightbox-attr">
    <div class="btn-close" id="button"></div>
    <div class="col-lg-12 col-md-12 col-sm-12 lightbox-center">
-
      <div class="lightbox-video"></div>
-
    </div>
  </div>
 </div>
 
-<div class="lightbox-image-container">
+<div class="lightbox-image-container" data-state="hidden">
  <div class="grid content-container lightbox-attr">
    <div class="btn-close" id="button"></div>
    <div class="grid lightbox-center">
-     <div class="col-lg-12 col-md-12 col-sm-12"><img class="lightbox-img" alt="" data-state=""></div>
+     <div class="col-lg-12 col-md-12 col-sm-12"><img class="lightbox-img"></div>
      <div class="col-lg-12 col-md-12 col-sm-12"><div class="lightbox-img_caption"><h4></h4></div></div>
    </div>
  </div>
