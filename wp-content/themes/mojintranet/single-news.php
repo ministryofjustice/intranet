757debb9b5e571f640c053ae4f322bab6bbd430c
Adam <adam.brown@digital.justice.gov.uk>
2017-09-18 16:05:45 +0100

    Comment functionality added to all news posts and supporting backend editor checkboxes.

--- wp-content/themes/mojintranet/single-news.php
+++ wp-content/themes/mojintranet/single-news.php
@@ -9,18 +9,23 @@ class Single_news extends MVC_controller {
   }
 
   function get_data(){
+    global $post;
     $article_date = get_the_date();
 
     ob_start();
     the_content();
     $content = ob_get_clean();
-
     $this_id = get_the_ID();
 
     $thumbnail_id = get_post_thumbnail_id($this_id);
     $thumbnail = wp_get_attachment_image_src($thumbnail_id, 'intranet-large');
     $alt_text = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
     $authors = dw_get_author_info($this_id);
+
+    $this->add_global_view_var('commenting_policy_url', site_url('/commenting-policy/'));
+    $this->add_global_view_var('comments_open', (boolean) comments_open($this_id));
+    $this->add_global_view_var('comments_on', (boolean) get_post_meta($this_id, 'dw_comments_on', true));
+    $this->add_global_view_var('logout_url', wp_logout_url($_SERVER['REQUEST_URI']));
 
     return array(
       'page' => 'pages/news_single/main',
@@ -39,10 +44,15 @@ class Single_news extends MVC_controller {
         'content' => $content,
         'raw_date' => $article_date,
         'human_date' => date("j F Y", strtotime($article_date)),
+        'share_email_body' => "Hi there,\n\nI thought you might be interested in this news post I've found on the MoJ intranet:\n",
+        'likes_count' => $likes['count'],
         'election_banner' => array(
           'visible' => strtotime($article_date) < strtotime('9 May 2015')?1:0
         )
       )
     );
+  }
+  private function get_likes_from_api($post_id) {
+    return $this->model->likes->read('post', $post_id);
   }
 }
--- wp-content/themes/mojintranet/views/pages/news_single/main.php
+++ wp-content/themes/mojintranet/views/pages/news_single/main.php
@@ -48,5 +48,42 @@
         <?=$content?>
       </div>
     </div>
+
+    <div class="grid">
+      <div class="col-lg-8 col-md-12 col-sm-12">
+        <ul class="social-actions post-social-actions">
+          <?php if ($comments_on): ?>
+            <li class="comments-count">
+              <span class="icon"></span>
+              <span class="count"></span>
+            </li>
+          <?php endif ?>
+
+          <li class="like-container" data-likes-count="<?=$likes_count?>" data-post-type="post" data-post-id="<?=$id?>">
+            <a class="like-link" href="#">
+              <span class="like-icon icon"></span>
+              <span class="like-description"></span>
+            </a>
+          </li>
+
+          <?php if ($comments_on): ?>
+            <li class="comments-count">
+              <span class="icon"></span>
+              <span class="count"></span>
+            </li>
+          <?php endif ?>
+
+
+          <li class="share-container">
+            <span class="share-via-email-icon"></span>
+            <a class="share-via-email"
+               href="mailto:"
+               data-title="<?=htmlspecialchars($title)?>"
+               data-date="<?=htmlspecialchars($human_date)?>"
+               data-body="<?=htmlspecialchars($share_email_body)?>">Share this post by email</a>
+          </li>
+      </div>
+    </div>
+    <?php $this->view('modules/comments/main') ?>
   </div>
 </div>
