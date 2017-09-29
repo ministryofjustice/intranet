e1228640b50cd163a787a1751e54933d95950246
Alex Foxleigh <alex@volcaniccreations.com>
2017-09-29 14:48:13 +0100

    added IE fix for article byline

--- page_blog.php
+++ page_blog.php
@@ -4,13 +4,7 @@ Template Name: Clarity - Blog
 */
 ?>
 <?php get_component('c-global-header'); ?>
-  <div id="maincontent" class="u-wrapper l-main t-blog-article" role="main">
-    <h1 class="o-title o-title--headline">Blog article title</h1>
-    <?php get_component('c-article-byline'); ?>
-    <?php get_component('c-article-excerpt'); ?>
-    <?php get_component('c-rich-text-block'); ?>
-    <?php get_component('c-share-post'); ?>
-    <?php get_component('c-comment-form'); ?>
-    <?php get_component('c-comments'); ?>
-  </div>
+<div id="maincontent" class="u-wrapper l-main t-blog-article" role="main">
+  <?php get_component('c-article'); ?>
+</div>
 <?php get_component('c-global-footer'); ?>
