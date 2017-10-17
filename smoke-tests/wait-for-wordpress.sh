baf402011e7d84d67e5443a71339044d0df03fce
Jesus Laiz <zheileman@users.noreply.github.com>
2017-10-17 15:53:13 +0100

    Forcing a change to trigger CI build, maybe?

--- smoke-tests/wait-for-wordpress.sh
+++ smoke-tests/wait-for-wordpress.sh
@@ -9,6 +9,6 @@ while ! curl -I -s ${wordpress_host} | grep -q 'HTTP/1.1 200 OK'; do
   sleep 10
 done
 
->&2 echo "Wordpress is up - executing command"
+>&2 echo "Wordpress is up - executing command..."
 
 exec ${command_to_run}
