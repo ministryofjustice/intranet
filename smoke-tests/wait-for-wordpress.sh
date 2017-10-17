e68f66221c9b160d0244c8796ed866c412b68931
Jesus Laiz <zheileman@users.noreply.github.com>
2017-10-17 15:27:02 +0100

    WIP: Enabling travis integration

---
+++ smoke-tests/wait-for-wordpress.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+wordpress_host=$1
+command_to_run=$2
+
+while ! curl -I -s ${wordpress_host} | grep -q 'HTTP/1.1 200 OK'; do
+  >&2 echo "Wordpress is unavailable - sleeping..."
+  sleep 10
+done
+
+>&2 echo "Wordpress is up - executing command"
+
+exec ${command_to_run}
