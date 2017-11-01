f676681606a405577b0858305d1a2067b046471c
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2017-11-01 14:30:21 +0000

    converted content block, now pulling in the data

--- page_campaign.php
+++ page_campaign.php
@@ -8,8 +8,12 @@
   <div id="maincontent" class="u-wrapper l-main t-campaign">
     <h1 class="o-title o-title--page"><?php the_title(); ?></h1>
     <div class="l-full-page" role="main">
-      <?php get_component('c-full-width-banner'); ?>
-      <?php get_component('c-rich-text-block'); ?>
+        <?php get_component('c-full-width-banner'); ?>
+        <?php
+            while ( have_posts() ) : the_post();
+                get_template_part( 'src/components/c-rich-text-block/view' );
+            endwhile; // End of the loop.
+        ?>
     </div>
   </div>
 <?php get_component('c-global-footer'); ?>
