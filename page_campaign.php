dd4b69b22b9ead1bde172ea58f6942f1063fb28c
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2017-11-01 14:22:57 +0000

    test push - banner converted

---
+++ page_campaign.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template name: CC Campaign Page
+ */
+?>
+
+<?php get_component('c-global-header'); ?>
+  <div id="maincontent" class="u-wrapper l-main t-campaign">
+    <h1 class="o-title o-title--page">Page Title</h1>
+    <div class="l-full-page" role="main">
+      <?php get_component('c-full-width-banner'); ?>
+      <?php get_component('c-rich-text-block'); ?>
+    </div>
+  </div>
+<?php get_component('c-global-footer'); ?>
--- src/components/c-full-width-banner/view.php
+++ src/components/c-full-width-banner/view.php
@@ -1,7 +1,8 @@
 <!-- c-full-width-banner starts here -->
 <section class="c-full-width-banner">
-  <a href="<?php echo $data['url'];?>">
-      <img src="<?php echo $data['image_url'];?>" alt="<?php echo $data['alt'];?>">
+  <a href="<?php the_field('dw_banner_url')?>">
+    <?php $image = wp_get_attachment_image_src(get_field('dw_page_banner'), 'full'); ?>
+    <img src="<?php echo $image[0]; ?>" alt="" />
   </a>
 </section>
 <!-- c-full-width-banner ends here -->
