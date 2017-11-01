66a5600939f1bcbea18169923091105b6307ec54
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2017-11-01 14:24:54 +0000

    replaced placholder text to wp_title

--- page_campaign.php
+++ page_campaign.php
@@ -6,7 +6,7 @@
 
 <?php get_component('c-global-header'); ?>
   <div id="maincontent" class="u-wrapper l-main t-campaign">
-    <h1 class="o-title o-title--page">Page Title</h1>
+    <h1 class="o-title o-title--page"><?php the_title(); ?></h1>
     <div class="l-full-page" role="main">
       <?php get_component('c-full-width-banner'); ?>
       <?php get_component('c-rich-text-block'); ?>
