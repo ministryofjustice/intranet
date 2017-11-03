370c15d2626f89317e9ad0811b36e6b6f10718d2
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2017-11-03 10:05:19 +0000

    made campaign colours style as a componet -> dynamic style

--- page_campaign.php
+++ page_campaign.php
@@ -4,31 +4,11 @@
  */
 ?>
 <?php
-    $campaign_colour = get_field('dw_campaign_colour');
     $lhs_menu_on = get_field('dw_lhs_menu_on') == true;
 ?>
-<style>
-.page-template-page_campaign .l-main .c-rich-text-block h2 strong,
-.page-template-page_campaign .l-main .c-rich-text-block h3 strong,
-.page-template-page_campaign .l-main .c-rich-text-block h4 strong,
-.page-template-page_campaign .l-main .c-rich-text-block h5 strong,
-.page-template-page_campaign .l-main .c-rich-text-block h6 strong {
-  color: <?=$campaign_colour?>;
-}
-
-.page-template-page_campaign .l-main .c-rich-text-block hr {
-  display: inline-block;
-  width: 100%;
-  margin: 10px 0 0;
-  border: 1px solid <?=$campaign_colour?>;
-}
-
-.l-main .c-rich-text-block .example {
-  border-left-color: <?=$campaign_colour?>;
-}
-</style>
 
 <?php get_component('c-global-header'); ?>
+<?php get_component('c-dynamic-style'); ?>
 <div class="l-main u-wrapper">
 
     <?php if($lhs_menu_on == true): ?>
