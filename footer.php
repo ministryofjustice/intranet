03d6dcafe0323f237496328648331f8d558fde03
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2017-11-09 09:37:16 +0000

    created a wp footer file which is needed for the theme

---
+++ footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ */
+
+?>
+
+<footer class="c-footer">
+  <div class="u-wrapper">
+    <div class="l-primary">
+      <?php get_component('c-feedback-container'); ?>
+      <?php get_component('c-footer-nav'); ?>
+    </div>
+    <div class="l-secondary">
+      <?php get_component('c-copyright-notice'); ?>
+  </div>
+</footer>
+<script src="<?php echo get_assets_folder(); ?>/js/core.min.js" charset="utf-8"></script>
+
+<?php
+  /**
+   * wp_footer() required WP function do not remove. Used by plugins to hook into and for theme development.
+   */
+  wp_footer();
+?>
+
+</body>
+</html>
+
