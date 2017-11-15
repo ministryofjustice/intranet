60fb6cc1ece0fc78aa3011849da3acf48f1a6536
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2017-11-15 16:50:04 +0000

    updated front page, now displaying featured, news, events, and sidebar

--- src/components/c-news-widget/view.php
+++ src/components/c-news-widget/view.php
@@ -1,7 +1,7 @@
 <section class="c-news-widget">
-  <h1 class="o-title o-title--section">Featured News</h1>
+  <h1 class="o-title o-title--section">Featured</h1>
   <?php get_component('c-featured-news-list'); ?>
   <h1 class="o-title o-title--section">News</h1>
   <?php get_component('c-news-list'); ?>
-  <a href="/newspage/" class="o-see-all-link">See all news</a>
+  <a href="<?php the_permalink( 129 ) ?>" class="o-see-all-link">See all news</a>
 </section>
