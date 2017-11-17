8b69df795d09734cfc360038b28482a5c7e7c45c
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2017-11-17 11:46:14 +0000

    assigned variable so it's more descriptive to let future/other developers know

--- src/components/c-news-widget/view.php
+++ src/components/c-news-widget/view.php
@@ -3,5 +3,6 @@
   <?php get_component('c-featured-news-list'); ?>
   <h1 class="o-title o-title--section">News</h1>
   <?php get_component('c-news-list'); ?>
-  <a href="<?php the_permalink( 129 ) ?>" class="o-see-all-link">See all news</a>
+  <?php $newspage_link = 129; ?>
+  <a href="<?php the_permalink( $newspage_link ) ?>" class="o-see-all-link">See all news</a>
 </section>
