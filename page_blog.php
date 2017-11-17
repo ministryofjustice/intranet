4a0a726dfd7aa221a1629a72697c6bb947db1180
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2017-11-17 17:04:25 +0000

    updated blog feed to use standard wordpress functions.

--- page_blog.php
+++ page_blog.php
@@ -5,6 +5,22 @@
  * 
  **/
 get_header();
+
+$year = 2017;
+$month = 04;
+
+$args = array(
+    'max_num_pages' => 5,
+    'posts_per_page' => 5,
+    'post_type' => 'post',
+    'date_query' => array(
+        array(
+            'year'  => $year,
+            'month' => $month,
+        ),
+    ),
+);
+$query = new WP_Query( $args );
 ?>
 
 <div class="l-main u-wrapper">
@@ -16,7 +32,23 @@ get_header();
     </div>  
     <div class="l-primary">
         <div id="maincontent" class="u-wrapper l-main t-campaign">
-            <?php get_component('c-blog-feed'); ?>
+            <section class="c-blog-feed">
+                <h1 class="o-title o-title--section">Latest</h1>
+                <div>
+                <?php
+                    
+                    if ( $query->have_posts() ) {
+                        while ( $query->have_posts() ) {
+                    
+                            $query->the_post();
+                            
+                            get_component('c-article-item');
+                        }
+                    }
+                wp_reset_postdata();
+                ?>
+                </div>
+            </section>
         </div>
     </div>
 </div>
--- src/components/c-blog-feed/view.php
+++ src/components/c-blog-feed/view.php
@@ -1,27 +1,55 @@
-<?php
-use MOJ\Intranet\Posts;
-
-$oPosts = new Posts();
-
-//Todo: Pass it as part of $data from the container
-$options = array (
-    'page' => 1,
-    'per_page' => 5,
-);
-
-$postsList = $oPosts->getPosts($options, true);
-
-if (!empty($postsList)) {
+<?php 
+    $args = array(
+        'max_num_pages' => 5,
+        'posts_per_page' => 5,
+        'post_type' => 'post',
+        'date_query' => array(
+            array(
+                'year'  => 2017,
+                'month' => 05,
+            ),
+        ),
+    );
+    $query = new WP_Query( $args );
 ?>
     <section class="c-blog-feed">
         <h1 class="o-title o-title--section">Latest</h1>
         <div>
-          <?php foreach ($postsList['results'] as $result) {
-            get_component('c-article-item', $result, 'blog');
-          } ?>
+        <?php
+            if ( $query->have_posts() ) {
+                while ( $query->have_posts() ) {
+            
+                    $query->the_post();
+                    
+                    ?>
+                    <article class="c-article-item js-article-item">
+                        <img src="<?php echo $thumbnail_url;?>" alt="<?php echo $alt_text;?>">
+                        <h1><a href="<?php echo get_the_permalink($id);?>"><?php echo get_the_title($id);?></a></h1>
+                        <?php
+                        // If the 'show_excerpt' value has been passed to $config: Display the excerpt.
+                        if ($config === 'show_excerpt') { ?>
+                            <?php get_component('c-article-excerpt'); ?>
+                        <?php } ?>
+                        <span class="c-article-item__dateline"><?php echo get_the_time('j M Y', $id);?></span>
+
+                        <?php
+                        // If the 'blog' value has been passed to $config: Display the byline.
+                        if ($config === 'blog') {
+                        ?>
+                            <span class="c-article-item__byline"><?php echo $authors[0]['name'];?></span>
+                        <?php } ?>
+                    </article>
+                    <?php
+                    
+                        //get_component('c-article-item','blog');
+                  
+            
+                }
+            
+            }
+        wp_reset_postdata();
+        ?>
         </div>
     </section>
-<?php
-}
-?>
+         
 
