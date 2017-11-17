07d1f2ecc4a61eda6124d92edb39ea06b0f0d484
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2017-11-17 12:01:07 +0000

    moved variable to the top to make the variable easier to find.

--- src/components/c-news-widget/view.php
+++ src/components/c-news-widget/view.php
@@ -1,8 +1,8 @@
+<?php $newspage_link = 129; ?>
 <section class="c-news-widget">
   <h1 class="o-title o-title--section">Featured</h1>
   <?php get_component('c-featured-news-list'); ?>
   <h1 class="o-title o-title--section">News</h1>
   <?php get_component('c-news-list'); ?>
-  <?php $newspage_link = 129; ?>
   <a href="<?php the_permalink( $newspage_link ) ?>" class="o-see-all-link">See all news</a>
 </section>
