2916881811fa4060d71994143b3f8e4c5f8cc233
Todd Tyree <tatyree@gmail.com>
2017-11-28 11:45:36 +0000

    Removing the method breaks admin login
    Not sure where or why, so the fastest solution was to put it back and
    change the filter such that it will permit anything down to 1x1.

--- wp-content/themes/mojintranet/inc/images.php
+++ wp-content/themes/mojintranet/inc/images.php
@@ -15,10 +15,29 @@ add_action( 'admin_init', 'dw_force_image_dimensions' );
 
 function dw_force_image_dimensions() {
   if(!current_user_can( 'administrator')) {
-    add_filter( 'wp_handle_upload_prefilter' );
+    add_filter( 'wp_handle_upload_prefilter', 'dw_block_small_images_upload' );
   }
 }
 
+function dw_block_small_images_upload( $file ) {
+  // Mime type with dimensions, check to exit earlier
+  $mimes = array( 'image/jpeg', 'image/png', 'image/gif' );
+
+  if( !in_array( $file['type'], $mimes ) ) {
+    return $file;
+  }
+
+  $img = getimagesize( $file['tmp_name'] );
+  $minimum = array( 'width' => 1, 'height' => 1 );
+
+  if ( $img[0] < $minimum['width'] ) {
+    $file['error'] = 'Image too small. Minimum width is ' . $minimum['width'] . 'px. Uploaded image width is ' . $img[0] . 'px';
+  } elseif ( $img[1] < $minimum['height'] ) {
+    $file['error'] = 'Image too small. Minimum height is ' . $minimum['height'] . 'px. Uploaded image height is ' . $img[1] . 'px';
+  }
+
+  return $file;
+}
 function dw_remove_size_attributes($html) {
   $html = preg_replace('/(width|height)="\d*"\s/', "", $html);
   return $html;
