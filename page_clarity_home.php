eaa69dbb39fdea506970df5d0593803f5b9a15fa
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2017-12-06 13:50:55 +0000

    rename homepage

---
+++ page_clarity_home.php
@@ -0,0 +1,30 @@
+<?php
+/***
+ *
+ * Template name: Clarity Home page
+ *
+ */
+get_header();
+
+use MOJ\Intranet\Agency;
+use MOJ\Intranet\HomepageBanners;
+
+$oAgency = new Agency();
+$activeAgency = $oAgency->getCurrentAgency();
+
+$emergencyBanner = HomepageBanners::getEmergencyBanner(get_intranet_code());
+$fullWidthTopBanner = HomepageBanners::getTopBanner(get_intranet_code());
+?>
+    <div id="maincontent" class="u-wrapper l-main t-home">
+    <?php get_component('c-phase-banner'); ?>
+<?php if ($emergencyBanner && $emergencyBanner['visible']) { ?>
+    <?php get_component('c-emergency-banner', $emergencyBanner); ?>
+<?php } ?>
+<?php if ($fullWidthTopBanner && $fullWidthTopBanner['visible']) { ?>
+    <?php get_component('c-full-width-banner', $fullWidthTopBanner); ?>
+<?php } ?>
+        <h1 class="o-title o-title--page"><?php echo $activeAgency['label'];?></h1>
+        <?php get_component('c-home-page-primary'); ?>
+        <?php get_component('c-home-page-secondary'); ?>
+    </div>
+<?php get_footer(); ?>
