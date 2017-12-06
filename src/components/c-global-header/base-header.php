6483fed88951372f82dd89f8d45fe20b2b4d36a0
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2017-12-06 11:23:51 +0000

    google tag manager added

--- src/components/c-global-header/base-header.php
+++ src/components/c-global-header/base-header.php
@@ -36,6 +36,25 @@
    ?>
    <?php wp_head(); ?>
 </head>
+<?php
+if (!defined('GT_CODE')) {
+  if ($_SERVER['HTTP_HOST'] == 'intranet.dev.wp.dsd.io') {
+    define('GT_CODE', 'GTM-W5VKJK');
+  }
+  else {
+    define('GT_CODE', 'GTM-P545JM');
+  }
+}
+?>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=<?=GT_CODE?>"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','<?=GT_CODE?>');</script>
+<!-- End Google Tag Manager -->
 <body <?php
 /**
  * Adds agency specific classes to the page.
