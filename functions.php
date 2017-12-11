d05c09e9ee6da04d14e34d6f222a71db33934104
vinhnguyen0429 <vinh.nguyen0429@gmail.com>
2017-12-11 16:13:44 +0000

    ReFactor agency cookie handling

--- functions.php
+++ functions.php
@@ -113,16 +113,17 @@ add_action('init', 'set_intranet_cookie');
 
 function set_intranet_cookie()
 {
-    $agency_value =  isset($_GET['agency']) ? trim($_GET['agency']) : 'hq';
-    if (!isset($_COOKIE['dw_agency'])) {
+    $default_agency = 'hq';
+
+    if (isset($_GET['agency'])) {
+        $agency_value =  isset($_GET['agency']) ? trim($_GET['agency']) : $default_agency;
         setcookie('dw_agency', $agency_value, time()+ (3650 * DAY_IN_SECONDS), COOKIEPATH, COOKIE_DOMAIN);
         $_COOKIE['dw_agency'] = $agency_value;
-    } else {
-        if (isset($_GET['agency'])) {
-            setcookie('dw_agency', $agency_value, time()+ (3650 * DAY_IN_SECONDS), COOKIEPATH, COOKIE_DOMAIN);
-            $_COOKIE['dw_agency'] = $agency_value;
-        }
+    } elseif (!isset($_COOKIE['dw_agency'])) {
+        setcookie('dw_agency', $default_agency, time()+ (3650 * DAY_IN_SECONDS), COOKIEPATH, COOKIE_DOMAIN);
+        $_COOKIE['dw_agency'] = $default_agency;
     }
+    
 }
 
 /**
