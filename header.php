0065bfdcbddf15f5f0419d63c08d49d7a81609f7
vinhnguyen0429 <vinh.nguyen0429@gmail.com>
2017-12-11 17:03:29 +0000

    added do not set cache on homepage

--- header.php
+++ header.php
@@ -1,1 +1,10 @@
-<?php get_component('c-global-header'); ?>
+<?php 
+
+    if(is_front_page()){
+        header('Cache-Control: max-age=0, no-cache, no-store, must-revalidate');
+        header('Expires: Thu, 01 Jan 1970 00:00:00 GMT');
+        header('Pragma: no-cache');
+    }
+
+    get_component('c-global-header'); 
+?>
