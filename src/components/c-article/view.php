678bef3e2fa10ca652d6dd0f380e99afb4af15a1
Adam <adam.brown@digital.justice.gov.uk>
2017-12-29 22:19:50 +0000

    Improved breadcrumb styling

--- src/components/c-article/view.php
+++ src/components/c-article/view.php
@@ -1,10 +1,13 @@
 <?php
-//use MOJ\Intranet\Authors;
+
+if (!defined('ABSPATH')) {
+    die();
+}
 
 ?>
 <!-- c-article starts here -->
 <article class="c-article">
-    <h1 class="o-title o-title--page"><a href="<?php echo get_the_permalink();?>"><?php echo get_the_title();?></a></h1>
+    <h1 class="o-title o-title--page"><?php echo get_the_title();?></h1>
     <?php get_template_part('src/components/c-article-byline/view'); ?>
     <?php get_template_part('src/components/c-rich-text-block/view'); ?>
     <?php get_template_part('src/components/c-share-post/view'); ?>
