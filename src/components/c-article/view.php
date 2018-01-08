1b9b9b9b0ea9ee18ce7bfd27af6dcb195940825a
Adam <adam.brown@digital.justice.gov.uk>
2018-01-08 15:25:24 +0000

    Disabled the share bar for now. Until Like Button plugin is finished.

--- src/components/c-article/view.php
+++ src/components/c-article/view.php
@@ -10,7 +10,7 @@ if (!defined('ABSPATH')) {
     <h1 class="o-title o-title--page"><?php echo get_the_title();?></h1>
     <?php get_template_part('src/components/c-article-byline/view'); ?>
     <?php get_template_part('src/components/c-rich-text-block/view'); ?>
-    <?php get_template_part('src/components/c-share-post/view'); ?>
+    <?php //get_template_part('src/components/c-share-post/view'); ?>
     <?php //get_template_part('src/components/c-comment-form/view'); ?>
     <?php //get_template_part('src/components/c-comments/view'); ?>
 </article>
