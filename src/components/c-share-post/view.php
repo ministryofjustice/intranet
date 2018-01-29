9e2ab9a88c40d2223faddf7da0eb96db2d1d60c6
Adam <adam.brown@digital.justice.gov.uk>
2018-01-29 23:39:03 +0000

    Added Like Button shortcode.

--- src/components/c-share-post/view.php
+++ src/components/c-share-post/view.php
@@ -2,7 +2,9 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <section class="c-share-post">
   <ul class="c-share-post__link">
-      <!-- add like button here -->
+
+    <li><?php echo do_shortcode('[likebutton]'); ?><li>
+
       <!--AF: I feel like this should be a form rather than a mailto link -->
       <span class="u-icon u-icon--redo2"></span>
       <a href="mailto:?subject=<?php echo get_the_title(); ?>&body=A colleague thought you would be intrested in this intranet article <?php echo get_permalink(); ?>">Share this post by email</a>
