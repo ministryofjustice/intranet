a974e81b87000c5ec04ba607cfa922daba434135
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2018-02-07 16:19:07 +0000

    saving comments work

--- inc/comments.php
+++ inc/comments.php
@@ -1,10 +1,69 @@
 <?php
+$valid_domains = array(
+    'cafcass.gsi.gov.uk',
+    'ccrc.x.gsi.gov.uk',
+    'cica.gsi.gov.uk',
+    'cjs.gsi.gov.uk',
+    'crowncommercial.gov.uk',
+    'digital.justice.gov.uk',
+    'hmcourts-service.gsi.gov.uk',
+    'hmcts.gsi.gov.uk',
+    'hmiprisons.gsi.gov.uk',
+    'hmiprobation.gsi.gov.uk',
+    'hmps.gsi.gov.uk',
+    'homeoffice.gsi.gov.uk',
+    'ips.gsi.gov.uk',
+    'jac.gsi.gov.uk',
+    'jaco.gsi.gov.uk',
+    'judiciary.gsi.gov.uk',
+    'justice.gov.uk',
+    'justice.gsi.gov.uk',
+    'lawcommission.gsi.gov.uk',
+    'legalaid.gsi.gov.uk',
+    'legalombudsman.org.uk',
+    'legalservicesboard.org.uk',
+    'noms.gsi.gov.uk',
+    'offsol.gsi.gov.uk',
+    'paroleboard.gsi.gov.uk',
+    'ppo.gsi.gov.uk',
+    'probation.gsi.gov.uk',
+    'publicguardian.gsi.gov.uk',
+    'sentencingcouncil.gsi.gov.uk',
+    'yjb.gsi.gov.uk'
+);
+
+function is_gov_email($email) {
+    $parts = explode('@', $email);
+    $domain = $parts[1];
+
+    return in_array($domain, $valid_domains);
+}
+
 function convert_to_time_ago( $date, $d, $comment ) {
 
     return human_time_diff( get_comment_time( 'U' ), current_time('timestamp') ) . ' ago';
 
 }
 add_filter( 'get_comment_date', 'convert_to_time_ago', 10,3 );
+
+function remove_comment_website_field($fields) { 
+    unset($fields['url']);
+    return $fields;
+}
+add_filter('comment_form_default_fields','remove_comment_website_field');
+
+function remove_must_be_logged_in($fields){
+    $fields['must_log_in'] = sprintf( 
+        __( '<p class="must-log-in">
+                You must <a href="%s">Register</a> or 
+            <a href="%s">Login</a> to post a comment.</p>' 
+        ),
+        wp_registration_url(),
+        wp_login_url( apply_filters( 'the_permalink', get_permalink() ) )   
+    );
+    return $fields;
+}
+add_filter('comment_form_defaults', 'remove_must_be_logged_in');
 
 function format_comment($comment, $args, $depth) {
 
--- src/components/c-comment-form/view.php
+++ src/components/c-comment-form/view.php
@@ -1,34 +1,9 @@
-<?php
-  $loginPrefix = 'cfl'
-  $commentPrefix = 'cfc'
-?>
 <!-- c-comment-form starts here -->
 <section class="c-comment-form">
   <h1 class="o-title o-title--subtitle">Comment on this page</h1>
   <!-- If !LoggedIn -->
   <p>Fill in your details below. We’ll then send you a link back to this page so you can start commenting.</p>
 
-  <form action="" class="c-comment-form__login">
-    <?php
-      form_builder('text', $loginPrefix, 'Screen name (Will appear on screen)', 'your_name', null, null, 'Enter your name', null, true, null, null);
-      form_builder('text', $loginPrefix, 'Email address (Will not be shown with your comment)', 'your_email', null, null, 'Enter your email address', null, true, null, null);
-    ?>
-    <button class="o-button">Get link</button>
-
-    <div class="o-clear-space">
-      <a href="">MoJ Commenting Policy</a>
-    </div>
-
-  </form>
-
-  <!-- If LoggedIn -->
-  <p>You're posting as Alex Foxleigh | <a href="">Not you?</a></p>
-  <form action="" class="c-comment-form__comment">
-    <?php
-      form_builder('textarea', $commentPrefix, 'Enter your comment', 'your_comment', null, null, 'Your comment goes here...', null, true, null, null);
-    ?>
-    <button class="o-button">Add Comment</button>
-    <button class="o-text-button">Cancel</button>
-  </form>
+  <?php comment_form(); ?>
 </section>
 <!-- c-comment-form ends here -->
