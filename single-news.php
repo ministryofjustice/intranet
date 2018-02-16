731a04c307d536598a38165ae096aa83599b838b
Adam <adam.brown@digital.justice.gov.uk>
2018-02-16 12:06:12 +0000

    Added new components that comprise the single news page.

---
+++ single-news.php
@@ -0,0 +1,23 @@
+<?php
+use MOJ\Intranet\Agency;
+/*
+* Single news post
+*/
+
+if (!defined('ABSPATH')) {
+    die();
+}
+
+get_header();
+
+$oAgency = new Agency();
+$activeAgency = $oAgency->getCurrentAgency();
+
+?>
+  <div id="maincontent" class="u-wrapper l-main t-news-article" role="main">
+    <?php get_template_part('src/components/c-breadcrumbs/view'); ?>
+    <?php get_template_part('src/components/c-article/view', 'news'); ?>
+
+  </div>
+
+<?php get_footer(); ?>
---
+++ src/components/c-article-byline/view-news.php
@@ -0,0 +1,27 @@
+<?php
+use MOJ\Intranet\Authors;
+
+if (!defined('ABSPATH')) {
+    die();
+}
+
+$post_object = get_post($id);
+$thumbnail_type = 'intranet-large';
+$thumbnail_id = get_post_thumbnail_id($id);
+$thumbnail = wp_get_attachment_image_src($thumbnail_id, $thumbnail_type);
+
+$oAuthor = new Authors();
+$authors = $oAuthor->getAuthorInfo($id);
+$thumbnail_url = $thumbnail[0];
+
+?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<!-- c-article-byline starts here -->
+<section class="c-article-byline">
+  <span class="c-article-byline__intro"><?php the_author(); ?></span>
+  <span class="c-article-byline__date"><?php the_date('d F Y'); ?></span>
+</section>
+<!-- c-article-byline ends here -->
+<?php endwhile; else : ?>
+	<p><?php esc_html_e('Sorry, nothing was found.'); ?></p>
+<?php endif; ?>
