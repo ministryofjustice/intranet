e827bdf073e0582b649beba09375b2998fc020d3
Adam <adam.brown@digital.justice.gov.uk>
2018-02-21 12:38:25 +0000

    Restructured the conditional statement on showing comments.

--- src/components/c-comments/view.php
+++ src/components/c-comments/view.php
@@ -20,8 +20,14 @@ $comments = get_comments(
 $post_id = get_the_ID();
 $post_meta = get_post_meta(get_the_ID());
 
+if (isset($post_meta["comment_disabled_status"][0])) {
+    $comments_disabled = $post_meta["comment_disabled_status"][0];
+} else {
+    $comments_disabled = '';
+}
 // Getting the post meta assosiated with what the admin has set the comments on this page to. See admin->comments.php
-$comments_disabled = isset($post_meta["comment_disabled_status"][0]) ? $post_meta["comment_disabled_status"][0] : '' ;
+
+//$comments_disabled = "" ? $comments_disabled === '0' : $comments_disabled;
 // Don't want to repeat ourselves - DRY
 $comment_title = 'Comments';
 ?>
@@ -30,14 +36,15 @@ $comment_title = 'Comments';
 <section class="c-comments">
 
 <?php
+
 /***
  *
  * If statement displays code based on three scenarios, comments on, off and closed.
  *
  */
 ?>
-<?php if (comments_open($post_id) === true && $comments_disabled === '0') : ?>
-
+<?php if (comments_open($post_id) && $comments_disabled === '0' || $comments_disabled === '') {
+    ?>
   <?php if (!get_comments_number()) : // Check in case comments gets switched on without any comments added.?>
 
     <h1 class="o-title o-title--subtitle"><?php echo $comment_title; ?></h1>
@@ -55,48 +62,49 @@ $comment_title = 'Comments';
         'avatar_size' => false,
         'type'=> 'comment',
         'callback' => 'format_comment',
-        ], $comments);
-    ?>
+        ], $comments); ?>
     </ul>
-
-  <?php endif; ?>
-
-<?php elseif (comments_open($post_id) === true && $comments_disabled === 'comments_disabled') : ?>
-
-  <?php if (!get_comments_number()) : //Check in case comments gets switched on without any comments added.?>
-
-    <h1 class="o-title o-title--subtitle"><?php echo $comment_title; ?></h1>
-    <h3>No comments have been left and comments have now been closed.</h3>
-
-  <?php else: ?>
-
-    <h1 class="o-title o-title--subtitle"><?php echo $comment_title; ?></h1>
-    <ul class="commentlist">
-    <p><span class="u-message u-message--warning">Comments are now closed</span></p>
-    <?php
-      wp_list_comments(
-        [
-        'reverse_top_level' => false,   // show newest at the top
-        'reverse_children' => true,     // Setting this to true will display the children (reply level comments) with the most recent ones first
-        'avatar_size' => false,
-        'type'=> 'comment',
-        'callback' => 'format_comment_closed',
-        ], $comments);
-    ?>
-    </ul>
-  <?php endif; ?>
-
-<?php elseif (comments_open($post_id) === false) : ?>
-
-  <?php // If comments are switched off display nothing. ?>
-  <?php echo ''; ?>
-
-<?php else: ?>
-
+    <?php endif; ?>
   <?php
-    // A fallback in case comment meta isn't set for some reason.
-    endif; // End of if statement.
-  ?>
+} elseif (comments_open($post_id) && $comments_disabled === 'comments_disabled') {
+    ?>
+    <?php if (!get_comments_number()) : //Check in case comments gets switched on without any comments added.?>
+
+      <h1 class="o-title o-title--subtitle"><?php echo $comment_title; ?></h1>
+      <h3>No comments have been left and comments have now been closed.</h3>
+
+    <?php else: ?>
+
+      <h1 class="o-title o-title--subtitle"><?php echo $comment_title; ?></h1>
+      <ul class="commentlist">
+      <p><span class="u-message u-message--warning">Comments are now closed</span></p>
+      <?php
+        wp_list_comments(
+          [
+          'reverse_top_level' => false,   // show newest at the top
+          'reverse_children' => true,     // Setting this to true will display the children (reply level comments) with the most recent ones first
+          'avatar_size' => false,
+          'type'=> 'comment',
+          'callback' => 'format_comment_closed',
+          ], $comments); ?>
+      </ul>
+    <?php endif; ?>
+  <?php
+} elseif (!comments_open($post_id) && $comments_disabled === '0') {
+    ?>
+  <?php echo ''; ?>
+  <?php 
+} elseif (!comments_open($post_id) && $comments_disabled === 'comments_disabled') {
+    ?>
+  <?php echo ''; ?>
+  <?php
+} else {
+    ?>
+    <?php echo 'Comments are not available.'; ?>
+  <?php
+
+}
+?>
 
 </section>
 <!-- c-comments ends here -->
