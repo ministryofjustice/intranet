183e78bf8c66e5271c5f14d68f2b832d956e7268
Adam <adam.brown@digital.justice.gov.uk>
2018-02-28 14:13:40 +0000

    Fix added for IE7. Reversed columns so date is on right. Made more sense.

--- src/components/c-article/view-events.php
+++ src/components/c-article/view-events.php
@@ -7,13 +7,14 @@ if (!defined('ABSPATH')) {
 ?>
 <!-- c-article events starts here -->
 <article class="c-article l-main">
-    <div class="l-secondary">
-      <?php get_template_part('src/components/c-events/view'); ?>
-    </div>
 
     <div class="l-primary" role="main">
       <h1 class="o-title o-title--page"><?php echo get_the_title();?></h1>
       <?php get_template_part('src/components/c-rich-text-block/view'); ?>
+    </div>
+
+    <div class="l-secondary">
+      <?php get_template_part('src/components/c-events/view'); ?>
     </div>
 
 </article>
