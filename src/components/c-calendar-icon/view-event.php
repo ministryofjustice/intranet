b485e817faa950b99b59c9fca2334fc19511d678
Adam <adam.brown@digital.justice.gov.uk>
2018-03-01 21:06:30 +0000

    Calendar display now shows date range if entered.

--- src/components/c-calendar-icon/view-event.php
+++ src/components/c-calendar-icon/view-event.php
@@ -14,15 +14,28 @@ $eventsList = $oEvents->getEvents();
 $post_id = get_the_ID();
 
 foreach ($eventsList as $event) {
-  if ($post_id == $event['id']) {
-    $data = $event['start_date'];
-  }
+    if ($post_id == $event['id']) {
+        $start_date = $event['start_date'];
+        $end_date = $event['end_date'];
+
+        if (isset($start_date) && isset($end_date)) {
+            $start_end_date = date("d M", strtotime($start_date)) . ' - ' . date("d M", strtotime($end_date));
+            $date = $event['start_date'];
+            if ($start_date === $end_date) {
+                $start_end_date = date("d M", strtotime($start_date));
+                $date = $event['start_date'];
+            }
+        } else {
+            $start_end_date = '';
+            $date = '';
+        }
+    }
 }
 
 ?>
-<time class="c-calendar-icon" datetime="<?php echo $data;?>">
+<time class="c-calendar-icon" datetime="<?php echo $date;?>">
   <h2 class="u-visually-hidden">Date:</h2>
-  <span class="c-calendar-icon--dow"><?php echo date("l", strtotime($data));?></span>
-  <span class="c-calendar-icon--dom"><?php echo date("d M", strtotime($data));?></span>
-  <span class="c-calendar-icon--my"><?php echo date("Y", strtotime($data));?></span>
+  <span class="c-calendar-icon--dow"><?php echo date("l", strtotime($date));?></span>
+  <span class="c-calendar-icon--dom"><?php echo $start_end_date;?></span>
+  <span class="c-calendar-icon--my"><?php echo date("Y", strtotime($date));?></span>
 </time>
