7ff3adc3cf766b327f5e4a6a3e1c0f242728c35a
Adam <adam.brown@digital.justice.gov.uk>
2018-03-01 16:37:09 +0000

    Applied one technique for swapping layout on mobile

--- src/components/c-article/view-events.php
+++ src/components/c-article/view-events.php
@@ -8,14 +8,14 @@ if (!defined('ABSPATH')) {
 <!-- c-article events starts here -->
 <article class="c-article l-main l-reverse-order">
 
-    <section class="l-primary" role="main">
-      <h1 class="o-title o-title--page"><?php echo get_the_title();?></h1>
-      <?php get_template_part('src/components/c-rich-text-block/view'); ?>
-    </section>
+  <aside class="l-secondary">
+    <?php get_template_part('src/components/c-events/view'); ?>
+  </aside>
 
-    <aside class="l-secondary">
-      <?php get_template_part('src/components/c-events/view'); ?>
-    </aside>
+  <section class="l-primary" role="main">
+    <h1 class="o-title o-title--page"><?php echo get_the_title();?></h1>
+    <?php get_template_part('src/components/c-rich-text-block/view'); ?>
+  </section>
 
 </article>
 <!-- c-article events ends here -->
