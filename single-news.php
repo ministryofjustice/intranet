f077723af3089ee4c5c2ddd15908b45526ab0cec
Adam <adam.brown@digital.justice.gov.uk>
2018-03-02 14:59:33 +0000

    New single news layout. Image added and newslisting

--- single-news.php
+++ single-news.php
@@ -17,8 +17,12 @@ $activeAgency = $oAgency->getCurrentAgency();
 ?>
 
 <div id="maincontent" class="u-wrapper l-main t-news-article" role="main">
-  <?php get_template_part( 'src/components/c-breadcrumbs/view' ); ?>
-  <?php get_template_part( 'src/components/c-article/view', 'news' ); ?>
+  <?php get_template_part('src/components/c-breadcrumbs/view'); ?>
+  <?php get_template_part('src/components/c-news-article/view'); ?>
+
+  <section class="l-full-page">
+  <?php get_template_part('src/components/c-share-post/view'); ?>
+  </section>
 </div>
 
 <?php get_footer(); ?>
