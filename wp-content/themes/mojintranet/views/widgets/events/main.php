72aa87c16ef21d7c9484fe2934b075a29539d506
Adam <adam.brown@digital.justice.gov.uk>
2018-03-06 16:34:34 +0000

    Added Clarity API and fixed styling to match old site template. Events showing correct on hub, regions still to do.

--- wp-content/themes/mojintranet/views/widgets/events/main.php
+++ wp-content/themes/mojintranet/views/widgets/events/main.php
@@ -1,19 +1,139 @@
-<?php if (!defined('ABSPATH')) die(); ?>
+<?php if (!defined('ABSPATH')) {
+    die();
+} ?>
+<?php
 
-<div class="events-widget" data-events-type="<?=$type?>">
+  global $post;
+
+  $terms = get_the_terms($post->ID, 'campaign_category');
+
+  foreach ($terms as $term) {
+      $campaign_id = $term->term_id;
+  }
+?>
+<div class="widget-event-item">
   <h2 class="category-name">Events</h2>
-  
-  <ul class="events-list grid"
-      data-skeleton-screen-count="<?=$skeleton_screen_count?>"
-      data-skeleton-screen-type="standard"
-      data-skeleton-screen-classes="col-lg-6 col-md-12 col-sm-12"></ul>
-
-  <p class="no-events-message">
-    <?=$no_items_found_message?>
-  </p>
-  <p class="see-all-container">
-    <a href="<?=$see_all_url?>"><?=$see_all_label?></a>
-  </p>
-
-  <?php $this->view('widgets/events/event_item') ?>
+  <div id="content">
+    <?php get_events_api('campaign', $campaign_id); ?>
+  </div>
 </div>
+<style>
+
+
+  .c-events-item {
+      position: relative;
+      padding-bottom: 1.5rem;
+      display: inline-block;
+      width: 100%;
+      margin-bottom: 2rem;
+  }
+
+  .c-events-item .c-calendar-icon {
+    position: absolute;
+    width: 25%;
+  }
+
+  .c-events-item h1 {
+    font-size: 1.4em;
+    font-family: nta,sans-serif;
+    font-weight: 700;
+    margin-bottom: 1rem;
+    display: block;
+    padding-left: 30%;
+  }
+
+  .c-events-item__time h2 {
+    font-size: 1em;
+    line-height: 0;
+    float: left;
+    clear: none;
+    text-align: inherit;
+    width: 33.33333333333333%;
+    margin-left: 0;
+    margin-right: 0;
+    font-weight: 400;
+  }
+
+  .c-events-item__time time {
+    float: left;
+    clear: none;
+    text-align: inherit;
+    width: 66.66666666666666%;
+    margin-left: 0;
+    margin-right: 0;
+  }
+
+  .c-events-item__location h2 {
+      font-size: 0.7em;
+      line-height: 0;
+      float: left;
+      clear: none;
+      text-align: inherit;
+      width: 33.33333333333333%;
+      margin-left: 0;
+      margin-right: 0;
+      font-weight: 400;
+    }
+
+  .c-events-item__time {
+    display: block;
+    padding-left: 30%;
+    clear: both;
+  }
+
+  .c-events-item__location {
+    display: block;
+    padding-left: 30%;
+    clear: both;
+    font-size: 1.4rem;
+  }
+
+  .c-events-item__location address {
+    font-size: 0.7em;
+    float: left;
+    clear: none;
+    text-align: inherit;
+    width: 66.66666666666666%;
+    margin-left: 0;
+    margin-right: 0;
+  }
+
+  .c-events-item__time address {
+    font-size: 0.7em;
+    float: left;
+    clear: none;
+    text-align: inherit;
+    width: 66.66666666666666%;
+    margin-left: 0;
+    margin-right: 0;
+  }
+
+  .c-calendar-icon span {
+      display: block;
+      text-align: center;
+      line-height: normal;
+  }
+
+  .c-calendar-icon--dow {
+    background-color: #ebedef;
+    font-size: 1rem;
+    padding-top: .2rem;
+  }
+
+  .c-calendar-icon--dom {
+    font-size: 2.4rem;
+    font-weight: 700;
+  }
+
+  .c-calendar-icon--my {
+      font-size: 1rem;
+      padding-bottom: .2rem;
+  }
+
+  .u-visually-hidden {
+    position: absolute;
+    left: -9999px;
+    top: -9999px;
+    color: transparent;
+}
+</style>
