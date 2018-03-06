444ac08a054caf8fafebe46f32a2202d9f6afc31
Adam <adam.brown@digital.justice.gov.uk>
2018-03-06 16:34:34 +0000

    Added Clarity API and fixed styling to match old site template. Events showing correct on hub, regions still to do.

--- wp-content/themes/mojintranet/views/widgets/events/event_item.php
+++ wp-content/themes/mojintranet/views/widgets/events/event_item.php
@@ -29,3 +29,16 @@
     </div>
   </li>
 </script>
+<?php if (!defined('ABSPATH')) {
+    die();
+} ?>
+<?php
+
+  global $post;
+
+  $terms = get_the_terms($post->ID, 'campaign_category');
+
+  foreach ($terms as $term) {
+      $campaign_id = $term->term_id;
+  }
+?>
