67fef8ba82e3e5f700b27c975aae2d1b5e20207e
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2018-03-08 13:26:17 +0000

    updated logo

--- src/components/c-logo-bar/view.php
+++ src/components/c-logo-bar/view.php
@@ -4,12 +4,17 @@
   $oAgency = new Agency();
   $activeAgency = $oAgency->getCurrentAgency();
 
+  if ($activeAgency['shortcode'] === 'law-commission'){
+    $logo = get_assets_folder() . '/images/lawcomms_logo.png';
+  }else{
+    $logo = get_assets_folder() . '/images/moj_logo.png';
+  }
 ?>
 
 <section class="c-logo-bar">
   <div class="u-wrapper">
     <a href="/" rel="home">
-      <img src="<?php echo get_assets_folder() ?>/images/moj_logo.png" alt="<?php echo $activeAgency['label']; ?> Logo">
+      <img src="<?php echo $logo; ?>" alt="<?php echo $activeAgency['label']; ?> Logo">
       <span class="agency-title"><?php echo $activeAgency['label']; ?></span>
     </a>
     <a href="/agency-switcher" class="c-logo-bar__switch">Switch to other intranet</a>
