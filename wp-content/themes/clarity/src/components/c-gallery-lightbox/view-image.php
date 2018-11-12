580e16ae2a8b42a44eb7d074cde3fa4a62bec3be
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2018-11-12 10:49:09 +0000

    Uploading compiled files.
    The last commit didn't have the compiled versions.

--- wp-content/themes/clarity/src/components/c-gallery-lightbox/view-image.php
+++ wp-content/themes/clarity/src/components/c-gallery-lightbox/view-image.php
@@ -1,6 +1,6 @@
 <?php //lightbox ?>
 <section class="c-image-gallery js-feature-video">
   <a class="popup-image" href="<?php echo $gallery_image; ?>">
-    <?php echo wp_get_attachment_image( $gallery_image_id, $size ); ?>
+    <img src="<?php echo wp_get_attachment_image_src( $gallery_image_id, $size ); ?>" alt="">
   </a>
 </section>
