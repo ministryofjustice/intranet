43fd1ea13e9def9c4522714d9b208dfe661ef60a
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2018-11-30 14:02:41 +0000

    save work

--- wp-content/themes/clarity/views/components/c-home-page-primary/view.php
+++ wp-content/themes/clarity/views/components/c-home-page-primary/view.php
@@ -18,16 +18,10 @@ $agency = $activeAgency['shortcode'];
 
 ?>
 <!-- c-home-page-primary starts here -->
-<section class="c-home-page-primary l-primary" role="main">
+<section class="c-home-page-primary l-full-page" role="main">
 
   <?php get_template_part('src/components/c-news-widget/view'); ?>
 
-  <?php
-  // removes sliding Need to Know gallery from agency homepage.
-   if ($agency !== 'opg' && $agency !== 'pb' && $agency !== 'cica' && $agency !== 'judicial-office') {
-       get_template_part('src/components/c-need-to-know-widget/view');
-   }
-   ?>
   <?php
   // removes event listing from agency homepage.
    if ($agency !== 'laa' && $agency !== 'hmcts') {
