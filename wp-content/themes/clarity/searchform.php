3aa4b5a92d429afd407bb48bf34aaebf4dad8a8b
Vinh Nguyen <vinh.nguyen0429@gmail.com>
2018-12-04 12:32:46 +0000

    save work

--- wp-content/themes/clarity/searchform.php
+++ wp-content/themes/clarity/searchform.php
@@ -13,11 +13,18 @@ use MOJ\Intranet\Agency;
  $activeAgency = $oAgency->getCurrentAgency();
  ?>
  <section class="c-search-bar">
-   <form role="search" method="GET" action="/" id="searchform" class="u-wrapper newclass">
-     <div class="c-search-bar__container">
-       <label for="s" class="u-visually-hidden">Search Intranet</label>
-       <input type="search" name="s" placeholder="Search <?php echo $activeAgency['abbreviation']; ?> intranet" id="s" value="<?php echo get_search_query(); ?>" />
-       <button type="submit" class="u-icon u-icon--search" aria-label="Go"><span>Go</span></button>
-     </div>
-   </form>
- </section>
+  <div class="split">
+      <form role="search" method="GET" action="/" id="searchform" class="u-wrapper newclass">
+       <div class="c-search-bar__container">
+         <label for="s" class="u-visually-hidden">Search Intranet</label>
+         <input type="search" name="s" placeholder="Search <?php echo $activeAgency['abbreviation']; ?> intranet" id="s" value="<?php echo get_search_query(); ?>" />
+         <button type="submit" class="u-icon u-icon--search" aria-label="Go"><span>Go</span></button>
+       </div>
+      </form>
+  </div>
+  <div class="split">
+    <h1>hello marker</h1>
+  </div>
+
+
+</section>
