1e699ab768a428cb7caba8ce5a891f6108e4302a
Adam <adam.brown@digital.justice.gov.uk>
2019-01-04 14:48:31 +0000

    Fix single news page aside news list
    When redesigning the homepage it had a knock on effect for the layout of
    the news listed causing it to look jumbled. It is now fixed and lists
    out correctly. To avoid future confusion I have created a new component.

--- wp-content/themes/clarity/src/components/c-news-article/view.php
+++ wp-content/themes/clarity/src/components/c-news-article/view.php
@@ -1,29 +1,24 @@
-<?php
-
-if (!defined('ABSPATH')) {
-    die();
-}
-
-?>
 <!-- c-news-article starts here -->
 <article class="c-news-article l-main">
 
   <section class="l-primary" role="main">
-    <h1 class="o-title o-title--headline"><?php echo get_the_title();?></h1>
-    <?php get_template_part('src/components/c-article-byline/view', 'news'); ?>
-    <?php get_template_part('src/components/c-article-featured-image/view', 'news'); ?>
-    <?php
-    if (has_excerpt()) {
-        get_template_part('src/components/c-article-excerpt/view');
-    }
-    ?>
-    <?php get_template_part('src/components/c-rich-text-block/view'); ?>
+	<h1 class="o-title o-title--headline"><?php echo get_the_title(); ?></h1>
+	<?php
+	get_template_part( 'src/components/c-article-byline/view', 'news' );
+	get_template_part( 'src/components/c-article-featured-image/view', 'news' );
+
+	if ( has_excerpt() ) {
+		get_template_part( 'src/components/c-article-excerpt/view' );
+	}
+
+	get_template_part( 'src/components/c-rich-text-block/view' );
+	?>
+
   </section>
 
   <aside class="l-secondary">
-    <h1 class="o-title">Recent news</h1>
-    <?php get_template_part('src/components/c-news-list/view', 'aside'); ?>
-    <a href="/newspage">See all news</a>
+	<h1 class="o-title">Recent news</h1>
+	<?php get_template_part( 'src/components/c-news-list-aside/view'); ?>
   </aside>
 
 </article>
