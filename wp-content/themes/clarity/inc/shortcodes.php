fe6a19e908628bcbe39a2e8c19b1551a9420d416
Adam <adam.brown@digital.justice.gov.uk>
2019-01-10 17:18:07 +0000

    Minor CSS changes to components

--- wp-content/themes/clarity/inc/shortcodes.php
+++ wp-content/themes/clarity/inc/shortcodes.php
@@ -1,22 +1,15 @@
 <?php
-function dw_columns_shortcode( $atts, $content = '' ) {
-	return '<div class="row">' . apply_filters( 'the_content', $content ) . '</div>';
-}
-add_shortcode( 'columns', 'dw_columns_shortcode' );
 
-function dw_col_shortcode( $atts, $content = '' ) {
-	return '<div class="l-half-section">' . apply_filters( 'the_content', $content ) . '</div>';
-}
-add_shortcode( 'col', 'dw_col_shortcode' );
+add_filter( 'the_content', 'fix_shortcode_p_gaps' );
 
-
-function dw_fix_shortcode_gaps( $content ) {
-	$block = join( '|', array( 'dw_columns', 'dw_col' ) );
+function fix_shortcode_p_gaps( $content = null ) {
+	$block = join( '|', array( 'mostpopular', 'dw_col' ) );
 	$rep   = preg_replace( "/(<p>)?\[($block)(\s[^\]]+)?\](<\/p>|<br \/>)?/", '[$2$3]', $content );
 	$rep   = preg_replace( "/(<p>)?\[\/($block)](<\/p>|<br \/>)?/", '[/$2]', $rep );
 	return $rep;
 }
-add_filter( 'the_content', 'dw_fix_shortcode_gaps' );
+
+add_shortcode( 'mostpopular', 'hr_most_popular_shortcode' );
 
 function hr_most_popular_shortcode() {
 	ob_start();
@@ -24,4 +17,3 @@ function hr_most_popular_shortcode() {
 	return ob_get_clean();
 
 }
-add_shortcode( 'mostpopular', 'hr_most_popular_shortcode' );
