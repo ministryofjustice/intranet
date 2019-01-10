169047517609f6a55d0f6756a8ea9aa8e2c3b55e
Adam <adam.brown@digital.justice.gov.uk>
2019-01-10 13:01:15 +0000

    Restructure homepage CSS and add additional tags
    To improve the symantic structure of the site CSS and to improve
    tracking on Google Tag manager, the homepage CSS structure has been
    changed. A tags that had no class now have classes so that they can be
    better tracked in Google Tag Manager.

--- wp-content/themes/clarity/page_home.php
+++ wp-content/themes/clarity/page_home.php
@@ -17,9 +17,8 @@ get_header();
 	<div id="maincontent" class="u-wrapper l-main t-home">
 	<?php
 		get_template_part( 'src/components/c-emergency-banner/view' );
-	  get_template_part( 'src/components/c-home-page-primary/view' );
+	  get_template_part( 'src/components/c-homepage-primary/view' );
 	?>
 	</div>
-<?php 
+<?php
 get_footer();
-
---
+++ wp-content/themes/clarity/src/components/c-homepage-blog-widget/view.php
@@ -0,0 +1,60 @@
+<?php
+use MOJ\Intranet\Agency;
+$oAgency      = new Agency();
+$activeAgency = $oAgency->getCurrentAgency();
+
+// TODO This block is required to check that featured items are not duplicated. Should be refactored.
+$agency              = get_intranet_code();
+$posttype_list_left  = get_field( $agency . '_post_type_list', 'option' );
+$agencies_list_left  = get_field( $agency . '_agency_list', 'option' );
+$posttype_list_right = get_field( $agency . '_post_type_list_right', 'option' );
+$agencies_list_right = get_field( $agency . '_agency_list_right', 'option' );
+$buildfeatleft       = $agency . '_feature_item_left_' . $posttype_list_left . '__' . $agencies_list_left;
+$buildfeatright      = $agency . '_feature_item_right_' . $posttype_list_right . '__' . $agencies_list_right;
+$feature_1           = get_field( $buildfeatleft, 'option' );
+$feature_2           = get_field( $buildfeatright, 'option' );
+$feature_1_id        = isset( $feature_1->ID ) ? $feature_1->ID : '';
+$feature_2_id        = isset( $feature_2->ID ) ? $feature_2->ID : '';
+$feature_array       = array( $feature_1_id, $feature_2_id );
+
+$args = array(
+	'post_type'      => 'post',
+	'order'          => 'DESC',
+	'orderby'        => 'date',
+	'posts_per_page' => 3,
+	// only show posts from active agency
+	'tax_query'      => array(
+		array(
+			'taxonomy' => 'agency',
+			'field'    => 'term_id',
+			'terms'    => $activeAgency['wp_tag_id'],
+		),
+	),
+	// exclude feature items by id
+	'post__not_in'   => $feature_array,
+
+);
+// Standard Query Loop  - https://codex.wordpress.org/Class_Reference/WP_Query
+$the_query = new WP_Query( $args ); ?>
+
+<?php
+
+if ( $the_query->have_posts() ) {
+
+	echo '<h1 class="o-title o-title--section">Latest blogs</h1>';
+
+	echo '<section class="c-homepage-blog-widget">';
+
+	while ( $the_query->have_posts() ) {
+		$the_query->the_post();
+		get_template_part( 'src/components/c-blog-article-item/view' );
+	}
+	echo '</section>';
+	/* Restore original Post Data */
+	wp_reset_postdata();
+} else {
+	// no posts found
+}
+?>
+
+<a href="blog" class="o-see-all-link">See all blogs</a>
