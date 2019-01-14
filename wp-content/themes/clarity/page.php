e93c0dd456778f8fd9276e6ded675c165ab2b482
Adam <adam.brown@digital.justice.gov.uk>
2019-01-14 15:53:20 +0000

    Cleaned up file/php formating

--- wp-content/themes/clarity/page.php
+++ wp-content/themes/clarity/page.php
@@ -2,25 +2,32 @@
 /**
  *
  * Template name: Default
- *
  */
 get_header();
 ?>
 
   <div id="maincontent" class="u-wrapper l-main l-reverse-order t-default">
-    <?php get_template_part('src/components/c-breadcrumbs/view', 'parents'); ?>
-    <div class="l-secondary">
-      <?php get_template_part('src/components/c-left-hand-menu/view'); ?>
-    </div>
-    <div class="l-primary" role="main">
-      <h1 class="o-title o-title--page"><?php the_title(); ?></h1>
-      <?php get_template_part('src/components/c-article-excerpt/view'); ?>
-      <?php get_template_part('src/components/c-rich-text-block/view'); ?>
-      <section class="l-full-page">
-      <?php get_template_part( 'src/components/c-last-updated/view' ); ?>  
-      <?php get_template_part('src/components/c-share-post/view'); ?>
-      </section>
-    </div>
+
+  <?php get_template_part( 'src/components/c-breadcrumbs/view', 'parents' ); ?>
+
+	<div class="l-secondary">
+		<?php get_template_part( 'src/components/c-left-hand-menu/view' ); ?>
+  </div>
+
+	<div class="l-primary" role="main">
+	  <h1 class="o-title o-title--page"><?php the_title(); ?></h1>
+    <?php 
+    get_template_part( 'src/components/c-article-excerpt/view' );
+    get_template_part( 'src/components/c-rich-text-block/view' );
+     ?>
+	  <section class="l-full-page">
+    <?php 
+    get_template_part( 'src/components/c-last-updated/view' );
+    get_template_part( 'src/components/c-share-post/view' ); 
+    ?>
+	  </section>
+  </div>
+
   </div>
 
 <?php
