9ae09370bf6c9c563873c1246cc7f59d08b5c12f
Adam <adam.brown@digital.justice.gov.uk>
2019-01-23 15:28:52 +0000

    Bug - fix no excerpt on campaign content
    Found an error being thrown on the campaign content page. Turned out the
    template was using a variable to get the excerpt that didn't exist so it
    was not showing the excerpt and throwing an error. Fixed with correct
    function to get excerpt.

--- wp-content/themes/clarity/page_campaign_content.php
+++ wp-content/themes/clarity/page_campaign_content.php
@@ -2,29 +2,34 @@
 /**
  *
  * Template name: Campaign content
- *
  */
 get_header();
+
+get_template_part( 'src/components/c-campaign-colour/view' );
 ?>
-<?php get_template_part('src/components/c-campaign-colour/view'); ?>
+
   <div id="maincontent" class="u-wrapper l-main l-reverse-order t-default">
-    <?php get_template_part('src/components/c-breadcrumbs/view'); ?>
-    <div class="l-secondary">
-      <?php get_template_part('src/components/c-left-hand-menu/view'); ?>
-    </div>
-    <div class="l-primary" role="main">
-      <?php get_template_part('src/components/c-campaign-banner/view'); ?>
-      <h1 class="o-title o-title--page"><?php the_title(); ?></h1>
-      <section class="c-article-excerpt">
-        <p><?=$excerpt?></p>
-      </section>
-      <div class="template-container ">
-        <?php get_template_part('src/components/c-rich-text-block/view'); ?>
-      </div>
-      <section class="l-full-page">
-      <?php get_template_part('src/components/c-share-post/view'); ?>
-      </section>
-    </div>
+	<?php get_template_part( 'src/components/c-breadcrumbs/view' ); ?>
+
+	<div class="l-secondary">
+		<?php get_template_part( 'src/components/c-left-hand-menu/view' ); ?>
+	</div>
+
+	<div class="l-primary" role="main">
+		<?php get_template_part( 'src/components/c-campaign-banner/view' ); ?>
+	  <h1 class="o-title o-title--page"><?php the_title(); ?></h1>
+	  <section class="c-article-excerpt">
+		<p><?php echo the_excerpt(); ?></p>
+	  </section>
+
+	  <div class="template-container ">
+		<?php get_template_part( 'src/components/c-rich-text-block/view' ); ?>
+	  </div>
+
+	  <section class="l-full-page">
+		<?php get_template_part( 'src/components/c-share-post/view' ); ?>
+	  </section>
+	</div>
   </div>
 
 <?php
