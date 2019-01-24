6a42cd80ddbb2f793a9bb22288915551e49cee38
Adam <adam.brown@digital.justice.gov.uk>
2019-01-24 10:34:49 +0000

    Add YJB link to intranet switcher
    YJB have requested a new link be added to the intranet agency switcher.

--- wp-content/themes/clarity/agency-switcher.php
+++ wp-content/themes/clarity/agency-switcher.php
@@ -2,10 +2,14 @@
 /*
 *  Agency switcher
 */
+get_header();
 ?>
-<?php get_header(); ?>
+
   <div id="maincontent" class="u-wrapper l-main t-agency-switcher" role="navigation">
     <h1 class="o-title o-title--page">Choose your agency or body</h1>
-    <?php get_component('c-intranet-switcher'); ?>
+
+    <?php get_template_part( 'src/components/c-intranet-switcher/view' ); ?>
   </div>
-<?php get_footer(); ?>
+
+<?php
+get_footer();
