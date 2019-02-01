8a486b44c1fc81dce35179eb31c0f55b1c87891f
Adam <adam.brown@digital.justice.gov.uk>
2019-02-01 19:04:06 +0000

    Fix teampage feature section, breadcrumbs & news

--- wp-content/themes/clarity/single-team_news.php
+++ wp-content/themes/clarity/single-team_news.php
@@ -14,9 +14,9 @@ $activeAgency = $oAgency->getCurrentAgency();
 
 <div id="maincontent" class="u-wrapper l-main t-news-article" role="main">
 	<?php
-  get_template_part( 'src/components/c-breadcrumbs/view' );
+	get_template_part( 'src/components/c-breadcrumbs/view', 'team' );
 	get_template_part( 'src/components/c-news-article/view', 'full' );
-  ?>
+	?>
 
   <section class="l-full-page">
 	<?php get_template_part( 'src/components/c-share-post/view' ); ?>
