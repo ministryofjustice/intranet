3951d70622eaca4d7e657207ff2dc18ccedf18c4
Adam <adam.brown@digital.justice.gov.uk>
2019-02-15 15:31:41 +0000

    Fix event date when viewed from another agency
    Users would get a blank event page when accessing an event from an
    agency the event is not associated with. This fixes that so users now
    receive a message with instructions and links to the event on the
    correct agency page.

--- wp-content/themes/clarity/single-event.php
+++ wp-content/themes/clarity/single-event.php
@@ -1,33 +1,68 @@
 <?php
-
+use MOJ\Intranet\Agency;
+use MOJ\Intranet\Event;
 /*
 * Single event post
 */
-// Query DB to tell this event is in the regions and serve correct breadcrumb
-$post_id   = get_the_ID();
+
+$oEvent  = new Event();
+$oAgency = new Agency();
+
+$post_id   = get_the_id();
 $region_id = get_the_terms( $post_id, 'region' );
-
-if ( $region_id ) :
-	foreach ( $region_id as $region ) :
-		// Current region, ie Scotland, North West etc
-		$current_region = '&region=' . $region->term_id;
-endforeach;
-endif;
-
-var_dump($region_id);
-var_dump($current_region);
+$terms     = get_the_terms( $post_id, 'agency' );
+$post_type = get_post_type( $post_id );
+$event     = $oEvent->get_event_list( 'search' );
 
 get_header();
 
 ?>
   <div id="maincontent" class="u-wrapper l-main t-events">
 	<?php
-	if ( $region_id ) :
+
+	// display correct breadcrumb
+	if ( $region_id ) {
 		get_template_part( 'src/components/c-breadcrumbs/view', 'region-single' );
-	else :
+	} else {
 		get_template_part( 'src/components/c-breadcrumbs/view', 'event' );
-	endif;
-	get_template_part( 'src/components/c-event-article/view' );
+	}
+
+	// display correct page based on agency access
+	if ( $event ) {
+		get_template_part( 'src/components/c-event-article/view' );
+	} else {
+
+		echo '<div class="o-notification-panel">';
+
+		if ( is_array( $terms ) ) {
+
+			$agency_count = sizeof( $terms );
+
+			if ( $agency_count > 1 ) {
+				$subject_verb   = 'one of';
+				$subject_verb_2 = 's';
+			} else {
+				$subject_verb   = '';
+				$subject_verb_2 = '';
+			}
+
+			echo '<h1>';
+			echo 'The event you are trying to access is not associated with your current agency ';
+			echo '</h1>';
+			echo 'View the event by switching agencies. Use ' . $subject_verb . ' the below link' . $subject_verb_2 . ':';
+			echo '<br><br>';
+
+			foreach ( $terms as $term ) {
+
+				$agency_name = $term->name;
+				$agency_slug = $term->slug;
+
+				echo ' <a href="' . get_permalink() . '?agency=' . esc_attr( $agency_slug ) . '">' . esc_attr( $agency_name ) . '</a>,';
+			}
+		}
+
+		echo '</div>';
+	}
 	?>
 
 	<section class="l-full-page">
