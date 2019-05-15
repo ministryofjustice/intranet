3e1d7901d05d06c624f6734686d0c4bb66d2e35e
Adam <adam.brown@digital.justice.gov.uk>
2019-05-15 11:24:30 +0100

    Add conditional so excerpt only shows when excerpt field is populated

--- wp-content/themes/clarity/src/components/c-article-excerpt/view.php
+++ wp-content/themes/clarity/src/components/c-article-excerpt/view.php
@@ -1,7 +1,9 @@
 <?php
+// Check template being used
 $current_template     = get_post_meta( get_the_ID(), '_wp_page_template', true );
 $campaign_hub_excerpt = get_field( 'campaign_hub_excerpt' ) ?? '';
 ?>
+
 <!-- c-article-excerpt starts here -->
 <section class="c-article-excerpt">
 	<?php
@@ -9,7 +11,11 @@ $campaign_hub_excerpt = get_field( 'campaign_hub_excerpt' ) ?? '';
 	if ( $current_template === 'page_campaign_landing.php' ) {
 		echo stripslashes( wp_filter_post_kses( addslashes( $campaign_hub_excerpt ) ) );
 	} else {
-		echo the_excerpt();
+		if ( has_excerpt() ) {
+			echo the_excerpt();
+		} else {
+			return;
+		}
 	}
 	?>
 </section>
