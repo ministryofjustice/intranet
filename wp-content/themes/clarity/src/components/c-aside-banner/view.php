4b47f39a75f08ca563e05e747180663eacf4253e
Adam <adam.brown@digital.justice.gov.uk>
2019-05-15 14:05:05 +0100

    Add crop to ACF banner on homepage

--- wp-content/themes/clarity/src/components/c-aside-banner/view.php
+++ wp-content/themes/clarity/src/components/c-aside-banner/view.php
@@ -9,14 +9,17 @@ $homepage_sidebar_banner_link     = get_field( $agency . '_homepage_sidebar_bann
 $homepage_sidebar_banner_alt_text = get_field( $agency . '_homepage_sidebar_banner_alt_text', 'option' );
 ?>
 
+
+
 <?php if ( $enable_banner_right_side == true ) : ?>
 
 <!-- c-aside-banner starts here -->
 <section class="c-aside-banner">
   <h1 class="o-title o-title--subtitle"><?php echo $banner_header; ?></h1>
 
+  
   <a href="<?php echo $homepage_sidebar_banner_link; ?>" class="c-aside-banner--link">
-	  <img src="<?php echo $homepage_sidebar_banner_image; ?>" alt="<?php echo $homepage_sidebar_banner_alt_text; ?>">
+	  <img src="<?php echo $homepage_sidebar_banner_image['sizes']['medium']; ?>" alt="<?php echo $homepage_sidebar_banner_alt_text; ?>">
   </a>
 </section>
 <!-- c-aside-banner ends here -->
