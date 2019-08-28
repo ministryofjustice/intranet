34f6ea58d5758c951ea05e3ba691f43b544001b3
Adam <adam.brown@digital.justice.gov.uk>
2019-08-28 19:59:03 +0100

    Remove deprecated smoke tests

--- smoke-tests/features/happypath/00_check_environment.feature
+++
@@ -1,5 +0,0 @@
-@quick_run
-Feature: Test Environment
-
-Scenario: Test Environment
-  Given I show my environment
--- smoke-tests/features/step_definitions/admin_working_version.rb
+++
@@ -1,4 +0,0 @@
-When(/^I visit the preview link$/) do
-  @link ||= find('#sample-permalink a')[:href]
-  step %[I visit "#{@link}"]
-end
