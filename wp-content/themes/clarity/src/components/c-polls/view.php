3063a5ef90975e12b79af7f33a20a8ebf2a8225f
adam <brown-a2@users.noreply.github.com>
2019-12-12 10:43:27 +0000

    Fix and update TotalPoll plugin support (#261)
    * Fix and update TotalPoll plugin support
    
    TotalPolls completely refactored their poll plugin, breaking some of the
    customisation work we had applied to the plugin. For example hiding
    paid-for features of the plugin we are not using, in the editor view. A
    warning was also thrown as the plugin had changed file names. This has
    been fixed.
    
    * Document theme poll component to explain it only is used for CSS

--- wp-content/themes/clarity/src/components/c-polls/view.php
+++ wp-content/themes/clarity/src/components/c-polls/view.php
@@ -1,13 +1,10 @@
-<?php
-use MOJ\Intranet\Agency;
-$agency = get_intranet_code();
-$polls  = get_field( $agency . '_homepage_polls_shortcode', 'option' );
-?>
+<!-- Note 
+
+This component is currently not being used anywhere on the site, however,
+its CSS style file is being used to determine the style of the poll plugin widget when displayed, so that
+its styling is compatible with the site.
+
+-->
 <!-- c-polls starts here -->
-<section class="c-polls js-polls">
-	<?php
-	echo do_shortcode( $polls );
-	?>
-  <br/>
-</section>
+<section class="c-polls js-polls"></section>
 <!-- c-polls ends here -->
