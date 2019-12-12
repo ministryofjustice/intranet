fc52aee5b6050ec68362f8af5cdd28a50be9d276
Adam <adam.brown@digital.justice.gov.uk>
2019-12-12 10:35:45 +0000

    Document theme poll component to explain it only is used for CSS

--- wp-content/themes/clarity/src/components/c-polls/view.php
+++ wp-content/themes/clarity/src/components/c-polls/view.php
@@ -1,4 +1,10 @@
+<!-- Note 
+
+This component is currently not being used anywhere on the site, however,
+it's CSS style file is being used to determine the style of the poll plugin widget so that
+its styling is compatible with the site.
+
+-->
 <!-- c-polls starts here -->
-<section class="c-polls js-polls">
-</section>
+<section class="c-polls js-polls"></section>
 <!-- c-polls ends here -->
