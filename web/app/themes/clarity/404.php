784a4ed9caa85561412886f7965785f662b4a7ed
Adam <adam.brown@digital.justice.gov.uk>
2019-12-31 12:33:30 +0000

    Update old WP 404 page and remove now unused 404 component.

--- web/app/themes/clarity/404.php
+++ web/app/themes/clarity/404.php
@@ -1,21 +1,11 @@
 <?php
 
 /**
- * The template for displaying 404 pages (Not Found)
+ * This page is not being used as per standard WP config. 
+ * All error pages now reside in the /error-pages/ directory.
+ * These are loaded via NGINX configurations.
  *
  * @package WordPress
  * @subpackage Clarity
  * @since Clarity 1.0
  */
-
-get_header();
-?>
-
-    <div id="maincontent" class="u-wrapper l-main t-page-error">
-      <section class="l-primary" role="main">
-        <?php get_template_part('src/components/c-page-error/view'); ?>
-    </section>
-    
-    </div>
-<?php
-get_footer();
