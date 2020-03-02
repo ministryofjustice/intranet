a6fff755efea7e00875075896ea005beb4bab0c5
adam <adam.brown@digital.justice.gov.uk>
2020-03-02 15:45:13 +0000

    Set email back to intranet-support address
    After testing, I noticed that changing the email to our team one
    "wordpress" rather then "intranet-support" was a bit confusing and that
    the old name, intranet support was still probably better. So I've
    switched it back and left it for now.

--- web/app/themes/clarity/src/components/c-register/view.php
+++ web/app/themes/clarity/src/components/c-register/view.php
@@ -101,10 +101,10 @@ if (isset($_POST['task']) && $_POST['task'] == 'register') {
             finish the registration.</p>
 
         <p><strong>Any problems?</strong></p>
-        <p>The email will be from <a href="mailto:wordpress@digital.justice.gov.uk"
-                target="_blank">wordpress@digital.justice.gov.uk</a>.<p>
+        <p>The email will be from <a href="mailto:intranet-support@digital.justice.gov.uk"
+                target="_blank">intranet-support@digital.justice.gov.uk</a>.<p>
 
-                <p[>If you can’t find it, check your junk folder then add the address to your safe list.
+                <p>If you can’t find it, check your junk folder then add the address to your safe list.
             </p>
 
             <p>Do not reply to the email.</p>
