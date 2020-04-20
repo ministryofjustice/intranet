59f880ab21ce546089a087c19a28c80be49152b4
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2020-04-20 08:52:23 +0100

    Condolence Header

---
+++ web/app/themes/clarity/src/components/c-condolence-header/view.php
@@ -0,0 +1,13 @@
+<?php
+/** Condolence - Black Header**/
+
+?>
+
+<!-- c-condolence-header starts here -->
+
+<div class="c-condolence-header">
+
+    <h1 class="o-title o-title--page"><?php echo get_the_title(); ?></h1>
+
+</div>
+<!-- c-condolence-header ends here -->
