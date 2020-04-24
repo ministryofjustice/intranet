d0357042688e9d35798e408d07e6edf2ff20a8e1
adam <adam.brown@digital.justice.gov.uk>
2020-04-24 09:23:27 +0100

    Temp disable condolence template

---
+++ web/app/themes/clarity/page_condolence_listing.php
@@ -0,0 +1,21 @@
+<?php
+/*
+* Add template name in when ready
+*/
+get_header();
+
+?>
+    <div id="maincontent" class="u-wrapper l-main t-article-list">
+        <?php get_template_part('src/components/c-condolence-header/view'); ?>
+        <div class="l-secondary">
+            <?php get_template_part('src/components/c-content-filter/view', 'condolences'); ?>
+        </div>
+        <div class="l-primary" role="main">
+            <div id="content">
+                <?php get_template_part('src/components/c-condolences-list/view'); ?>
+            </div>
+        </div>
+    </div>
+
+<?php
+get_footer();
