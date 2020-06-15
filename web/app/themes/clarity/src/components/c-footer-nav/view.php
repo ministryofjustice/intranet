13ebe9eb27f8bf4d0ee68fe65487d2c6f05e2d32
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2020-06-15 08:51:05 +0100

    Add Footer Nav

---
+++ web/app/themes/clarity/src/components/c-footer-nav/view.php
@@ -0,0 +1,9 @@
+<nav class="c-footer-nav">
+  <div class="u-wrapper">
+    <?php
+    if ( has_nav_menu( 'footer-menu' ) ) {
+        wp_nav_menu(['theme_location' => 'footer-menu']);
+    }
+    ?>
+  </div>
+</nav>
