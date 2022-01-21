6cb01103937d2a569d677dac111410ac9a378fde
adam <brown.a2@gmail.com>
2022-01-21 23:25:19 +0000

    Add OSPT to registered menus. Add to global header.

--- web/app/themes/clarity/src/components/c-global-header/view-ospt.php
+++ web/app/themes/clarity/src/components/c-global-header/view-ospt.php
@@ -1,5 +1,5 @@
 <?php
   $agency_colour    = '#005ea5';
-  $agency_title     = 'HQ';
-  $agency_shortcode = 'hq';
+  $agency_title     = 'OSPT';
+  $agency_shortcode = 'ospt';
   require_once 'base-header.php';
