9012d11c0c080147623173d42fc101e59150bd01
Meena <meena.modhvadia@digital.justice.gov.uk>
2022-04-06 09:57:02 +0100

    aria-expanded and aria-haspopup added

--- web/app/themes/clarity/src/components/c-left-hand-menu/left-hand-menu.js
+++ web/app/themes/clarity/src/components/c-left-hand-menu/left-hand-menu.js
@@ -8,18 +8,20 @@ export default (function ($) {
                     $(this).addClass('u-closed')
                 }
                 // If the list item has children, attach a click event to each directly descended anchor element
-                $(this).filter('.page_item_has_children').find('> a .dropdown').on(
+                $(this).filter('.page_item_has_children').find('> a ').on(
                     'click',
                     function (e) {
                         if ($(this).parentsUntil('ul').hasClass('u-closed')) {
                             e.preventDefault()
                             $(this).parentsUntil('ul').removeClass('u-closed')
+                            $(this).attr('aria-expanded', 'true')    
                         } else {
                             e.preventDefault()
                             $(this).parentsUntil('ul').addClass('u-closed')
+                            $(this).attr('aria-expanded', 'false')
                         }
                     }
-                )
+                ).attr('aria-expanded', 'false').attr('aria-haspopup', 'true')
             }
         )
         return this
