0c2e2e9119e98bb975ce956e9cfd8d76f62fdd5f
Meena <meena.modhvadia@digital.justice.gov.uk>
2022-06-28 11:50:44 +0100

    aria-expanded added to anchor

--- web/app/themes/clarity/src/components/c-feedback-form/view.php
+++ web/app/themes/clarity/src/components/c-feedback-form/view.php
@@ -6,7 +6,9 @@ $oAgency      = new Agency();
 $activeAgency = $oAgency->getCurrentAgency();
 // LEGACY END
 global $wp;
-$current_url = home_url($wp->request)
+$current_url = home_url($wp->request);
+$current_user = wp_get_current_user();
+$name = $current_user->display_name;
 ?>
 
 <?php 
@@ -16,7 +18,7 @@ $current_url = home_url($wp->request)
 <form class="c-feedback-form js-reveal-target" id="<?php echo $prefix; ?>" action="<?php echo $current_url; ?>#confirmation-message" method="POST">
     <?php
 
-    form_builder('text', $prefix, 'Your name', 'name', null, null, 'Enter your name', null, true, null, null);
+    form_builder('text', $prefix, 'Your name', 'name', null, $name, 'Enter your name', null, true, null, null);
     form_builder('text', $prefix, 'Your email', 'email', null, null, 'Enter your email', null, true, null, null);
     form_builder('textarea', $prefix, 'Describe what\'s wrong with this page', 'message', null, null, 'Enter your feedback', null, true, null, null);
 
