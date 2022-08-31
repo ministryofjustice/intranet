eae56bcc252e2efa651146d0cdec41b012778d00
Meena <meena.modhvadia@digital.justice.gov.uk>
2022-08-31 12:42:43 +0100

    Update various plugins plus fixed vulnerabilities

--- web/app/themes/clarity/webpack.mix.js
+++ web/app/themes/clarity/webpack.mix.js
@@ -1,31 +1,24 @@
-const mix = require('laravel-mix')
+let mix = require('laravel-mix')
 
-const stylDeps = [require('jeet')(), require('rupture')()]
-
-require('laravel-mix-imagemin')
+let stylDeps = {
+    stylusOptions: {
+        use: [require('jeet')(), require('rupture')()]
+    }
+};
 
 mix.setPublicPath('./dist/')
 
 /*******************/
 mix.js('src/globals/js/script-loader.js', 'dist/js/main.min.js')
-    .stylus('src/globals/css/_init.styl', 'dist/css/globals.css', { use: stylDeps })
-    .stylus('src/components/style.print.styl', 'dist/css/', { use: stylDeps })
-    .stylus('src/components/style.ie.styl', 'dist/css/', { use: stylDeps })
-    .stylus('src/components/style.ie8.styl', 'dist/css/', { use: stylDeps })
-    .stylus('src/components/style.styl', 'dist/css/', { use: stylDeps })
-    .imagemin([
-            'images/**.*',
-            'images/*/**.*'
-        ],
-        { context: 'src/globals' },
-        {
-            optipng: { optimizationLevel: 5 },
-            jpegtran: null
-        }
-    )
+    .stylus('src/globals/css/_init.styl', 'dist/css/globals.css', stylDeps)
+    .stylus('src/components/style.print.styl', 'dist/css/', stylDeps)
+    .stylus('src/components/style.ie.styl', 'dist/css/', stylDeps)
+    .stylus('src/components/style.ie8.styl', 'dist/css/', stylDeps)
+    .stylus('src/components/style.styl', 'dist/css/', stylDeps)
     .copy('src/vendors/*', 'dist/js/')
     .copy('./node_modules/jquery/dist/jquery.min.js', 'dist/js/')
     .copy('src/globals/fonts/*', 'dist/fonts/')
+    .copy('src/globals/images', 'dist/images/')
     .options({ processCssUrls: false })
 
 if (mix.inProduction()) {
