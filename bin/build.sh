62445ca8f1835c960b659a9c91871cd004c8bbe9
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2022-10-18 12:25:19 +0100

    Clean up package and build files

--- bin/build.sh
+++ bin/build.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 set -e
 
-###
 # Build Script
 # Use this script to build theme assets,
 # and perform any other build-time tasks.
-##
 
 # Install PHP dependencies (WordPress, plugins, etc.)
 composer install
 
-
 # Build theme assets
-# ~ enter theme path and uncomment the following commands:
-
 cd web/app/themes/clarity
 npm install
 npm run dev
