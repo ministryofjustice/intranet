6e7a1312af9eab9cc9a17255435bed3452d72039
Damien Wilson <47327051+wilson1000-MoJ@users.noreply.github.com>
2023-05-15 12:11:34 +0100

    Php8 final upgrade check (#404)
    * Improve homepage feature search (#394)
    
    * CDPT-678 : Integrate GovUK Notify (#396)
    
    * Short-circuit wp_mail() use GovUK Notify
    
    * API key check
    
    * Declare true null
    
    * Look for UUID
    
    * CDPT-282-remove-recently-edited-content-widget (#398)
    
    cdpt-282-remove-recently-edited-content-widget
    
    * Remove recently edited content widget (#399)
    
    * cdpt-282-remove-recently-edited-content-widget
    
    * Add SMTP_PASS to catch dev
    
    * CDPT-687 Improve GovUK Notify installation (#400)
    
    * cdpt-282-remove-recently-edited-content-widget
    
    * Add SMTP_PASS to catch dev
    
    * Lock WP to versions 6.2.x
    
    * Add 4 new templates
    
    * Remove redundant html_mail function
    
    * Improve template selection process
    
    * Update mail to send via Notify templates
    
    * Composer updated
    
    * Improve GovUK Notify Installation (#401)
    
    * cdpt-282-remove-recently-edited-content-widget
    
    * Add SMTP_PASS to catch dev
    
    * Lock WP to versions 6.2.x
    
    * Add 4 new templates
    
    * Remove redundant html_mail function
    
    * Improve template selection process
    
    * Update mail to send via Notify templates
    
    * Composer updated
    
    * Add break to correct alignment
    
    * php8 upgrade

--- web/app/themes/clarity/inc/mail-templates.php
+++ web/app/themes/clarity/inc/mail-templates.php
@@ -10,7 +10,7 @@ return [
 
     /**
      * Email configuration
-     ***********************/
+     ************************/
 
     'email' => [
 
@@ -21,7 +21,43 @@ return [
                 'message' => '<message>'
             ]
         ],
+        'comment-registration' => [
+            'id' => 'ab5e3dbb-4bda-4323-86ca-a200fa3d6689',
+            'personalisation' => [
+                'name' => '<name>',
+                'reply_link' => '<reply_link>'
+            ]
+        ],
 
+        'comment-deletion' => [
+            'id' => '99c82592-b7fe-42f0-ac0a-acad5d2fe19d',
+            'personalisation' => [
+                'name' => '<name>',
+                'delete_link' => '<delete_link>'
+            ]
+        ],
+
+        'wrong-with-this-page' => [
+            'id' => '29cc5601-662e-463b-b04a-47fd7f18efd2',
+            'personalisation' => [
+                'date' => '',
+                'name' => '',
+                'email_creator' => '',
+                'message' => '',
+                'agency' => '',
+                'page_url' => '',
+                'user_agent' => ''
+            ]
+        ],
+
+        'wrong-with-this-page-confirmation' => [
+            'id' => '183a0daf-18ab-4426-920a-fcb3831a0ade',
+            'personalisation' => [
+                'date' => '',
+                'name' => '',
+                'agency_name' => ''
+            ]
+        ],
         'auto-response' => [
             'id' => '4f4639e0-9673-4221-9f42-fe5e543f1718'
         ]
@@ -32,5 +68,4 @@ return [
      **********************/
 
     'sms' => []
-
 ];
--- web/app/themes/clarity/src/components/c-register/view.php
+++ web/app/themes/clarity/src/components/c-register/view.php
@@ -1,89 +1,90 @@
 <!-- c-register starts here -->
 <?php
 
-if (! defined('ABSPATH')) {
+if (!defined('ABSPATH')) {
     die();
 }
 
-$err = $err_name = $err_email = '';
-$success = '';
+$err = $err_name = '';
+$success = false;
 $first_name = '';
 $email = '';
 
+$c_register_post_array = $_POST;
+
 global $wpdb, $PasswordHash, $current_user, $user_ID;
 
-if (isset($_POST['task']) && $_POST['task'] == 'register') {
-    $pwd1 = wp_generate_password();
+if (isset($c_register_post_array['task']) && $c_register_post_array['task'] == 'register') {
 
-    $first_name = esc_sql(trim($_POST['first_name']));
-    $email      = esc_sql(trim($_POST['email']));
-    $username   = $email;
+    $first_name = esc_sql(trim($c_register_post_array['first_name']));
+    $email = esc_sql(trim($c_register_post_array['email']));
+    $username = $email;
 
+    /**
+     * Test the given name
+     */
     if ($first_name == '') {
         $err_name = 'Enter a screen name';
-    }    
-    if ($email == '') {
-        $err_email = 'Enter an email address';
-    } elseif (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $err_email = 'Enter a valid email address';
-    } elseif (email_exists($email)) {
-        $err_email = 'This email already exists';
-    } elseif (! is_gov_email($email)) {
-        $err_email = 'Enter an MoJ email address';
-    } 
-    if ($err_name == '' && $err_email== '') {
-        $create_user = wp_insert_user(
-            array(
+    }
+
+    /**
+     * Test the given email
+     */
+    switch (true) {
+        case $email == '':
+            $err_email = 'Enter an email address';
+            break;
+        case !filter_var($email, FILTER_VALIDATE_EMAIL):
+            $err_email = 'Enter a valid email address';
+            break;
+        case email_exists($email):
+            $err_email = 'This email already exists';
+            break;
+        case !is_gov_email($email):
+            $err_email = 'Enter an MoJ email address';
+            break;
+        default:
+            $err_email = '';
+    }
+
+    if ($err_name == '' && $err_email == '') {
+        $create_user = wp_insert_user([
                 'first_name' => apply_filters('pre_user_name', $first_name),
-                'user_pass'  => apply_filters('pre_user_user_pass', $pwd1),
+                'user_pass' => apply_filters('pre_user_user_pass', wp_generate_password()),
                 'user_login' => apply_filters('pre_user_user_login', $username),
                 'user_email' => apply_filters('pre_user_user_email', $email),
-                'role'       => 'subscriber',
-            )
-        );
+                'role' => 'subscriber'
+        ]);
+
         if (is_wp_error($create_user)) {
-            $err = '<p class="error-message">Error on user creation</p>';
+            $err = '<p class="error-message">There was an error creating your account.</p>';
         } else {
             do_action('user_register', $create_user);
 
-            $to = $email;
+            $user = get_user_by('email', $email);
+            $rp_key = get_password_reset_key($user);
+            $user_name = $user->display_name;
+            $rp_link = network_site_url(
+                "wp-login.php?action=rp&key=$rp_key&login=" . rawurlencode($user->user_login),
+                'login'
+            );
 
-            $user           = get_user_by('email', $email);
-            $user_id_number = $user->ID;
-            $rp_key         = get_password_reset_key($user);
-            $user_login     = $user->user_login;
-            $rp_link        = '<a style="display:inline-block;padding:8px 15px 5px;background-color:#00823b;color:#ffffff;font-size:19px;font-family:Arial,sans-serif;line-height:25px;text-decoration:none;vertical-align:top" href="' . network_site_url("wp-login.php?action=rp&key=$rp_key&login=" . rawurlencode($user_login), 'login') . '"> Reset Password </a>';
+            add_filter('intranet_mail_templates', function ($templates) use ($rp_link, $user_name) {
+                $template = $templates['email']['comment-registration'];
+                $template['personalisation']['name'] = $user_name;
+                $template['personalisation']['reply_link'] = $rp_link;
+                return $template;
+            }, 10, 1);
 
-            $subject = 'You are now registered on the MoJ intranet';
-            $body    =
-                '<div style="background-color:black">
-						<p style="color:#fff">
-						<img src="https://peoplefinder.service.gov.uk/assets/moj_logo_horizontal_36x246-90c698afdefe7275f7580065062aebc6.png" alt="Ministry of Justice" height="36px" style="padding:20px 40px" class="CToWUd">
-						</p>
-					</div>
-					<p style="padding:5px 0;font-size:19px;font-family:Arial,sans-serif">Hello,</p>
-					<p style="padding:5px 0;font-size:19px;font-family:Arial,sans-serif">To add your comments to the intranet, just click on the button below and finish the registration</p>' .
-                $rp_link .
-                '<br/>
-					<p style="padding:5px 0;font-size:19px;font-family:Arial,sans-serif">This will take you to the intranet reset password page where you need to set your password.</p>
-					<p style="padding:5px 0;font-size:19px;font-family:Arial,sans-serif"><strong>Any problems?</strong></p>
-					<p style="padding:5px 0;font-size:19px;font-family:Arial,sans-serif">If this link has expired, you’ll need to fill in your details again to get another link. If you don’t want to comment on the intranet, ignore this email.</p>
-					<p style="padding:25px 0 5px;font-size:16px;font-family:Arial,sans-serif;color:#6f777b">This email is generated automatically. Do not reply.</p>
-					<div style="background-color:#dee0e2">
-						<p style="padding:20px;font-size:16px;font-family:Arial,sans-serif">
-							If you\'re unsure an email is from the MoJ, forward it to <a href="mailto:phishing@digital.justice.gov.uk" target="_blank">phishing@digital.justice.gov.<wbr>uk</a>.
-						</p>
-          			</div>';
-            $headers = array( 'Content-Type: text/html; charset=UTF-8' );
+            wp_mail($email, 'default', 'default');
 
-            wp_mail($to, $subject, $body, $headers);
-
-            $success = 'You\'re successfully registered';
+            $success = true;
         }
-    } else { 
+    } else {
         if ($err_name != '') {
             $err .= "<p><a href='#name-error' class='error-message'>$err_name</a></p>";
         }
+
         if ($err_email != '') {
             $err .= "<p><a href='#email-error' class='error-message'>$err_email</a></p>";
         }
@@ -96,61 +97,64 @@ if (isset($_POST['task']) && $_POST['task'] == 'register') {
     <!--display error/success message-->
 
     <?php
-    if (! empty($err)) :
-        echo '<div id="message" class="error" aria-labelledby="error-summary-title" role="alert">';
-        echo '<h2 id="error-summary-title" class="error-title">There is a problem</h2>';
-        echo $err;
-        echo '</div>';
+    if (!empty($err)) :
+        echo '<div id="message" class="error" aria-labelledby="error-summary-title" role="alert">' .
+                '<h2 id="error-summary-title" class="error-title">There is a problem</h2>' .
+                    $err .
+             '</div>';
     endif;
     ?>
 
     <?php
-    if (! empty($success)) :
+    if ($success) :
         ?>
-    <div id="message" class="success">
-        <p><strong>Now check your email</strong></p>
-        <p>We're sending an email to <?php echo $email; ?>. This can take up to 5 minutes.</p>
+        <div id="message" class="success">
+            <p><strong>Now check your email</strong></p>
+            <p>We're sending an email to <?php echo $email; ?>. This can take up to 5 minutes.</p>
 
-        <p>Open the email and click on the link. This will take you to the reset password page, where you would need to
-            finish the registration.</p>
+            <p>Open the email and click on the link. This will take you to the reset password page, where you would need
+                to
+                finish the registration.</p>
 
-        <p><strong>Any problems?</strong></p>
-        <p>The email will be from <a href="mailto:wordpress@digital.justice.gov.uk"
-                target="_blank">wordpress@digital.justice.gov.uk</a>.<p>
+            <p><strong>Any problems?</strong></p>
+            <p>The email will be from <a href="mailto:wordpress@digital.justice.gov.uk"
+                                         target="_blank">wordpress@digital.justice.gov.uk</a>.
+            <p>
 
-                <p>If you can’t find it, check your junk folder then add the address to your safe list.
+            <p>If you can’t find it, check your junk folder then add the address to your safe list.
             </p>
 
             <p>Do not reply to the email.</p>
-    </div>
-        <?php
+        </div>
+    <?php
     endif;
     ?>
 
     <p>Fill in your details. We’ll then send you a link back to this page so you can start commenting.</p>
 
     <form method="post" action="?#respond" novalidate>
-        <div <?php if ($err_name !='') echo 'class="error-state"'; ?>>
+        <div <?php if ($err_name != '') echo 'class="error-state"'; ?>>
             <p class="label-paragraph"><label for="first_name">Screen name (will appear on screen)</label></p>
-            <?php if (trim($err_name) !='') {?>
+            <?php if (trim($err_name) != '') { ?>
                 <p id="name-error" class="error-message">
                     <span class="govuk-visually-hidden">Error:</span> <?php echo $err_name; ?>
                 </p>
             <?php } ?>
-            <p><input type="text" value="<?php echo $first_name;?>" name="first_name" id="first_name" /></p>
+            <p><input type="text" value="<?php echo $first_name; ?>" name="first_name" id="first_name"/></p>
         </div>
-        <div <?php if ($err_email !='') echo 'class="error-state"'; ?>>
-            <p class="label-paragraph"><label for="email">Email address (will not be shown with your comment)</label></p>
-            <?php if ($err_email !='') {?>
+        <div <?php if ($err_email != '') echo 'class="error-state"'; ?>>
+            <p class="label-paragraph"><label for="email">Email address (will not be shown with your comment)</label>
+            </p>
+            <?php if ($err_email != '') { ?>
                 <p id="email-error" class="error-message">
                     <span class="govuk-visually-hidden">Error:</span> <?php echo $err_email; ?>
                 </p>
             <?php } ?>
-            <p><input type="email" value="<?php echo $email;?>" name="email" id="email" /></p>
-        </div>    
+            <p><input type="email" value="<?php echo $email; ?>" name="email" id="email"/></p>
+        </div>
         <button type="submit" name="btnregister" class="button">Register</button>
-        <input type="hidden" name="task" value="register" />
-        
+        <input type="hidden" name="task" value="register"/>
+
     </form>
 </div>
 <!-- c-register ends here -->
