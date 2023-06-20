92a424832c443c588895cecae504cec31f552b4d
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2023-06-20 17:10:01 +0100

    Include users/add-notes-from-antonia.php

--- web/app/themes/clarity/functions.php
+++ web/app/themes/clarity/functions.php
@@ -14,7 +14,6 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
     require_once 'inc/commands/NotesFromAntonia.php';
 }
 
-
 require_once 'inc/admin/acf-field-group.php';
 require_once 'inc/admin/admin-commands.php';
 
@@ -41,6 +40,7 @@ require_once 'inc/admin/remove-customizer.php';
 require_once 'inc/admin/suppress-wp-update-msg.php';
 require_once 'inc/admin/tinymce-editor-settings.php';
 require_once 'inc/admin/users/add-acf-capabilities.php';
+require_once 'inc/admin/users/add-notes-from-antonia.php';
 require_once 'inc/admin/users/add-agency-admin.php';
 require_once 'inc/admin/users/add-agency-editor.php';
 require_once 'inc/admin/users/add-regional-editor.php';
@@ -95,6 +95,7 @@ require_once 'inc/post-types/regional-news.php';
 require_once 'inc/post-types/regional-page.php';
 require_once 'inc/post-types/team-area.php';
 require_once 'inc/post-types/webchat.php';
+require_once 'inc/post-types/notes-from-antonia.php';
 
 require_once 'inc/preselect.php';
 require_once 'inc/rewrite_rules.php';
--- web/app/themes/clarity/inc/post-types/notes-from-antonia.php
+++ web/app/themes/clarity/inc/post-types/notes-from-antonia.php
@@ -1,13 +1,11 @@
 <?php
-// Define news post type
-
 add_action('init', function () {
     register_post_type('note-from-antonia', array(
         'labels' => array(
             'name' => 'Notes from Antonia',
             'singular_name' => 'Note',
             'menu_name' => 'Notes from Antonia',
-            'all_items' => 'All Notes from Antonia',
+            'all_items' => 'Notes from Antonia',
             'edit_item' => 'Edit Note',
             'view_item' => 'View Note',
             'view_items' => 'View Notes from Antonia',
@@ -19,7 +17,6 @@ add_action('init', function () {
             'not_found_in_trash' => 'No notes from antonia found in Trash',
             'archives' => 'Note Archives',
             'attributes' => 'Note Attributes',
-            'insert_into_item' => 'Insert into note',
             'uploaded_to_this_item' => 'Uploaded to this note',
             'filter_items_list' => 'Filter notes from antonia list',
             'filter_by_date' => 'Filter notes from antonia by date',
@@ -37,20 +34,128 @@ add_action('init', function () {
         'public' => true,
         'show_in_rest' => true,
         'rest_base' => 'notes-from-antonia',
-        'menu_position' => 3,
-        'menu_icon' => 'dashicons-welcome-write-blog',
-        'has_archive' => true,
-        'taxonomies' => array('agency'),
-        'supports' => array(
-            0 => 'title',
-            1 => 'editor',
-            2 => 'revisions',
-            3 => 'thumbnail',
-        ),
+        'show_in_menu' => false,
+        'supports' => [
+            'title',
+            'editor',
+            'revisions'
+        ],
         'rewrite' => array(
             'slug' => 'notes-from-antonia',
             'with_front' => false,
         ),
         'delete_with_user' => false,
+        'capability_type' => [
+            'note_from_antonia',
+            'notes_from_antonia'
+        ],
+        'map_meta_cap' => true
     ));
+
+    if (!Agency_Context::current_user_can_have_context()) {
+        return false;
+    }
+
+    if (Agency_Context::get_agency_context() === 'hq') {
+        add_action('admin_menu', function () {
+            add_menu_page(
+                __('Notes from Antonia', 'clarity'),
+                'Notes from Antonia',
+                'notes_from_antonia',
+                'edit.php?post_type=note-from-antonia',
+                '',
+                'dashicons-welcome-write-blog',
+                1
+            );
+
+            add_submenu_page(
+                'edit.php?post_type=note-from-antonia',
+                'Add  Note',
+                'Add New Note',
+                'notes_from_antonia',
+                'post-new.php?post_type=note-from-antonia'
+            );
+        });
+    }
 });
+
+// Hook into this post_type, we need to detect
+// new notes and apply the agencies that have
+// access to the main page...
+/**
+ * Copy tagged agencies from 'Notes from Antonia' page to individual Notes.
+ *
+ * Agencies have the ability to include content on their own Intranets. If they
+ * choose Notes from Antonia then each individual Note will need to reflect
+ * this, otherwise it won't show up in search results for them.
+ *
+ * @param null $note
+ */
+function moj_intranet_copy_agencies_to_notes($note = null)
+{
+    $agencies = [];
+    $notes = [$note];
+
+    // get agencies attached to the page
+    // this is our source of truth...
+    $page = get_page_by_path('notes-from-antonia');
+    foreach (wp_get_object_terms($page->ID, 'agency') as $agency) {
+        $agencies[] = $agency->slug;
+    }
+
+    if (!$note) {
+        // get all notes
+        $notes = get_posts([
+            'post_type' => 'note-from-antonia',
+            'numberposts' => -1
+        ]);
+    }
+
+    foreach ($notes as $note) {
+
+        // check if agencies match the current saved agencies...
+        $terms = get_the_terms($note->ID, 'agency');
+        $agencies_current = [];
+        foreach ($terms as $agency) {
+            $agencies_current[] = $agency->slug;
+        }
+
+        // we are checking if the agency arrays are different
+        // if they are, we will make changes, otherwise, do nothing.
+        if (!empty(array_diff($agencies, $agencies_current))) {
+
+            // set as defined
+            $terms = wp_set_object_terms($note->ID, $agencies, 'agency');
+
+            if (is_wp_error($terms)) {
+                trigger_error("Terms could not be added for a note with an ID of: " . $note->ID);
+            }
+        }
+    }
+}
+
+// Hook into wp_insert_post action.
+// This will fire on new post open, save, publish, update
+add_action('wp_insert_post', function ($note_id, $note) {
+
+    if ($note->post_type === 'note-from-antonia') {
+        moj_intranet_copy_agencies_to_notes($note);
+    }
+}, 10, 2);
+
+
+// let's create a task to keep agencies up to date
+// create custom schedule in case it doesn't exist
+add_filter('cron_schedules', function ($schedules) {
+    $schedules['one_minute'] = [
+        'interval' => 60,
+        'display' => esc_html__('Every Minute')
+    ];
+    return $schedules;
+});
+
+add_action('notes_from_antonia_cron_hook', 'moj_intranet_copy_agencies_to_notes');
+
+if (!wp_next_scheduled('notes_from_antonia_cron_hook')) {
+    wp_schedule_event(time(), 'one_minute', 'notes_from_antonia_cron_hook');
+}
