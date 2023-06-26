87ab91ff2a7265f2d53d93c4c0bbafd9abf2b05b
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2023-06-26 17:16:53 +0100

    Loose AB example code

--- web/app/themes/clarity/src/components/c-search-results-filter/view.php
+++ web/app/themes/clarity/src/components/c-search-results-filter/view.php
@@ -11,37 +11,97 @@ $document = $selected_value === 'document';
 $news = $selected_value === 'news';
 $post = $selected_value === 'post';
 $event = $selected_value === 'event';
+$note = $selected_value === 'note-from-antonia';
 
 $prefix = 'srf';
 $oAgency = new Agency();
 $activeAgency = $oAgency->getCurrentAgency();
+
+/**
+ * A/B testing for new layout
+ */
+$is_show_new_layout = $_GET['ab_test'] ?? false;
+$is_show_new_layout = $is_show_new_layout || ($_COOKIE['moj_ab_radio_filter_test'] ?? false);
+
+/**
+ * We are having trouble setting this cookie; we should
+ * set it earlier with a hook to init - detect the
+ * search page first:
+ * if (is_search_page()) {
+ *     add_action('init', 'set_the_cookie');
+ * }
+ */
+$did_cookie_save = true;
+if ($is_show_new_layout) {
+    $did_cookie_save = setcookie('moj_ab_radio_filter_test', 1, time() + 60 * 60 * 24 * 30);
+}
+
 ?>
 <!-- c-search-results-filter starts here -->
 <section class="c-search-results-filter c-content-filter">
     <p>You are searching across <strong><?php esc_attr_e($activeAgency['abbreviation']); ?></strong>. To search another
         agency use <a href="/agency-switcher/">agency switcher</a>.</p>
-    <form action="<?php echo esc_url(home_url('/')); ?>" method="get" id="<?php echo $prefix; ?>" class="u-wrapper"
-          id="searchform"/>
-    <?php
-    $placeholder = 'Search' . $activeAgency['abbreviation'] . 'intranet';
-    $keyword_query = get_search_query();
 
-    // Keyword input field
-    form_builder('text', '', false, 's', null, $keyword_query, $placeholder, null, false, null, null);
+    <form action="<?= esc_url(home_url('/')) ?>" method="get" id="<?= $prefix ?>" class="u-wrapper" id="searchform">
+        <?php
+        $placeholder = 'Search' . $activeAgency['abbreviation'] . 'intranet';
+        $keyword_query = get_search_query();
 
-    // Dropdown options field
-    $select_options = [
-        ['All', 'any', $any],
-        ['Blogs', 'post', $post],
-        ['Documents &amp; forms', 'document', $document],
-        ['Events', 'event', $event],
-        ['News', 'news', $news],
-        ['Pages', 'page', $page],
-    ];
-    form_builder('select', '', 'Filter by', 'post_types', null, $selected_value, null, null, false, null, $select_options);
-    ?>
+        // Keyword input field
+        form_builder('text', '', false, 's', null, $keyword_query, $placeholder);
 
-    <input type="submit" value="Filter" id="ff_button_submit"/>
+        if ($is_show_new_layout) {
+
+            echo '<input type="submit" value="Search" id="ff_button_submit"/><br><br><br><hr><br>';
+
+            form_builder(
+                'radio-group',
+                '',
+                'Filter by',
+                'post_types',
+                null,
+                $selected_value,
+                null,
+                'js-radios-onChange',
+                false,
+                null,
+                [
+                    ['All', 'any', $any],
+                    ['Pages', 'page', $page],
+                    ['Documents &amp; forms', 'document', $document],
+                    ['News', 'news', $news],
+                    ['Blogs', 'post', $post],
+                    ['Events', 'event', $event]
+                ]
+            );
+
+        } else {
+
+            form_builder(
+                'select',
+                '',
+                'Filter by',
+                'post_types',
+                null,
+                $selected_value,
+                null,
+                null,
+                false,
+                null,
+                [
+                    ['All', 'any', $any],
+                    ['Blogs', 'post', $post],
+                    ['Documents &amp; forms', 'document', $document],
+                    ['Events', 'event', $event],
+                    ['News', 'news', $news],
+                    ['Pages', 'page', $page]
+                ]
+            );
+
+            echo '<input type="submit" value="Filter" id="ff_button_submit"/>';
+        }
+
+        ?>
     </form>
 </section>
 <!-- c-search-results-filter ends here -->
