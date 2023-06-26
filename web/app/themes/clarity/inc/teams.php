6b2c801e1ccbcc70c1ae2ee96182dbcdb4bf04fa
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2023-06-26 17:17:31 +0100

    Cleaner teams code

--- web/app/themes/clarity/inc/teams.php
+++ web/app/themes/clarity/inc/teams.php
@@ -1,133 +1,82 @@
 <?php
+
 namespace MOJ\Intranet;
 
 /**
  * Retrieves and returns guidance and form related data
  */
-
 class Teams
 {
+    /**
+     * 'http://127.0.0.1' is a temporary measure so that API calls do not get blocked by
+     * changing IPs not whitelisted. All calls are within container.
+     */
+    public string $url = 'http://127.0.0.1';
 
-    private $page_meta;
+    public function __construct(){}
 
-    public function __construct()
-    {
-        $this->page_meta = [
-            'post_id'  => get_the_ID(),
-            'agency'   => get_intranet_code(),
-            'home_url' => get_home_url(),
-        ];
-    }
-
-     /**
-      *
-      * Team News API
-      *
-      * @param
-      * @return
-      */
+    /**
+     * Team News API
+     *
+     * @param
+     * @return int|mixed|void
+     */
     public function team_news_api($number)
     {
-        $post_per_page = 'per_page=' . $number;
-        $current_page  = '&page=1';
-        $agency_name   = '&agency=' . $this->page_meta['agency'];
+        return $this->remote_get('news', [
+            'per_page' => $number,
+            'page' => 1
+        ]);
+    }
 
-        /*
-        * A temporary measure so that API calls do not get blocked by
-        * changing IPs not whitelisted. All calls are within container.
-        */
-        $siteurl = 'http://127.0.0.1';
-
-        $response = wp_remote_get($siteurl . '/wp-json/wp/v2/team-news/?' . $post_per_page . $current_page);
-
-        if (is_wp_error($response)) :
-            return;
-        endif;
-
-        $pagetotal        = wp_remote_retrieve_header($response, 'x-wp-totalpages');
-        $posts            = json_decode(wp_remote_retrieve_body($response), true);
-        $response_code    = wp_remote_retrieve_response_code($response);
-        $response_message = wp_remote_retrieve_response_message($response);
-
-        if (200 == $response_code && $response_message == 'OK') :
-            return $posts;
-        else :
-                return 0;
-        endif;
-    } // END team_news_api()
-
-     /**
-      *
-      * Team Blog API
-      *
-      * @param
-      * @return
-      */
+    /**
+     * Team Blog API
+     *
+     * @param
+     * @return int|mixed|void
+     */
     public function team_blog_api($number)
     {
-        $post_per_page = 'per_page=' . $number;
-        $current_page  = '&page=1';
-        $agency_name   = '&agency=' . $this->page_meta['agency'];
+        return $this->remote_get('blogs', [
+            'per_page' => $number,
+            'page' => 1
+        ]);
+    }
 
-        /*
-        * A temporary measure so that API calls do not get blocked by
-        * changing IPs not whitelisted. All calls are within container.
-        */
-        $siteurl = 'http://127.0.0.1';
-
-        $response = wp_remote_get($siteurl . '/wp-json/wp/v2/team-blogs/?' . $post_per_page . $current_page);
-
-        if (is_wp_error($response)) :
-            return;
-        endif;
-
-        $pagetotal        = wp_remote_retrieve_header($response, 'x-wp-totalpages');
-        $posts            = json_decode(wp_remote_retrieve_body($response), true);
-        $response_code    = wp_remote_retrieve_response_code($response);
-        $response_message = wp_remote_retrieve_response_message($response);
-
-        if (200 == $response_code && $response_message == 'OK') :
-            return $posts;
-        else :
-                return 0;
-        endif;
-    } // END team_blog_api()
-
-     /**
-      *
-      * Team Events API
-      *
-      * @param
-      * @return
-      */
+    /**
+     * Team Events API
+     *
+     * @param
+     * @return int|mixed|void
+     */
     public function team_events_api($number)
     {
-        $post_per_page = 'per_page=' . $number;
-        $current_page  = '&page=1';
-        $orderby       = '&meta_key=event-start-date';
-        $order         = '&order=desc';
+        return $this->remote_get('events', [
+            'per_page' => $number,
+            'page' => 1,
+            'meta_key' => 'event-start-date', // orderby
+            'order' => 'desc'
+        ]);
+    }
 
-        /*
-        * A temporary measure so that API calls do not get blocked by
-        * changing IPs not whitelisted. All calls are within container.
-        */
-        $siteurl = 'http://127.0.0.1';
+    /**
+     * @param $type
+     * @param $queries
+     * @return int|mixed|void
+     */
+    public function remote_get($type, $queries)
+    {
+        $response = wp_remote_get($this->url . '/wp-json/wp/v2/team-' . $type . '/?' . http_build_query($queries));
 
-        $response = wp_remote_get($siteurl . '/wp-json/wp/v2/team-events/?' . $post_per_page . $current_page . $orderby . $order);
-
-        if (is_wp_error($response)) :
+        if (is_wp_error($response)) {
             return;
-        endif;
+        }
 
-        $pagetotal        = wp_remote_retrieve_header($response, 'x-wp-totalpages');
-        $posts            = json_decode(wp_remote_retrieve_body($response), true);
-        $response_code    = wp_remote_retrieve_response_code($response);
+        wp_remote_retrieve_header($response, 'x-wp-totalpages');
+        $posts = json_decode(wp_remote_retrieve_body($response), true);
+        $response_code = wp_remote_retrieve_response_code($response);
         $response_message = wp_remote_retrieve_response_message($response);
 
-        if (200 == $response_code && $response_message == 'OK') :
-            return $posts;
-        else :
-                return 0;
-        endif;
-    } // END team_events_api()
+        return (200 == $response_code && $response_message == 'OK') ? $posts : 0;
+    }
 }
