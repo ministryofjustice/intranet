13417f598a6555e939f6025739a232b72aa6a029
Damien Wilson <47327051+wilson1000-MoJ@users.noreply.github.com>
2023-07-04 09:21:47 +0100

    CDPT 790 Fix duplicate breadcrumb path (#437)
    * Add revisions to region_pages
    
    * Selectively drop a parent path
    
    * Improve function syntax
    
    * Flex tabbed content tabs
    
    * Remove overly complex comments

--- web/app/themes/clarity/src/components/c-breadcrumbs/view-region.php
+++ web/app/themes/clarity/src/components/c-breadcrumbs/view-region.php
@@ -5,49 +5,47 @@
 * hierarchy from homepage (news, event, blog singles)
 */
 
-function get_breadcrumb()
+function get_breadcrumb(): string
 {
-
     global $post;
 
-    $trail      = '';
-    $page_title = get_the_title($post->ID);
+    $trail = '';
+    $breadcrumbs = [];
 
     if ($post->post_parent) {
         $parent_id = $post->post_parent;
 
         while ($parent_id) {
-            $page          = get_page($parent_id);
-            $breadcrumbs[] = '<li class="c-breadcrumbs__list-item c-breadcrumbs__list-item--separated"><a href="' . get_the_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a></li>';
-            $parent_id     = $page->post_parent;
+            $page = get_post($parent_id);
+            $breadcrumbs[] = '<li class="c-breadcrumbs__list-item c-breadcrumbs__list-item--separated">
+                <a href="' . get_the_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a>
+            </li>';
+
+            $parent_id = $page->post_parent;
         }
 
-        $breadcrumbs = array_reverse($breadcrumbs);
-        foreach ($breadcrumbs as $crumb) {
-            $trail .= $crumb;
-        }
+        $trail = implode('', array_reverse($breadcrumbs));
     }
 
-    $trail .= '<li class="c-breadcrumbs__list-item c-breadcrumbs__list-item--separated">' . $page_title . "</li>";
-
-    return $trail;
+    return $trail . '<li class="c-breadcrumbs__list-item c-breadcrumbs__list-item--separated">' . get_the_title($post->ID) . "</li>";
 }
+
 ?>
 
-  <!-- c-breadcrumbs starts here -->
-  <section class="c-breadcrumbs">
+<!-- c-breadcrumbs [region] starts here -->
+<section class="c-breadcrumbs">
     <ol class="c-breadcrumbs__list">
-      <li class="c-breadcrumbs__list-item">
-        <a title="Go home." href="<?php echo get_home_url(); ?>" class="home">
-          <span>Home</span>
-        </a>
-      </li>  
-      <li class="c-breadcrumbs__list-item c-breadcrumbs__list-item--separated">
-        <a href="/regional-pages/">
-          <span>Regions</span>
-        </a>
-      </li>
-      <?php echo get_breadcrumb(); ?>
+        <li class="c-breadcrumbs__list-item">
+            <a title="Go home." href="<?php echo get_home_url(); ?>" class="home">
+                <span>Home</span>
+            </a>
+        </li>
+        <li class="c-breadcrumbs__list-item c-breadcrumbs__list-item--separated">
+            <a href="/regional-pages/">
+                <span>Regions</span>
+            </a>
+        </li>
+        <?php echo get_breadcrumb(); ?>
     </ol>
-  </section>
-  <!-- c-breadcrumbs ends here -->
+</section>
+<!-- c-breadcrumbs ends here -->
