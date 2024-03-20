4689ca433076e01b5693d0bbd0a80158e66d65f2
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-03-20 15:48:20 +0000

    Update agency switcher add comment at top.

---
+++ web/app/themes/clarity/page_agency-switcher.php
@@ -0,0 +1,17 @@
+<?php
+
+/***
+ * Template name: Agency switcher
+ */
+
+get_header();
+?>
+
+  <main id="maincontent" class="u-wrapper l-main t-agency-switcher" role="main">
+    <h1 class="o-title o-title--page">Choose your agency or body</h1>
+    <p>Other agencies and bodies have their own specific intranet content available to view by visiting the links below. HMPPS and YJB links are external intranet websites not managed by this central MoJ intranet.</p>
+    <?php get_template_part('src/components/c-intranet-switcher/view'); ?>
+  </main>
+
+<?php
+get_footer();
