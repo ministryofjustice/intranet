2cf06c5f174f1b66f9f3f5f91b604c3759b8c47c
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-03-20 16:05:15 +0000

    Ensure that the template for the agency-switcher page is persisted.

--- web/app/themes/clarity/inc/admin/page.php
+++ web/app/themes/clarity/inc/admin/page.php
@@ -7,3 +7,10 @@ function add_page_excerpts()
 {
     add_post_type_support('page', 'excerpt');
 }
+
+// Ensure that the template for the agency-switcher page is persisted.
+add_action('save_post', function ($post_id, $post) {
+    if ($post->post_name === 'agency-switcher') {
+        update_post_meta($post_id, '_wp_page_template', 'agency-switcher.php');
+    }
+}, 99, 2);
