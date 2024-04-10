1def531b235f13a219c1ab481c19de1797b59f04
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-04-10 15:49:22 +0100

    Use script with env_vars instead of inline command

---
+++ deploy/config/cron/wp-cron-exec.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# set hosts
+ENV_HOST=$1    # raw hostname of the container
+NGINX_HOST=$2  # hostname of the nginx service
+HOST_TEST=$(contains "$ENV_HOST" "-prod")
+
+curl_it() {
+  curl http://"$NGINX_HOST":8080/wp/wp-cron.php --silent
+}
+
+contains() {
+    string="$1"
+    substring="$2"
+    if [ "${string#*"$substring"}" != "$string" ]; then
+        return 0    # in $string
+    else
+        return 1    # not in $string
+    fi
+}
+
+if [ "$HOST_TEST" = 0 ]; then
+  curl_it
+else
+  NOW=$(date +"%H")
+  if [ "$NOW" -gt "6" ] && [ "$NOW" -lt "22" ]; then
+    curl_it
+  fi
+fi
