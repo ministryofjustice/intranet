981ce3f8c613de624565cc876f589118f0b48d1c
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-04-10 15:52:18 +0100

    Change logical order

--- deploy/config/cron/wp-cron-exec.sh
+++ deploy/config/cron/wp-cron-exec.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# set hosts
-ENV_HOST=$1    # raw hostname of the container
-NGINX_HOST=$2  # hostname of the nginx service
-HOST_TEST=$(contains "$ENV_HOST" "-prod")
-
 curl_it() {
   curl http://"$NGINX_HOST":8080/wp/wp-cron.php --silent
 }
@@ -18,6 +13,11 @@ contains() {
         return 1    # not in $string
     fi
 }
+
+# set hosts
+ENV_HOST=$1    # raw hostname of the container
+NGINX_HOST=$2  # hostname of the nginx service
+HOST_TEST=$(contains "$ENV_HOST" "-prod")
 
 if [ "$HOST_TEST" = 0 ]; then
   curl_it
