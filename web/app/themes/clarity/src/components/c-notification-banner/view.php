4291875b71046ac7af093df35e9d6e3e2b60f75f
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-06-28 14:44:52 +0100

    Add general use component for notification banner. Add that template before rich text block.

---
+++ web/app/themes/clarity/src/components/c-notification-banner/view.php
@@ -0,0 +1,45 @@
+<?php
+
+defined('ABSPATH') || exit;
+
+if (empty($args['heading'])) {
+    return;
+}
+
+$variants = [
+    'default' => [
+        'label' => 'Information'
+    ],
+    'information' => [
+        'label' => 'Information',
+        'icon' => 'information'
+    ],
+    'success' => [
+        'label' => 'Success',
+        'icon' => 'success'
+    ],
+    'warning' => [
+        'label' => 'Warning',
+        'icon' => 'warning'
+    ]
+];
+
+$variant_with_default = in_array($args['variant'], array_keys($variants)) ? $args['variant'] : 'default';
+
+$variant_values = $variants[$variant_with_default];
+
+?>
+
+<!-- c-moj-banner starts here -->
+<section class="c-moj-banner c-moj-banner--<?= $variant_with_default ?>" role="region" aria-label="<?= $variant_values['label'] ?>">
+    <?php
+    if ($variant_values['icon']) {
+        require 'icons/' . $args['variant'] . '.php';
+    }
+    ?>
+    <div class="c-moj-banner__message">
+        <h2 class="o-title"><?= $args['heading']; ?></h2>
+    </div>
+</section>
+<!-- c-moj-banner ends here -->
+ 
