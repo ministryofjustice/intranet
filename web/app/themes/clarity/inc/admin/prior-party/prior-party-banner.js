e08e57466a67621b507aee2a09e7c91da8e29d83
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-06-28 15:05:30 +0100

    Add and remove posts

--- web/app/themes/clarity/inc/admin/prior-party/prior-party-banner.js
+++ web/app/themes/clarity/inc/admin/prior-party/prior-party-banner.js
@@ -6,8 +6,27 @@ const MOJ_PPB = {
 }
 
 jQuery(document).ready(function ($) {
+    /**
+     * React to clicks on banners, redirect to preview
+     */
     $('.ppb-banners__row').on('click', function (e) {
         // redirect to post preview...
         window.location.href = window.location.href+"&"+$.param({'ref':$(this).data('reference')})
+    });
+
+    /**
+     * react to clicks on Post table, add the ID to the exclude array
+     */
+    $('.ppb-posts__row').on('click', function (e) {
+        const post_id = $(this).data('id');
+        const index = $.inArray(post_id, MOJ_PPB.excluded);
+
+        if (index !== -1) {
+            MOJ_PPB.excluded.splice(index, 1);
+        } else {
+            MOJ_PPB.excluded.push(post_id);
+        }
+
+        console.log(MOJ_PPB.excluded);
     })
 })
