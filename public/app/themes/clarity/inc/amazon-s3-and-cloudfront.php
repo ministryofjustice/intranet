6b711d4c0115295c4b770942b60ef16bc63bf675
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-07-16 12:43:55 +0100

    Increase batch sizes

---
+++ public/app/themes/clarity/inc/amazon-s3-and-cloudfront.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DeliciousBrains\WP_Offload_Media\Tweaks;
+
+/**
+ * Amazon S3 and CloudFront tweaks.
+ * 
+ * A class for functions related to the Amazon S3 and CloudFront.
+ * Functions related to cookie signing and Minio belong in the adjacent files.
+ */
+
+class AmazonS3AndCloudFrontTweaks
+{
+
+    public function __construct()
+    {
+        // Increase limits from 50 to 5000. 
+        add_filter( 'as3cf_update_replace_provider_urls_batch_size', fn() => 5000);
+        add_filter( 'as3cf_update_filter_post_excerpt_batch_size', fn() => 5000);
+
+        // Increase limit from 50 to 750.
+        add_filter( 'as3cf_update_as3cf_items_table_batch_size', fn() => 750);
+        // 750 items take ~20 secs, so decrease interval from 2 to 1 minute.
+        add_filter( 'as3cf_update_as3cf_items_table_interval', fn() => 1);
+    }
+
+}
+
+new AmazonS3AndCloudFrontTweaks();
