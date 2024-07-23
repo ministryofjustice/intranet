513d267fa25bdaf9a5f91adc2bede55d113c38ed
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-07-23 14:56:03 +0100

    Increase fix_broken_item_extra_data batch size

--- public/app/themes/clarity/inc/amazon-s3-and-cloudfront.php
+++ public/app/themes/clarity/inc/amazon-s3-and-cloudfront.php
@@ -17,6 +17,7 @@ class AmazonS3AndCloudFrontTweaks
         // Increase limits from 50 to 5000. 
         add_filter( 'as3cf_update_replace_provider_urls_batch_size', fn() => 5000);
         add_filter( 'as3cf_update_filter_post_excerpt_batch_size', fn() => 5000);
+        add_filter( 'as3cf_update_fix_broken_item_extra_data_batch_size', fn() => 2500);
 
         // Increase limit from 50 to 750.
         add_filter( 'as3cf_update_as3cf_items_table_batch_size', fn() => 750);
