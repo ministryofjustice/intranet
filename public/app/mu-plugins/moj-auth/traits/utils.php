a37f3a676a00ee9d4cdfa423020ae66c19a9f01d
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-07-26 17:25:45 +0100

    Remove previous IP_RANGES, use nginx init script to populate conf file.

--- public/app/mu-plugins/moj-auth/traits/utils.php
+++ public/app/mu-plugins/moj-auth/traits/utils.php
@@ -29,68 +29,6 @@ trait AuthUtils
     }
 
     /**
-     * Checks if a given IP address matches the specified CIDR subnet/s
-     * 
-     * @see https://gist.github.com/tott/7684443?permalink_comment_id=2108696#gistcomment-2108696
-     * 
-     * @param string $ip The IP address to check
-     * @param mixed $cidrs The IP subnet (string) or subnets (array) in CIDR notation
-     * @param string $match optional If provided, will contain the first matched IP subnet
-     * @return boolean TRUE if the IP matches a given subnet or FALSE if it does not
-     */
-
-    public function ipMatch($ip, $cidrs, &$match = null): bool
-    {
-        $this->log('ipMatch()');
-
-        foreach ((array) $cidrs as $cidr) {
-            if (empty($cidr)) {
-                continue;
-            }
-            $parts = explode('/', $cidr);
-            $subnet = $parts[0];
-            $mask = $parts[1] ?? 32;
-            if (((ip2long($ip) & ($mask = ~((1 << (32 - $mask)) - 1))) == (ip2long($subnet) & $mask))) {
-                $match = $cidr;
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-
-    /**
-     * Check if the IP address is allowed.
-     * 
-     * Checks that we have the environment variable ALLOWED_IPS and server property HTTP_X_REAL_IP set.
-     * Runs the ipMatch method to check if the HTTP_X_REAL_IP is in the ALLOWED_IPS.
-     * 
-     * @return bool Returns true if the IP address is allowed, otherwise false.
-     */
-
-    public function ipAddressIsAllowed(): bool
-    {
-        $this->log('ipAddressIsAllowed()');
-
-        return false;
-
-        if (empty($_ENV['ALLOWED_IPS']) || empty($_SERVER['HTTP_X_REAL_IP'])) {
-            return false;
-        }
-
-        $newline_pattern  = '/\r\n|\n|\r/'; // Match newlines.
-        $comments_pattern = '/\s*#.*/'; // Match comments.
-
-        $allowedIps = array_map(
-            'trim',
-            preg_split($newline_pattern, preg_replace($comments_pattern, '', $_ENV['ALLOWED_IPS']))
-        );
-
-        return $this->ipMatch($_SERVER['HTTP_X_REAL_IP'], $allowedIps);
-    }
-
-    /**
      * Hash a value using SHA256 and a salt.
      * 
      * @param string $value The value to hash.
