4f4c819aeadbee4bec8f3b4ec84566c70f42e1a1
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-07-29 15:53:06 +0100

    Update auth-heartbeat.js

--- public/app/themes/clarity/src/globals/js/auth-heartbeat.js
+++ public/app/themes/clarity/src/globals/js/auth-heartbeat.js
@@ -1,43 +1,6 @@
 export default (function ($) {
-  $(function () {
-    $("body").append(`
-            <dialog>
-                <button autofocus>Close</button>
-                <p>This modal dialog has a groovy backdrop!</p>
-            </dialog>
-            <button>Show the dialog</button>
-          `);
-    console.log("ready!");
-
-    const dialog = document.querySelector("dialog");
-    const showButton = document.querySelector("dialog + button");
-    const closeButton = document.querySelector("dialog button");
-
-    // "Show the dialog" button opens the dialog modally
-    showButton.addEventListener("click", () => {
-    dialog.showModal();
-    });
-
-    // "Close" button closes the dialog
-    closeButton.addEventListener("click", () => {
-    dialog.close();
-    });
-
-  });
-
-  console.log("hi");
-
-  function showModal() {
-    console.log("in showModal");
-  }
-
-  // Send a request to the heartbeat endpoint, this will refresh the oauth token.
-  setInterval(function () {
-    jQuery.ajax({
-      url: "/auth/heartbeat",
-      error: function (xhr, status) {
-        showModal();
-      },
-    });
-  }, 10000);
-})(jQuery);
+    // Send a request to the heartbeat endpoint, this will refresh the oauth token.
+    setInterval(function(){
+        $.get( "/auth/heartbeat" )
+    }, 10000)
+})(jQuery)
