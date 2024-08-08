9a15b4dae64255e402ee91f15494c72127f25b55
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-08-08 10:13:34 +0100

    ACF: fix select2 formatting.

--- public/app/themes/clarity/inc/admin/js/acf.js
+++ public/app/themes/clarity/inc/admin/js/acf.js
@@ -4,25 +4,17 @@ jQuery(document).ready(function () {
   }
 
   // @see https://www.advancedcustomfields.com/resources/javascript-api/#filters-select2_escape_markup
-  acf.add_filter("select2_escape_markup", (escaped_value) => {
-    // Let's make use of select2 escaping, but reverse it for a few allowed elements.
+  acf.add_filter(
+    "select2_escape_markup",
+    (escaped_value, original_value, _$select, _settings, field) => {
+      // If the data type is post_object then the html is post title and date.
+      // It's not user input, so we can safely return the original_value here.
+      if (field.data("type") === "post_object") {
+        return original_value;
+      }
 
-    // Allowed elements
-    const allowedTags = ["br", "small"];
-
-    // Regex parts.
-    const openAngle = "&lt;";
-    const closingAngle = "&gt;";
-    // An optional closing slash.
-    const closingSlash = "(?<closingSlash>/?)";
-    // Name of the tag.
-    const tagName = `(?<tagName>${allowedTags.join("|")})`;
-
-    // Build up a regex expression, from the defined parts.
-    const expression = `${openAngle}${closingSlash}${tagName}${closingAngle}`;
-
-    var regexp = new RegExp(expression, "g");
-
-    return escaped_value.replace(regexp, "<$<closingSlash>$<tagName>>");
-  });
+      // Otherwise, return the escaped html.
+      return escaped_value;
+    },
+  );
 });
