ad04a230cf226666731f6c54a0c85341ee11c09d
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-08-12 11:01:05 +0100

    Remove meta-box from attachment page

--- public/app/themes/clarity/inc/admin/plugins/wp-offload-media.php
+++ public/app/themes/clarity/inc/admin/plugins/wp-offload-media.php
@@ -8,25 +8,56 @@ namespace MOJ\Intranet;
 
 class WPOffloadMedia
 {
+    /**
+     * Should we restrict the view for the current user?
+     *
+     * @var bool
+     */
+    var bool $view_restricted = false;
+
     public function __construct()
     {
+        // do early stuff here, outside WP ecosys...
+
+        // load hooks here, inside WP ecosys...
         $this->hooks();
     }
 
     public function hooks(): void
     {
-        add_filter('manage_media_columns', [$this, 'ManageMediaColumns'], 99, 1);
+        add_action('init', [$this, 'cannotView']);
+        add_filter('manage_media_columns', [$this, 'maybeRemoveColumns'], 99, 1);
+        add_action('add_meta_boxes_attachment', [$this, 'maybeRemoveMetaBoxes'], 99, 1);
     }
 
-    public function ManageMediaColumns($columns)
+    /**
+     * Determine if the current user should have a restricted view
+     *
+     * @return void
+     */
+    public function cannotView(): void
     {
         $user = wp_get_current_user();
 
         if (!user_can($user->ID, 'administrator')) {
+            $this->view_restricted = true;
+        }
+    }
+
+    public function maybeRemoveColumns($columns)
+    {
+        if ($this->view_restricted) {
             unset($columns['as3cf_bucket']);
             unset($columns['as3cf_access']);
         }
 
         return $columns;
+    }
+
+    public function maybeRemoveMetaBoxes($post): void
+    {
+        if ($this->view_restricted) {
+            remove_meta_box('s3-actions', 'attachment', 'side');
+        }
     }
 }
