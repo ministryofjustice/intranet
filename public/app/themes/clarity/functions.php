f90e33612aa9a676392ce804e3729d3a61cf3818
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-08-13 11:47:07 +0100

    Initialise ElasticPress hooks

--- public/app/themes/clarity/functions.php
+++ public/app/themes/clarity/functions.php
@@ -36,6 +36,7 @@ require_once 'inc/admin/page.php';
 require_once 'inc/admin/permission-display-page.php';
 require_once 'inc/admin/plugins/co-authors-plus.php';
 require_once 'inc/admin/plugins/polls.php';
+require_once 'inc/admin/plugins/wp-elasticsearch.php';
 require_once 'inc/admin/plugins/wp-offload-media.php';
 require_once 'inc/admin/prior-party/prior-party-banner-admin.php';
 require_once 'inc/admin/prior-party/prior-party-banner-email.php';
@@ -119,6 +120,7 @@ require_once 'inc/whitelisted-emails.php';
 
 new MOJ\Intranet\AdminBranding();
 new MOJ\Intranet\WPOffloadMedia();
+new MOJ\Intranet\WPElasticPress();
 
 /// Prevent the Agency Switcher page from being overwritten
 add_action('save_post', function ($post_id, $post) {
