2ad28bcefa6b183d12f29e473780833a6ccf6cb7
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-09-12 10:40:05 +0100

    Add comment block with table

--- config/wp-offload-media.php
+++ config/wp-offload-media.php
@@ -10,65 +10,65 @@ use Roots\WPConfig\Config;
 
 use function Env\env;
 
-$as3_settings = array(
-    // Storage Provider ('aws', 'do', 'gcp')
+/**
+ *  Setting                | Type    | Description
+ *  -----------------------|---------|---------------------------------------------------------------------------
+ *  provider               | string  | Storage Provider ('aws', 'do', 'gcp')
+ *  use-server-roles       | boolean | Use IAM Roles on Amazon Elastic Compute Cloud
+ *  bucket                 | string  | Bucket to upload files to
+ *  region                 | string  | Bucket region (e.g. 'eu-west-2 string)
+ *  copy-to-s3             | boolean | Automatically copy files to bucket on upload
+ *  enable-object-prefix   | boolean | Enable object prefix, useful if bucket used for other files
+ *  object-prefix          | string  | Object prefix to use if 'enable-object-prefix = 'true'
+ *  use-yearmonth-folders  | boolean | Organize bucket files into YYYY/MM directories
+ *  object-versioning      | boolean | Append a timestamp - avoids filename clashes
+ *  delivery-provider      | string  | One of: storage, aws, do, gcp, cloudflare, keycdn, stackpath, other
+ *  serve-from-s3          | boolean | Rewrite file URLs to bucket (s3 or cloudfront)
+ *  enable-delivery-domain | string  | Use a custom domain (CNAME) - not supported when delivery-provider = storage
+ *  delivery-domain        | string  | Custom domain (CNAME) - not supported when delivery-provider = storage
+ *  force-https            | boolean | Serve files over HTTPS
+ *  remove-local-file      | boolean | Remove the local file version once offloaded to bucket
+ *  use-bucket-acls        | boolean | Access Control List for the bucket
+ *  access-key-id          | string  | Access Key ID for Storage Provider (aws and do only, replace '*')
+ *  secret-access-key      | string  | Secret Access Key for Storage Providers (aws and do only, replace '*')
+ *
+ */
+$as3_settings = [
     'provider' => 'aws',
-    // Use IAM Roles on Amazon Elastic Compute Cloud (EC2) or Google Compute Engine (GCE)
     'use-server-roles' => true,
-    // Bucket to upload files to
     'bucket' => env('AWS_S3_BUCKET'),
-    // Bucket region (e.g. 'us-west-1' - leave blank for default region)
     'region' => 'eu-west-2',
-    // Automatically copy files to bucket on upload
     'copy-to-s3' => true,
-    // Enable object prefix, useful if you use your bucket for other files
     'enable-object-prefix' => true,
-    // Object prefix to use if 'enable-object-prefix' is 'true'
     'object-prefix' => 'media/',
-    // Organize bucket files into YYYY/MM directories matching Media Library upload date
     'use-yearmonth-folders' => true,
-    // Append a timestamped folder to path of files offloaded to bucket to avoid filename clashes and bust CDN cache if updated
     'object-versioning' => true,
-    // Delivery Provider ('storage', 'aws', 'do', 'gcp', 'cloudflare', 'keycdn', 'stackpath', 'other')
     'delivery-provider' => env('AWS_CLOUDFRONT_HOST') ? 'aws' : 'storage',
-    // Rewrite file URLs to bucket (s3 or cloudfront)
     'serve-from-s3' => true,
-    // Use a custom domain (CNAME), not supported when using 'storage' Delivery Provider
     'enable-delivery-domain' => !!env('AWS_CLOUDFRONT_HOST'),
-    // Custom domain (CNAME), not supported when using 'storage' Delivery Provider
     'delivery-domain' => env('AWS_CLOUDFRONT_HOST'),
-    // Enable signed URLs for Delivery Provider that uses separate key pair (currently only 'aws' supported, a.k.a. CloudFront)
-    // 'enable-signed-urls' => false,
-    // Access Key ID for signed URLs (aws only, replace '*')
-    // 'signed-urls-key-id' => '********************',
-    // Key File Path for signed URLs (aws only, absolute file path, not URL)
-    // Make sure hidden from public website, i.e. outside site's document root.
-    // 'signed-urls-key-file-path' => '/path/to/key/file.pem',
-    // Private Prefix for signed URLs (aws only, relative directory, no wildcards)
-    // 'signed-urls-object-prefix' => 'private/',
-    // Serve files over HTTPS
     'force-https' => env('AWS_CLOUDFRONT_SCHEME') !== 'http',
-    // Remove the local file version once offloaded to bucket
     'remove-local-file' => true,
-    // Access Control List for the bucket
-    'use-bucket-acls' => false,
-);
+    'use-bucket-acls' => false
+];
 
-// Merge in the access key and secret if they are set
+/**
+ * Manage storage access on different stacks.
+ * 
+ * Local development uses long-lived access keys and
+ * secrets where stacks in CI/CD use server roles.
+ */
 if (env('AWS_ACCESS_KEY_ID') && env('AWS_SECRET_ACCESS_KEY')) {
     unset($as3_settings['use-server-roles']);
 
     $as3_settings = array_merge($as3_settings, [
-        // Access Key ID for Storage Provider (aws and do only, replace '*')
         'access-key-id' => env('AWS_ACCESS_KEY_ID'),
-        // Secret Access Key for Storage Providers (aws and do only, replace '*')
-        'secret-access-key' => env('AWS_SECRET_ACCESS_KEY'),
+        'secret-access-key' => env('AWS_SECRET_ACCESS_KEY')
     ]);
 }
 
-
+Config::define('AS3CF_SETTINGS', serialize($as3_settings));
 Config::define('AS3CFPRO_LICENCE', env('AS3CF_PRO_LICENCE') ?? '');
 Config::define('AWS_S3_CUSTOM_HOST', env('AWS_S3_CUSTOM_HOST') ?? '');
 Config::define('AWS_CLOUDFRONT_HOST', env('AWS_CLOUDFRONT_HOST') ?? '');
 Config::define('AS3CF_SHOW_ADD_METADATA_TOOL', true);
-Config::define('AS3CF_SETTINGS', serialize($as3_settings));
