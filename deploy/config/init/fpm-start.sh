86aac7c0eebecc42aea5241561c3bd2d133a9f19
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-10-01 15:15:23 +0100

    Update fpm-start.sh (#720)

--- deploy/config/init/fpm-start.sh
+++ deploy/config/init/fpm-start.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-wp sync-user-roles sync
+if wp core is-installed 2&gt;/dev/null; then
+    # WP is installed.
+    wp sync-user-roles sync
+else
+    # Fallback if WP is not installed.
+    # This will happen during a first run on localhost.
+    echo 'WordPress is not installed yet, so skipping command `wp sync-user-roles sync` in `fpm-start.sh`.'
+fi
