e9640b162446d9171ccd366360638ee213ef08a3
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-10-08 17:01:56 +0100

    News and blog search to use OpenSearch, ordering results by relevance

--- public/app/themes/clarity/inc/content-filter/search.php
+++ public/app/themes/clarity/inc/content-filter/search.php
@@ -1,229 +1,272 @@
 <?php
 
+/**
+ * FilterSearch
+ * 
+ * This class is responsible for handling the AJAX requests for the search filter.
+ * 
+ * @package Clarity
+ */
+
+namespace MOJ\Intranet;
+
 use MOJ\Intranet\Agency;
 use MOJ\Intranet\EventsHelper;
+use WP_Query;
 
-add_action('wp_ajax_load_events_filter_results', 'load_events_filter_results');
-add_action('wp_ajax_nopriv_load_events_filter_results', 'load_events_filter_results');
-
-function load_events_filter_results()
+class FilterSearch
 {
+    /**
+     * FilterSearch constructor.
+     * 
+     * @return void
+     */
 
-    if (!wp_verify_nonce($_POST['nonce_hash'], 'search_filter_nonce')) {
-        exit('Access not allowed.');
+    public function __construct()
+    {
+        $this->hooks();
     }
 
-    $oAgency = new Agency();
-    $activeAgency = $oAgency->getCurrentAgency();
-    $agency_term_id = $activeAgency['wp_tag_id'];
-    $date_filter = sanitize_text_field($_POST['valueSelected']);
-    $post_id = get_the_ID();
-    $query = sanitize_text_field($_POST['query']);
-
-    $filter_options = ['keyword_search' => $query];
-
-    if ($date_filter != 'all') {
-        $filter_options['date_filter'] = $date_filter;
+    /**
+     * Hooks
+     * 
+     * @return void
+     */
+    public function hooks(): void
+    {
+        add_action('wp_ajax_load_search_results', [$this, 'loadSearchResults']);
+        add_action('wp_ajax_nopriv_load_search_results', [$this, 'loadSearchResults']);
+        add_action('wp_ajax_load_events_filter_results', [$this, 'loadEventSearchResults']);
+        add_action('wp_ajax_nopriv_load_events_filter_results', [$this, 'loadEventSearchResults']);
     }
 
-    $EventsHelper = new EventsHelper();
+    /**
+     * Load results for events.
+     * 
+     * @return void
+     */
 
-    if (isset($_POST['termID'])) {
-        $tax_id = sanitize_text_field($_POST['termID']);
+    public function loadEventSearchResults()
+    {
+        if (!wp_verify_nonce($_POST['nonce_hash'], 'search_filter_nonce')) {
+            exit('Access not allowed.');
+        }
 
-        $filter_options['region_filter'] = $tax_id;
+        $active_agency = (new Agency())->getCurrentAgency();
+        $agency_term_id = $active_agency['wp_tag_id'];
+        $date_filter = sanitize_text_field($_POST['valueSelected'] ?? 'all');
+        $post_id = get_the_ID();
+        $query = sanitize_text_field($_POST['query']);
 
-        $events = $EventsHelper->get_events($agency_term_id, $filter_options);
-    } else {
-        $events = $EventsHelper->get_events($agency_term_id, $filter_options);
+        $filter_options = ['keyword_search' => $query];
+
+        if ($date_filter != 'all') {
+            $filter_options['date_filter'] = $date_filter;
+        }
+
+        $events_helper = new EventsHelper();
+
+        if (isset($_POST['termID'])) {
+            $tax_id = sanitize_text_field($_POST['termID']);
+
+            $filter_options['region_filter'] = $tax_id;
+
+            $events = $events_helper->get_events($agency_term_id, $filter_options);
+        } else {
+            $events = $events_helper->get_events($agency_term_id, $filter_options);
+        }
+
+        if ($events) {
+            echo '<div class="data-type" data-type="event"></div>';
+
+            foreach ($events as $key => $event) :
+                $event_id = $event->ID;
+                $post_url = $event->url;
+                $event_title = $event->post_title;
+
+                $start_date = $event->event_start_date;
+                $end_date = $event->event_end_date;
+                $start_time = $event->event_start_time;
+                $end_time = $event->event_end_time;
+                $location = $event->event_location;
+                $date = $event->event_start_date;
+                $day = date('l', strtotime($start_date));
+                $month = date('M', strtotime($start_date));
+                $year = date('Y', strtotime($start_date));
+                $all_day = $event->event_allday;
+
+                if ($all_day == true) {
+                    $all_day = 'all_day';
+                }
+
+                echo '<div class="c-events-item-list">';
+
+                include locate_template('src/components/c-calendar-icon/view.php');
+
+                include locate_template('src/components/c-events-item-byline/view.php');
+
+                echo '</div>';
+            endforeach;
+        } else {
+            echo 'No events found during this date range :(';
+        }
+        die();
     }
 
-    if ($events) {
-        echo '<div class="data-type" data-type="event"></div>';
+    /**
+     * Load results for post types except for events.
+     * 
+     * @return void
+     */
 
-        foreach ($events as $key => $event) :
-            $event_id = $event->ID;
-            $post_url = $event->url;
-            $event_title = $event->post_title;
+    public function loadSearchResults()
+    {
+        if (!wp_verify_nonce($_POST['nonce_hash'], 'search_filter_nonce')) {
+            exit('Access not allowed.');
+        }
 
-            $start_date = $event->event_start_date;
-            $end_date = $event->event_end_date;
-            $start_time = $event->event_start_time;
-            $end_time = $event->event_end_time;
-            $location = $event->event_location;
-            $date = $event->event_start_date;
-            $day = date('l', strtotime($start_date));
-            $month = date('M', strtotime($start_date));
-            $year = date('Y', strtotime($start_date));
-            $all_day = $event->event_allday;
+        // Enable ElasticPress integration with AJAX requests.
+        add_filter('ep_ajax_wp_query_integration', '__return_true');
 
-            if ($all_day == true) {
-                $all_day = 'all_day';
-            }
+        // Apply the weighting fields configuration to the query.
+        add_filter('ep_enable_do_weighting', '__return_true');
 
-            echo '<div class="c-events-item-list">';
+        // Run a query based on generated query arguments.
+        $query = new WP_Query($this->getQueryArgs());
 
-            include locate_template('src/components/c-calendar-icon/view.php');
+        // Use output buffering to capture the HTML output.
+        // This is necessary to get the html without refactoring the component's code.
+        ob_start();
+        echo '<div class="data-type" data-type="' . sanitize_text_field($_POST['postType']) . '"></div>';
+        foreach ($query->posts as $post) {
+            // $post is used in the included file.
+            include locate_template('src/components/c-article-item/view-news-feed.php');
+        }
+        $result_html = ob_get_clean();
 
-            include locate_template('src/components/c-events-item-byline/view.php');
+        // Get the pagination HTML.
+        $pagination_html = $this->getPagination(
+            sanitize_text_field($_POST['valueSelected']),
+            sanitize_text_field($_POST['nextPageToRetrieve']),
+            $query->max_num_pages
+        );
 
-            echo '</div>';
-        endforeach;
-    } else {
-        echo 'No events found during this date range :(';
+        // Return the results as JSON.
+        return wp_send_json([
+            'results' =>  $result_html,
+            'total' => $query->found_posts  . ' search results',
+            'pagination' => $pagination_html
+        ]);
     }
-    die();
+
+    /**
+     * Get Query Args
+     * 
+     * @return array
+     */
+
+    function getQueryArgs()
+    {
+        // Get the active agency.
+        $active_agency = (new Agency())->getCurrentAgency();
+
+        // Pagination.
+        $post_per_page = 10;
+        $next_page_to_retrieve = sanitize_text_field($_POST['nextPageToRetrieve'] ?? '');
+        $offset = $next_page_to_retrieve ? (($next_page_to_retrieve - 1) * $post_per_page) : 0;
+
+        // Post type, with a cleanup of the value.
+        $post_type = sanitize_text_field($_POST['postType'] ?? '');
+        $post_type = $post_type === 'posts' ? 'post' : $post_type;
+
+        // Is the request for a news category?
+        $news_category_id = sanitize_text_field($_POST['newsCategoryValue'] ?? '');
+
+        // Check if the post type is regional.
+        $is_regional = $post_type === 'regional_news' ? true : false;
+
+        $args = [
+            'numberposts' => $post_per_page,
+            'post_type' => $post_type,
+            'post_status' => 'publish',
+            'offset' => $offset,
+            'tax_query' => [
+                'relation' => 'AND',
+                [
+                    'taxonomy' => 'agency',
+                    'field' => 'term_id',
+                    'terms' => $active_agency['wp_tag_id']
+                ],
+                // If the region is set add its ID to the taxonomy query
+                ...($is_regional ? [
+                    'taxonomy' => 'region',
+                    'field' => 'region_id',
+                    'terms' =>  $news_category_id,
+                ] : []),
+                // If the news category is set add its ID unless the query is regional, 
+                // as it will have already been added to the tax query.
+                ...(!empty($news_category_id) && !$is_regional ? [
+                    'taxonomy' => 'news_category',
+                    'field' => 'category_id',
+                    'terms' =>  $news_category_id,
+                ] : []),
+            ]
+        ];
+
+        // Get the date filter value.
+        $value_selected = sanitize_text_field($_POST['valueSelected'] ?? '');
+
+        // Parse dates from the value selected.
+        if (!empty($value_selected)) {
+            preg_match('/&after=([^&]*)&before=([^&]*)/', $value_selected, $matches);
+            $args['date_query'] = [
+                'after' =>  date('Y-m-d', strtotime($matches[1])),
+                'before' => date('Y-m-d', strtotime($matches[2])),
+                'inclusive' => false,
+            ];
+        }
+
+        // Get the search query.
+        $query = sanitize_text_field($_POST['query'] ?? '');
+
+        // If there is a search query, set the orderby to relevance.
+        if (!empty($query)) {
+            $args['orderby'] = 'relevance';
+            $args['s'] = $query;
+        }
+
+        return $args;
+    }
+
+    /**
+     * Get Pagination
+     * 
+     * @param string $selected
+     * @param int|string $next
+     * @param int $total
+     * @return string
+     */
+
+    function getPagination(string $selected, int|string $next, int $total): string
+    {
+        $html = '';
+        if ($next == $total) {
+            $html .= '<span class="nomore-btn" data-date="' . $selected . '">';
+            $html .= '<span class="c-pagination__main">No More Results</span>';
+            $html .= '</span>';
+        } elseif ($total <= 1) {
+            $html .= '<button class="more-btn" data-page="' . $next . '" data-date="' . $selected . '">';
+            $html .= '<span class="c-pagination__main">No More Results</span>';
+            $html .= '<span class="c-pagination__count"> ' . $next . ' of 1</span>';
+            $html .= '</button>';
+        } else {
+            $html .= '<button class="more-btn" data-page="' . $next . '" data-date="' . $selected . '">';
+            $html .= '<span class="c-pagination__main"><span class="u-icon u-icon--circle-down"></span> Load Next 10 Results</span>';
+            $html .= '<span class="c-pagination__count"> ' . $next . ' of ' . $total . '</span>';
+            $html .= '</button>';
+        }
+        return $html;
+    }
 }
 
-function get_args() {
-    $oAgency = new Agency();
-    $activeAgency = $oAgency->getCurrentAgency();
-    $post_per_page = 10;
-
-    $valueSelected = isset($_POST['valueSelected']) ? sanitize_text_field($_POST['valueSelected']) : ''; 
-    $before = '';
-    $after = '';
-
-    if ($valueSelected) {
-        preg_match('/&after=([^&]*)&before=([^&]*)/', $valueSelected, $matches);
-        $after = date('Y-m-d', strtotime($matches[1]));
-        $before = date('Y-m-d', strtotime($matches[2]));
-    }
-
-    $nextPageToRetrieve = isset($_POST['nextPageToRetrieve']) ?  sanitize_text_field($_POST['nextPageToRetrieve']) : '';
-    $offset = $nextPageToRetrieve ? (($nextPageToRetrieve - 1) * $post_per_page) : 0;
-
-    $postType = isset($_POST['postType']) ? sanitize_text_field($_POST['postType']) : '';
-    $newsCategoryValue = isset($_POST['newsCategoryValue']) ?  sanitize_text_field($_POST['newsCategoryValue']) : '';
-    $newsCategory_ID = $newsCategoryValue ? $newsCategoryValue : '';
-    
-    $query = isset($_POST['query']) ?  sanitize_text_field($_POST['query']) : '';
-
-    $regional = $postType === 'regional_news' ? true : false;
-
-    $args = [
-        'numberposts' => $post_per_page,
-        'post_type' => $postType === 'posts' ? 'post' : $postType,
-        'post_status' => 'publish',
-        'offset' => $offset,
-        's' => $query,
-        'date_query' => [
-          'after' => $after,
-          'before' => $before,
-          'inclusive' => false,  
-        ],
-        'tax_query' => [
-          'relation' => 'AND',
-          [
-            'taxonomy' => 'agency',
-            'field' => 'term_id',
-            'terms' => $activeAgency['wp_tag_id']
-          ],
-          // If the region is set add its ID to the taxonomy query
-          ...( $regional ? [
-            'taxonomy' => 'region',
-            'field' => 'region_id',
-            'terms' =>  $newsCategory_ID,
-          ] : []),
-          // If the news category is set add its ID unless the query is regional, as it will have already been added to the tax query
-          ...( $newsCategory_ID && !$regional ? [
-            'taxonomy' => 'news_category',
-            'field' => 'category_id',
-            'terms' =>  $newsCategory_ID,
-          ] : []),
-      ]
-      ];
-
-      return $args;
-}
-
-add_action('wp_ajax_load_search_results', 'load_search_results');
-add_action('wp_ajax_nopriv_load_search_results', 'load_search_results');
-
-function load_search_results()
-{
-    if (!wp_verify_nonce($_POST['nonce_hash'], 'search_filter_nonce')) {
-        exit('Access not allowed.');
-    }
-
-    $postType = isset($_POST['postType']) ? sanitize_text_field($_POST['postType']) : '';
-    $posts = get_posts(get_args());
-
-    echo '<div class="data-type" data-type="' . $postType . '"></div>';
-    foreach ($posts as $key => $post) {
-        include locate_template('src/components/c-article-item/view-news-feed.php');
-    }
-    die();
-}
-
-add_action('wp_ajax_load_next_results', 'load_next_results');
-add_action('wp_ajax_nopriv_load_next_results', 'load_next_results');
-
-function load_next_results()
-{
-    if (!wp_verify_nonce($_POST['nonce_hash'], 'search_filter_nonce')) {
-        exit('Access not allowed.');
-    }
-
-    $postType = isset($_POST['postType']) ? sanitize_text_field($_POST['postType']) : '';
-    $posts = get_posts(get_args());
-
-    echo '<div class="data-type" data-type="' . $postType . '"></div>';
-    foreach ($posts as $key => $post) {
-      include locate_template('src/components/c-article-item/view-news-feed.php');
-    }
-    die();
-}
-
-add_action('wp_ajax_load_search_results_total', 'load_search_results_total');
-add_action('wp_ajax_nopriv_load_search_results_total', 'load_search_results_total');
-
-function load_search_results_total()
-{
-    if (!wp_verify_nonce($_POST['nonce_hash'], 'search_filter_nonce')) {
-        exit('Access not allowed.');
-    }
-
-    $query = new WP_Query(get_args());
-    $post_total = $query->found_posts;
-
-    echo $post_total . ' search results';
-    wp_reset_postdata();
-    die();
-}
-
-add_action('wp_ajax_load_page_total', 'load_page_total');
-add_action('wp_ajax_nopriv_load_page_total', 'load_page_total');
-
-function load_page_total()
-{
-    if (!wp_verify_nonce($_POST['nonce_hash'], 'search_filter_nonce')) {
-        exit('Access not allowed.');
-    }
-
-    $nextPageToRetrieve = sanitize_text_field($_POST['nextPageToRetrieve']);
-    $valueSelected = sanitize_text_field($_POST['valueSelected']);
-
-    $args = get_args();
-
-    $query = new WP_Query($args);
-    $pagetotal = $query->max_num_pages;
-
-    if ($nextPageToRetrieve == $pagetotal) {
-        echo '<span class="nomore-btn" data-date="' . $valueSelected . '">';
-        echo '<span class="c-pagination__main">No More Results</span>';
-        echo '</span>';
-    } elseif ($pagetotal <= 1) {
-        echo '<button class="more-btn" data-page="' . $nextPageToRetrieve . '" data-date="' . $valueSelected . '">';
-        echo '<span class="c-pagination__main">No More Results</span>';
-        echo '<span class="c-pagination__count"> ' . $nextPageToRetrieve . ' of 1</span>';
-        echo '</button>';
-    } else {
-        echo '<button class="more-btn" data-page="' . $nextPageToRetrieve . '" data-date="' . $valueSelected . '">';
-        echo '<span class="c-pagination__main"><span class="u-icon u-icon--circle-down"></span> Load Next 10 Results</span>';
-        echo '<span class="c-pagination__count"> ' . $nextPageToRetrieve . ' of ' . $pagetotal . '</span>';
-        echo '</button>';
-    }
-    wp_reset_postdata();
-    die();
-}
+new FilterSearch();
--- public/app/themes/clarity/src/globals/js/blog-content_filter.js
+++ public/app/themes/clarity/src/globals/js/blog-content_filter.js
@@ -1,176 +1,113 @@
 export default (function ($) {
-    $.fn.moji_ajaxFilter = function () {
-        jQuery(document).on('submit', '#ff', function (e) {
-            e.preventDefault()
+  $.fn.moji_ajaxFilter = function () {
+    const postType = $(".data-type").data("type");
+    const termID = $(".l-secondary").data("termid");
+    const nonceHash = $("#_search_filter_wpnonce").val();
 
-            var nextPageToRetrieve = jQuery('#ff').data('page') + 1
-            jQuery('.more-btn').attr('data-page', nextPageToRetrieve)
+    $(document).on("submit", "#ff", function (e) {
+      e.preventDefault();
 
-            var $input = jQuery(this).find('input[name="ff_keywords_filter"]')
-            var query = $input.val()
-            var $content = jQuery('#content')
-            var nonce = $('#_search_filter_wpnonce').val()
+      const nextPageToRetrieve = $("#ff").data("page") + 1;
+      $(".more-btn").attr("data-page", nextPageToRetrieve);
 
-            var optionSelected = jQuery(this).find('#ff_date_filter option:selected, #ff_region_news_date_filter option:selected')
-            var valueSelected = optionSelected.val()
-            var newsCategorySelected = jQuery(this).find('#ff_categories_filter_e-news, input[name="ff_categories_filter_regions"]')
-            var newsCategoryValue = newsCategorySelected.val()
+      const valueSelected = $(this)
+        .find(
+          "#ff_date_filter option:selected, #ff_region_news_date_filter option:selected",
+        )
+        ?.val();
+      const newsCategoryValue =
+        $(this)
+          .find(
+            '#ff_categories_filter_e-news, input[name="ff_categories_filter_regions"]',
+          )
+          ?.val() ?? 0;
 
-            var postType = jQuery('.data-type').data('type')
-            var termID = jQuery('.l-secondary').data('termid')
+      $.ajax({
+        type: "post",
+        url: mojAjax.ajaxurl,
+        dataType: "json",
+        data: {
+          action: "load_search_results",
+          query: $(this).find("#ff_keywords_filter").val(),
+          nextPageToRetrieve,
+          valueSelected,
+          postType,
+          newsCategoryValue,
+          termID,
+          nonce_hash: nonceHash,
+        },
+        success: function (response) {
+          $(".c-article-item").remove();
+          $("#content").html(response.results);
+          $("#title-section").html(response.total);
+          $(".c-pagination").html(response.pagination);
+        },
+      });
 
-            if (newsCategoryValue === 'undefined') {
-                newsCategoryValue = 0
-            }
+      return false;
+    });
 
-            jQuery.ajax({
-                type: 'post',
-                url: mojAjax.ajaxurl,
-                dataType: 'html',
-                data: {
-                    action: 'load_search_results',
-                    query: query,
-                    valueSelected: valueSelected,
-                    postType: postType,
-                    newsCategoryValue: newsCategoryValue,
-                    termID: termID,
-                    nonce_hash: nonce
-                },
-                success: function (response) {
-                    $('.c-article-item').remove()
-                    $content.html(response)
-                }
-            })
+    $(".c-pagination").on("click", function () {
+      $("#load_more div.data-type").addClass("shown-item");
 
-            jQuery.ajax({
-                type: 'post',
-                url: mojAjax.ajaxurl,
-                dataType: 'html',
-                data: {
-                    action: 'load_search_results_total',
-                    query: query,
-                    valueSelected: valueSelected,
-                    postType: postType,
-                    newsCategoryValue: newsCategoryValue,
-                    termID: termID,
-                    nonce_hash: nonce
-                },
-                success: function (response) {
-                    jQuery('#title-section').html(response)
-                }
-            })
+      const nextPageToRetrieve = $(".more-btn").data("page") + 1;
+      $(".more-btn").attr("data-page", nextPageToRetrieve);
 
-            jQuery.ajax({
-                type: 'post',
-                url: mojAjax.ajaxurl,
-                dataType: 'html',
-                data: {
-                    action: 'load_page_total',
-                    query: query,
-                    nextPageToRetrieve: nextPageToRetrieve,
-                    valueSelected: valueSelected,
-                    postType: postType,
-                    newsCategoryValue: newsCategoryValue,
-                    termID: termID,
-                    nonce_hash: nonce
-                },
-                success: function (response) {
-                    $('.c-pagination').html(response)
-                }
-            })
-            return false
-        })
+      const newsCategoryValue =
+        $('input[name="ff_categories_filter_news-category"]:checked')?.val() ??
+        0;
 
-        jQuery('.c-pagination').on('click', function () {
-            $("#load_more div.data-type").addClass("shown-item")
-            var nonce = $('#_search_filter_wpnonce').val()
-            var nextPageToRetrieve = jQuery('.more-btn').data('page') + 1
-            jQuery('.more-btn').attr('data-page', nextPageToRetrieve)
-            var query = jQuery('#ff_keywords_filter').val()
-            var valueSelected = jQuery('.more-btn, .nomore-btn').data('date')
-            var postType = jQuery('.data-type').data('type')
-            var newsCategorySelected = jQuery('input[name="ff_categories_filter_news-category"]:checked')
-            var newsCategoryValue = newsCategorySelected.val()
-            if (newsCategoryValue === 'undefined') {
-                newsCategoryValue = 0
-            }
+      $.ajax({
+        type: "post",
+        url: mojAjax.ajaxurl,
+        dataType: "json",
+        data: {
+          action: "load_search_results",
+          query: $(this).find("#ff_keywords_filter").val(),
+          valueSelected: $(".more-btn, .nomore-btn").data("date"),
+          nextPageToRetrieve,
+          postType,
+          newsCategoryValue,
+          termID,
+          nonce_hash: nonceHash,
+        },
 
-            jQuery.ajax({
-                type: 'post',
-                url: mojAjax.ajaxurl,
-                dataType: 'html',
-                data: {
-                    action: 'load_next_results',
-                    query: query,
-                    nextPageToRetrieve: nextPageToRetrieve,
-                    valueSelected: valueSelected,
-                    postType: postType,
-                    newsCategoryValue: newsCategoryValue,
-                    nonce_hash: nonce
-                },
+        success: function (response) {
+          $("#load_more").append(response.results);
+          $(".c-pagination").html(response.pagination);
+          $(
+            "#load_more div.data-type:not('.shown-item')+article div.content a",
+          ).focus();
+        },
+      });
 
-                success: function (response) {
-                    $('#load_more').append(response)
-                    $("#load_more div.data-type:not('.shown-item')+article div.content a").focus()
-                }
-            })
+      return false;
+    });
 
-            jQuery.ajax({
-                type: 'post',
-                url: mojAjax.ajaxurl,
-                dataType: 'html',
-                data: {
-                    action: 'load_page_total',
-                    query: query,
-                    nextPageToRetrieve: nextPageToRetrieve,
-                    valueSelected: valueSelected,
-                    postType: postType,
-                    newsCategoryValue: newsCategoryValue,
-                    nonce_hash: nonce
-                },
-                success: function (response) {
-                    $('.c-pagination').html(response)
-                }
-            })
-            return false
-        })
+    $(document).on("submit", "#ff_events", function (e) {
+      e.preventDefault();
 
-        jQuery(document).on('submit', '#ff_events', function (e) {
-            e.preventDefault()
+      const nextPageToRetrieve = $("#ff").data("page") + 1;
+      $(".more-btn").attr("data-page", nextPageToRetrieve);
 
-            var nextPageToRetrieve = jQuery('#ff').data('page') + 1
-            jQuery('.more-btn').attr('data-page', nextPageToRetrieve)
-
-            var $input = jQuery(this).find('input[name="ff_keywords_filter"]')
-            var query = $input.val()
-            var $content = jQuery('#content')
-
-            var optionSelected = jQuery(this).find('#ff_date_filter option:selected')
-            var valueSelected = optionSelected.val()
-
-            var postType = jQuery('.data-type').data('type')
-            var termID = jQuery('.l-secondary').data('termid')
-
-            var nonce = $('#_search_filter_wpnonce').val()
-
-            jQuery.ajax({
-                type: 'post',
-                url: mojAjax.ajaxurl,
-                dataType: 'html',
-                data: {
-                    action: 'load_events_filter_results',
-                    query: query,
-                    valueSelected: valueSelected,
-                    postType: postType,
-                    termID: termID,
-                    nonce_hash: nonce
-                },
-                success: function (response) {
-                    $('.c-article-item').remove()
-                    $content.html(response)
-                }
-            })
-            return false
-        })
-    }
-})(jQuery)
+      $.ajax({
+        type: "post",
+        url: mojAjax.ajaxurl,
+        dataType: "html",
+        data: {
+          action: "load_events_filter_results",
+          query: $(this).find('input[name="ff_keywords_filter"]').val(),
+          valueSelected: $(this).find("#ff_date_filter option:selected").val(),
+          postType,
+          termID,
+          nonce_hash: nonceHash,
+        },
+        success: function (response) {
+          $(".c-article-item").remove();
+          $("#content").html(response);
+        },
+      });
+      return false;
+    });
+  };
+})(jQuery);
