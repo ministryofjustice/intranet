ef2a5c5ce2c899c0c31f222ffd2987e2320485e9
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-10-09 20:50:21 +0100

    Dynamic background sizes

--- public/app/themes/clarity/src/components/c-logo-bar/view.php
+++ public/app/themes/clarity/src/components/c-logo-bar/view.php
@@ -6,10 +6,11 @@ $oAgency = new Agency();
 $activeAgency = $oAgency->getCurrentAgency();
 $header_logo = get_field(get_intranet_code() .'_header_logo', 'option');
 $logo = get_stylesheet_directory_uri() . '/dist/images/moj_logo_header.png';
+$size = "94%";
 
-# If law commission, use different logo
 if ($activeAgency['shortcode'] === 'law-commission') {
     $logo = get_stylesheet_directory_uri() . '/dist/images/lawcomms_logo_new.jpg';
+    $size = "100%";
 }
 
 # If header logo is set, use that instead
@@ -22,7 +23,7 @@ if (!empty($header_logo)) {
 <section class="c-logo-bar">
   <div class="u-wrapper">
         <div class="u-wrapper__stack--left">
-            <a href="/" rel="home">
+            <a href="/" rel="home" style="background-size: <?= $size ?>">
                 <span class="image" style="background-image: url('<?= $logo ?>')">&nbsp;</span>
                 <span class="agency-title l-half-section"><?= $activeAgency['label'] ?></span>
             </a>
