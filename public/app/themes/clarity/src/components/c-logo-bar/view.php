dd3b6f40a1f45045890d6202f137b7547888145a
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-10-09 20:02:26 +0100

    CDPT 2144 Update Coat of Arms (#727)
    * Add new coat of arms
    
    * Tidy function
    
    * Update packages
    
    * Restyle logo placement
    
    * Housekeeping

--- public/app/themes/clarity/src/components/c-logo-bar/view.php
+++ public/app/themes/clarity/src/components/c-logo-bar/view.php
@@ -1,42 +1,35 @@
 <?php
-  use MOJ\Intranet\Agency;
 
-  $oAgency      = new Agency();
-  $activeAgency = $oAgency->getCurrentAgency();
+use MOJ\Intranet\Agency;
+
+$oAgency = new Agency();
+$activeAgency = $oAgency->getCurrentAgency();
+$header_logo = get_field(get_intranet_code() .'_header_logo', 'option');
+$logo = get_stylesheet_directory_uri() . '/dist/images/moj_logo_header.png';
 
 if ($activeAgency['shortcode'] === 'law-commission') {
     $logo = get_stylesheet_directory_uri() . '/dist/images/lawcomms_logo_new.png';
-} else {
-	$logo = get_stylesheet_directory_uri() . '/dist/images/moj_logo_new.png';
 }
-
-$agency = get_intranet_code();
-
-$header_logo  = get_field($agency .'_header_logo', 'option');
 
 if(!empty($header_logo)){
     $logo = $header_logo;
 }
 
-  $page_name = get_query_var('name');
 ?>
 
 <section class="c-logo-bar">
   <div class="u-wrapper">
-
         <div class="u-wrapper__stack--left">
             <a href="/" rel="home">
-            <img aria-hidden="true" src="<?= $logo ?>" alt>
-            <span class="agency-title l-half-section"><?= $activeAgency['label'] ?></span>
+                <span class="image" style="background-image: url('<?= $logo ?>')">&nbsp;</span>
+                <span class="agency-title l-half-section"><?= $activeAgency['label'] ?></span>
             </a>
         </div>
 
         <div class="u-wrapper__stack--right">
-            <?php if ($page_name !== 'agency-switcher') : ?>
+            <?php if (get_query_var('name') !== 'agency-switcher') : ?>
             <a href="/agency-switcher" class="c-logo-bar__switch">Switch to other intranet</a>
             <?php endif; ?>
         </div>
-        
     </div>
-  </div>
 </section>
