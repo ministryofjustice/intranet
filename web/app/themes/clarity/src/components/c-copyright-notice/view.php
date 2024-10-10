81e0aee9b6dc2ffdd3b683c095402f264079953d
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-10-10 16:49:15 +0100

    Favicon using tudor crest (#732)
    * Generate Favicon and footer crest SVG
    
    * Resize footer image

--- web/app/themes/clarity/src/components/c-copyright-notice/view.php
+++ web/app/themes/clarity/src/components/c-copyright-notice/view.php
@@ -1,5 +1,5 @@
 <div class="c-copyright-notice">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/crown_copyright_logo.png" alt="crown copyright logo">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/tudor_crest.svg" alt="crown copyright logo">
     <br>
     <span>&copy; Crown copyright</span>
   </div>
