40f219f15ee33caef7321d13df74cd72898ca817
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-10-10 17:14:15 +0100

    Update favicons and other icons (#734)
    * Update icons and paths
    
    * Start up local redis
    
    * Optimise crests

--- public/app/themes/clarity/src/components/c-logo-bar/view.php
+++ public/app/themes/clarity/src/components/c-logo-bar/view.php
@@ -6,14 +6,13 @@ $oAgency = new Agency();
 $activeAgency = $oAgency->getCurrentAgency();
 $header_logo = get_field(get_intranet_code() .'_header_logo', 'option');
 $logo = get_stylesheet_directory_uri() . '/dist/images/moj_logo_header.png';
-$size = "94%";
 
+# If the agency is the law commission, use the new logo
 if ($activeAgency['shortcode'] === 'law-commission') {
-    $logo = get_stylesheet_directory_uri() . '/dist/images/lawcomms_logo_new.jpg';
-    $size = "100%";
+    $logo = get_stylesheet_directory_uri() . '/dist/images/lawcomms_logo_new.png';
 }
 
-# If header logo is set, use that instead
+# If a header logo is set, use that instead
 if (!empty($header_logo)) {
     $logo = $header_logo;
 }
@@ -23,9 +22,9 @@ if (!empty($header_logo)) {
 <section class="c-logo-bar">
   <div class="u-wrapper">
         <div class="u-wrapper__stack--left">
-            <a href="/" rel="home" style="background-size: <?= $size ?>">
-                <span class="image" style="background-image: url('<?= $logo ?>')">&nbsp;</span>
-                <span class="agency-title l-half-section"><?= $activeAgency['label'] ?></span>
+            <a href="/" rel="home">
+            <img class="c-logo-bar__logo" aria-hidden="true" src="<?= $logo ?>" alt="" />
+            <span class="agency-title l-half-section"><?= $activeAgency['label'] ?></span>
             </a>
         </div>
 
