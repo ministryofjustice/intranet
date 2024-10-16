c6be1f051a0412025686f73dd1858842c1ea63eb
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2024-10-16 09:06:02 +0100

    IDE says commit

--- bin/fpm-liveness.sh
+++ bin/fpm-liveness.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
 # Exit with code 1 if any php process has been running for more than 1 hour.
-if [ $(ps -o time,comm | grep -c '.*h.*php-fpm') -gt 0 ]; then exit 1; fi
+# shellcheck disable=SC2009
+if [ "$(ps -o time,comm | grep -c '.*h.*php-fpm')" -gt 0 ]; then exit 1; fi
