bee99ea74f0028b0721334fa66f0386cb9eee6da
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-10-24 12:42:34 +0100

    Update local-run-s3-push.sh

--- bin/local-run-s3-push.sh
+++ bin/local-run-s3-push.sh
@@ -2,7 +2,7 @@
 
 # This script will build an s3-push image and run the container.
 # When the container runs, assets will be pushed to the S3 bucket.
-# This script is meant to be run locally and not in the CI/CD pipeline.
+# This script is meant to be run locally (for testing/development) and not in the CI/CD pipeline.
 
 # Prerequisites:
 # - Have the .env file in the project root.
