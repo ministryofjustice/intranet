0b5b8128bfced1ec3bce0b29c4f137263f0ec21b
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-10-30 15:43:06 +0000

    Update composer-post-install.sh

--- bin/composer-post-install.sh
+++ bin/composer-post-install.sh
@@ -29,15 +29,17 @@ MOJ_COMPONENTS_FILE=/var/www/html/public/app/mu-plugins/wp-moj-components/compon
 MOJ_COMPONENTS_SEARCH_EMAIL="justice\.web@digital\.justice\.gov\.uk"
 MOJ_COMPONENTS_REPLACE_EMAIL="intranet-support@digital.justice.gov.uk"
 
-
 # If search string is in file. Then replace it.
 if grep -q  $MOJ_COMPONENTS_SEARCH_EMAIL $MOJ_COMPONENTS_FILE ; then
   echo "Replacing email text in wp-moj-components plugin."
   sed -i "s/$MOJ_COMPONENTS_SEARCH_EMAIL/$MOJ_COMPONENTS_REPLACE_EMAIL/g" $MOJ_COMPONENTS_FILE
 fi
 
-echo "Replacing paragraph text in wp-moj-components plugin"
 MOJ_COMPONENTS_SEARCH_PARAGRAPH="<p>This website is ([\w\W]*?)<\/p>"
 MOJ_COMPONENTS_REPLACE_PARAGRAPH="<p>This website is technically maintained by Justice Digital, Central Digital Product Team:<\/p>"
-MOJ_COMPONENTS_CONTENT=$(perl -0777pe 's/'"$MOJ_COMPONENTS_SEARCH_PARAGRAPH"'/'"$MOJ_COMPONENTS_REPLACE_PARAGRAPH"'/s' "$MOJ_COMPONENTS_FILE")
-echo "$MOJ_COMPONENTS_CONTENT" > "$MOJ_COMPONENTS_FILE"
+
+if [ -f "$MOJ_COMPONENTS_FILE" ] ; then
+  echo "Replacing paragraph text in wp-moj-components plugin"
+  MOJ_COMPONENTS_CONTENT=$(perl -0777pe 's/'"$MOJ_COMPONENTS_SEARCH_PARAGRAPH"'/'"$MOJ_COMPONENTS_REPLACE_PARAGRAPH"'/s' "$MOJ_COMPONENTS_FILE")
+  echo "$MOJ_COMPONENTS_CONTENT" > "$MOJ_COMPONENTS_FILE"
+fi
