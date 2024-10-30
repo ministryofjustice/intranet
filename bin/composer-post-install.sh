db784891b53926d911205114eee3797a971c5f26
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-10-30 11:32:02 +0000

    Update email and team name on the Contact Us dashboard widget.

--- bin/composer-post-install.sh
+++ bin/composer-post-install.sh
@@ -23,3 +23,21 @@ if grep -q  $TOTAL_POLL_SEARCH $TOTAL_POLL_FILE ; then
   echo "Fixing syntax error in totalpoll-lite..."
   sed -i "s/$TOTAL_POLL_SEARCH/$TOTAL_POLL_REPLACE/g" $TOTAL_POLL_FILE
 fi
+
+
+MOJ_COMPONENTS_FILE=/var/www/html/public/app/mu-plugins/wp-moj-components/component/Introduce/Introduce.php
+MOJ_COMPONENTS_SEARCH_1="justice\.web@digital\.justice\.gov\.uk"
+MOJ_COMPONENTS_REPLACE_1="intranet-support@digital.justice.gov.uk"
+MOJ_COMPONENTS_SEARCH_2="Justice\son\sthe\sWeb\steam"
+MOJ_COMPONENTS_REPLACE_2="Central Digital Product Team"
+
+# If search string is in file. Then replace it.
+if grep -q  $MOJ_COMPONENTS_SEARCH_1 $MOJ_COMPONENTS_FILE ; then
+  echo "Replacing email text in wp-moj-components plugin."
+  sed -i "s/$MOJ_COMPONENTS_SEARCH_1/$MOJ_COMPONENTS_REPLACE_1/g" $MOJ_COMPONENTS_FILE
+fi
+
+if grep -q  $MOJ_COMPONENTS_SEARCH_2 $MOJ_COMPONENTS_FILE ; then
+  echo "Replacing team text in wp-moj-components plugin."
+  sed -i "s/'$MOJ_COMPONENTS_SEARCH_2'/'$MOJ_COMPONENTS_REPLACE_2'/g" $MOJ_COMPONENTS_FILE
+fi
