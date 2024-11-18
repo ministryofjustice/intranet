07198df394c34bad921d1353bb4841a8c763b4cf
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-11-18 16:52:37 +0000

    Update composer-post-install.sh

--- bin/composer-post-install.sh
+++ bin/composer-post-install.sh
@@ -87,7 +87,15 @@ if grep -q  $TREE_VIEW_SEARCH $TREE_VIEW_FILE ; then
 fi
 
 
-ELASTIC_PRESS_TARGET_VERSION="5.1.3"
+# This find/replace is for the ElasticPress plugin.
+# 
+# When a new post is created via the WordPress dashboard, the existing behaviour of the ElasticPress plugin 
+# is to delete the post from the search index. Delteing a post that's not in the index causes a 404 error to 
+# be logged on Cloud Platform's OpenSearch proxy server.
+#
+# This change adds a check to see if the post is in the index before deleting it, preventing the 404 error.
+
+ELASTIC_PRESS_TARGET_VERSION="5.1.4"
 verify_composer_package_version "wpackagist-plugin/elasticpress" $ELASTIC_PRESS_TARGET_VERSION
 
 ELASTIC_PRESS_FILE=/var/www/html/public/app/mu-plugins/elasticpress/includes/classes/Indexable/Post/SyncManager.php
