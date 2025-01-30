8117d8385a6d30a3332a37bedd95a1f0e65f7ee5
Emily Hazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2025-01-30 16:03:10 +0000

    CDPT-1939: Splash page - Remove HTTP_REFERER as it doesn't appear to do anything

--- public/app/themes/clarity/src/components/c-intranet-switcher/view.php
+++ public/app/themes/clarity/src/components/c-intranet-switcher/view.php
@@ -6,44 +6,12 @@ $oAgency = new Agency();
 $activeAgencies = $oAgency->getList();
 $current_intranet = get_intranet_code();
 
-$referrer = (isset($_SERVER['HTTP_REFERER']) ? parse_url($_SERVER['HTTP_REFERER']) : parse_url(get_home_url()));
 $url = get_home_url();
 
 // Redirect back to the previous page after selecting an agency if this is their first visit
 if (isset($_GET['send_back'])) {
     // Validate the URL to prevent open redirects
     $url = wp_validate_redirect($_GET['send_back'], get_home_url());
-}
-
-/**
- * Remove the query parameter so it isn't sent
- * twice/several times on repeated access to this page
- */
-if (isset($referrer['query'])) {
-    parse_str($referrer['query'], $output);
-
-    if (isset($output['agency'])) {
-        unset($output['agency']);
-    }
-
-    // Remove send_back as it's only needed to set the url
-    if (isset($output['send_back'])) {
-        unset($output['send_back']);
-    }
-
-    $previous_parameters = [];
-
-    foreach ($output as $key => $value) {
-        $previous_parameters[] = $key . '=' . $value;
-    }
-
-    $referrer['query'] = implode('&', $previous_parameters);
-
-    if (trim($referrer['query']) != '') {
-        $referrer['query'] = '&' . $referrer['query'];
-    }
-} else {
-    $referrer['query'] = '';
 }
 
 // Temporarily filtering out JAC/PB until site is ready to go live
@@ -86,7 +54,7 @@ array_map(function($key, array $agency) use (&$integrated, &$external) {
                             } else {
                                 $extra_class = '';
                             }
-                            echo '<li class="c-intranet-switcher__switch c-intranet-switcher__switch--' . $agency_id . $extra_class . ' "><a href="'. $url .'?agency=' . $agency_id . $referrer['query'] . '">' . $agency['label'] . '</a></li>';
+                            echo '<li class="c-intranet-switcher__switch c-intranet-switcher__switch--' . $agency_id . $extra_class . ' "><a href="'. $url .'?agency=' . $agency_id . '">' . $agency['label'] . '</a></li>';
                         }
                     ?>
                 </ul>
