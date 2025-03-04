0ab28c540b2f912a678d3cdb66ef8ec083bf0e8f
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-03-04 11:08:54 +0000

    15A Add multisite compatibility to the theme's total poll actions.

--- public/app/themes/clarity/inc/admin/plugins/polls.php
+++ public/app/themes/clarity/inc/admin/plugins/polls.php
@@ -1,17 +1,25 @@
 <?php
 
+namespace MOJ\Intranet\Polls;
+
 /**
  * This section modifies the TotalPoll plugin in admin to suit our theme
  * For frontend polls code see src/components/c-polls
  */
 
+$active_plugins = get_option('active_plugins');
+
+if (is_multisite()) {
+    $active_plugins = array_merge($active_plugins, array_keys(get_site_option('active_sitewide_plugins')));
+}
+
 // Check if plugin is activated
-if (in_array('totalpoll-lite/plugin.php', apply_filters('active_plugins', get_option('active_plugins')))) :
+if (in_array('totalpoll-lite/plugin.php', apply_filters('active_plugins', $active_plugins))) :
 
     /**
      * Remove (for editors) options and settings not needed.
      */
-    add_action('admin_notices', 'remove_poll_meta_boxes_and_tabs');
+    add_action('admin_notices', 'MOJ\Intranet\Polls\remove_poll_meta_boxes_and_tabs');
 
     // Remove everything that an editor doesn't need to see.
     function remove_poll_meta_boxes_and_tabs()
@@ -20,7 +28,7 @@ if (in_array('totalpoll-lite/plugin.php', apply_filters('active_plugins', get_op
         // Get the current admin screen and use it as a check so we only add the CSS to the poll plugin dashboard
         $currentAdminScreen = get_current_screen();
 
-        if (! current_user_can('administrator') && ( $currentAdminScreen->id === 'poll_page_dashboard' )) {
+        if (! current_user_can('administrator') && ($currentAdminScreen->id === 'poll_page_dashboard')) {
             echo '<style> .totalpoll-page-tabs { display:none !important }</style>';
             echo '<style> .totalpoll-column-sidebar { display:none !important }</style>';
             echo '<style> .totalpoll-box-totalsuite { display:none !important }</style>';
@@ -31,7 +39,7 @@ if (in_array('totalpoll-lite/plugin.php', apply_filters('active_plugins', get_op
     }
 
     // Remove lefthand menu items
-    add_action('admin_menu', 'remove_menu_links', 9999);
+    add_action('admin_menu', 'MOJ\Intranet\Polls\remove_menu_links', 9999);
 
     function remove_menu_links()
     {
