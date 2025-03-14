649a1ee94e80d53dae437426609dbbb3acea2eed
Emily Hazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2025-03-14 15:01:43 +0000

    CDPT-1709: Purge cache after content update - Add function to clear cache when a post is updated

--- public/app/themes/clarity/inc/admin/page.php
+++ public/app/themes/clarity/inc/admin/page.php
@@ -2,6 +2,7 @@
 
 // Page post type. Add excerpts to pages
 add_action('init', 'add_page_excerpts');
+add_action('wp_after_insert_post', 'clear_cache', 10, 2);
 
 function add_page_excerpts()
 {
@@ -14,3 +15,28 @@ add_action('save_post', function ($post_id, $post) {
         update_post_meta($post_id, '_wp_page_template', 'agency-switcher.php');
     }
 }, 99, 2);
+
+/**
+ * Send a purge cache request to the Nginx server when a post is saved or updated.
+ *
+ * @param int $post_id The post ID
+ *
+ * @return void
+ */
+function clear_cache(int $post_id): void {
+    // Check if the post is a revision or unpublished.
+    if (wp_is_post_revision($post_id) || get_post_status($post_id) !== 'publish') {
+        return;
+    }
+    // Get the post URL.
+    $url = get_permalink($post_id);
+    $path = parse_url($url, PHP_URL_PATH);
+    $nginx_cache_path = get_home_url(null, '/purge-cache' . $path);
+    // Purge the cache.
+    $result = wp_remote_get($nginx_cache_path, ['blocking' => false, 'keep_home_url' => true]);
+    if (is_wp_error($result)) {
+        error_log('Unable to clear cache for path ' . $nginx_cache_path);
+        error_log('Error message: ' . $result->get_error_message());
+    }
+}
+
