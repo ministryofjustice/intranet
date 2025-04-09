5fe661125835525880f1c1a423bb5f030c2e3d6b
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-04-09 16:46:14 +0100

    Add compatibility to homepage settings

--- public/app/themes/clarity/inc/admin/acf-field-group.php
+++ public/app/themes/clarity/inc/admin/acf-field-group.php
@@ -1684,7 +1684,7 @@
                 'news' => 'News',
                 'post' => 'Blogs',
                 'pages' => 'Pages',
-                'note' => 'Notes from Antonia',
+                'note' => 'Notes from Perm. Sec.',
             ),
             'allow_null' => 0,
             'other_choice' => 0,
@@ -1866,7 +1866,8 @@
             ),
             'admin_only' => 0,
             'post_type' => array(
-                0 => 'note-from-antonia',
+                0 => 'note-from-amy',
+                1 => 'note-from-antonia',
             ),
             'post_status' => '',
             'taxonomy' => array(
@@ -3069,7 +3070,7 @@
                 'news' => 'News',
                 'post' => 'Blogs',
                 'pages' => 'Pages',
-                'note' => 'Notes from Antonia',
+                'note' => 'Notes from Perm. Sec.',
             ),
             'allow_null' => 0,
             'other_choice' => 0,
@@ -3251,7 +3252,8 @@
             ),
             'admin_only' => 0,
             'post_type' => array(
-                0 => 'note-from-antonia',
+                0 => 'note-from-amy',
+                1 => 'note-from-antonia',
             ),
             'post_status' => '',
             'taxonomy' => array(
