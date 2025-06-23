ea4671e91fd13bc2fcc85059c0b18d17a31f9aeb
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-06-23 12:35:24 +0100

    Update protect-key-pages.php

--- public/app/themes/clarity/inc/admin/users/protect-key-pages.php
+++ public/app/themes/clarity/inc/admin/users/protect-key-pages.php
@@ -118,7 +118,8 @@ class ProtectKeyPages
         if ('delete_post' === $args[0]) {
             $allcaps['delete_others_pages'] = false;
             // Removing the manage_options capability is also required to completely drop the ability delete.
-            // This can be verified by switching to a non-admin user, and visiting the All Pages admin screen.
+            // This can be verified by switching to a non-admin user, visiting the All Pages admin screen,
+            // and hovering over a key page - the Bin link should not be visible.
             $allcaps['manage_options'] = false;
         }
 
