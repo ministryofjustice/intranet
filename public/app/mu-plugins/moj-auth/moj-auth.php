eae672022ae2b6fb7f6d50cdf33eedd10b67a37d
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-07-10 11:01:14 +0100

    Allow for auth to be disabled

--- public/app/mu-plugins/moj-auth/moj-auth.php
+++ public/app/mu-plugins/moj-auth/moj-auth.php
@@ -20,6 +20,10 @@ defined('ABSPATH') || exit;
 
 // If the plugin isn't enabled, return early.
 if (Config::get('MOJ_AUTH_ENABLED') === false) {
+    // Exit here to return a 200 response for the heartbeat endpoint.
+    $_SERVER['REQUEST_URI'] === '/auth/heartbeat' && exit;
+
+    // For all other requests, return here, because we don't want to run any of the code below.
     return;
 }
 
