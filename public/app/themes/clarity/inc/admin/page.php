48fd3f852b570e31eb76893b52efdca4ea4d4f3f
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-09-24 12:14:08 +0100

    Improve `clear_nginx_cache` for documents and parent pages.

--- public/app/themes/clarity/inc/admin/page.php
+++ public/app/themes/clarity/inc/admin/page.php
@@ -52,13 +52,18 @@ function handle_post_status_transition(
  * Send a purge cache request to all Nginx servers when a post is saved or updated.
  *
  * @param int $post_id The post ID
- *
  * @return void
  */
 function clear_nginx_cache(int $post_id): void
 {
-    // Check if the post is a revision or unpublished.
-    if (wp_is_post_revision($post_id) || get_post_status($post_id) !== 'publish') {
+    // Check if the post is a revision.
+    if (wp_is_post_revision($post_id)) {
+        $post_id = wp_is_post_revision($post_id);
+    }
+
+    if (get_post_status($post_id) === 'trash') {
+        // If the post is in the trash, we can't clear the cache because we can't determine a URL.
+        // But, this is fine because his function will have been called before the post was trashed.
         return;
     }
 
@@ -66,27 +71,64 @@ function clear_nginx_cache(int $post_id): void
     $post_url = get_permalink($post_id);
     $post_path = parse_url($post_url, PHP_URL_PATH);
 
-    // Get all Nginx hosts from the ClusterHelper.
-    $cluster_helper = new ClusterHelper();
-    $nginx_hosts = $cluster_helper->getNginxHosts('hosts');
+    // Get all parent pages, if a blog was published then unpublished, we need to update the parent pages.
+    // Do this based on URL. e.g. if /blog/post-name is the URL, we need to clear `/`, `blog` and `blog/post-name`.
+    $paths_to_purge = array_reduce(
+        explode('/', trim($post_path, '/')),
+        function ($acc, $item) {
+            // Append the current item to the last path in the accumulator.
+            $acc[] = $acc[count($acc) - 1] . $item . '/';
+            return $acc;
+        },
+        ['/'] // Start with the root path
+    );
 
-    // Loop through each Nginx host and send a purge request.
+    // If we are dealing with a document, these are served without a trailing slash at the end of the path.
+    // Append a path to the array, it's the last entry, but with the trailing slash removed.
+    if (get_post_type($post_id) === 'document') {
+        $last_path = rtrim(end($paths_to_purge), '/');
+        $paths_to_purge[] = $last_path; // Add the last path without trailing slash
+    }
+
+    // Get all Nginx hosts from the ClusterHelper.
+    $nginx_hosts = ClusterHelper::getNginxHosts('hosts');
+
+    // An array of urls to purge.
+    // Will be populated with the full URLs to purge.
+    // e.g. locally with a single host: 
+    // ['http://nginx:8080/purge-cache/', 'http://nginx:8080/purge-cache/blog/', 'http://nginx:8080/purge-cache/blog/post-name/']
+    // e.g. on production with multiple hosts:
+    // [
+    //   'http://172.20.177.215:8080/purge-cache/', 'http://172.20.177.215:8080/purge-cache/blog/', 'http://172.20.177.215:8080/purge-cache/blog/post-name',
+    //   'http://172.20.176.123:8080/purge-cache/', 'http://172.20.176.123:8080/purge-cache/blog/', 'http://172.20.176.123:8080/purge-cache/blog/post-name',
+    //   ... (for each host)
+    // ]
+    $purge_urls = [];
+
+    // 1️⃣ Loop through each Nginx host to purge.
     foreach ($nginx_hosts as $host) {
-        // Construct the full URL for the purge request.
-        $nginx_cache_path = $host . '/purge-cache' . $post_path;
+        // 2️⃣ Loop through each path to purge.
+        foreach ($paths_to_purge as $path) {
+            $purge_urls[] = $host . '/purge-cache' . $path;
+        }
+    }
+
+
+    foreach ($purge_urls as $purge_url) :
 
         // Purge the cache.
-        $result = wp_remote_get($nginx_cache_path, [
+        $result = wp_remote_get($purge_url, [
             'blocking' => false,
             'headers' => ['Host' => parse_url(home_url(), PHP_URL_HOST)]
         ]);
 
         // Check for errors in the response.
         if (is_wp_error($result)) {
-            error_log(sprintf('Error purging cache at %s: %s', $nginx_cache_path, $result->get_error_message()));
+            error_log(sprintf('Error purging cache at %s: %s', $purge_url, $result->get_error_message()));
             continue;
         }
 
-        error_log(sprintf('Cache cleared at %s', $nginx_cache_path));
-    }
+        error_log(sprintf('Cache cleared at %s', $purge_url));
+
+    endforeach;
 }
