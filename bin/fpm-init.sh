be7bb68fb803108cacdbec8dfe6e99b91141f7b2
earthlingdavey <15802017+earthlingdavey@users.noreply.github.com>
2025-10-23 11:21:22 +0100

    CDPTKAN-287 Minimise use of scheduled events (#896)
    * Make blogroll schedule infrequent.
    
    * Disable wp_version_check, wp_update_plugins and wp_update_themes events.
    
    * Block total poll domains.
    
    * Delete various scheduled events on startup.
    
    * Block certain scheduled events.

--- bin/fpm-init.sh
+++ bin/fpm-init.sh
@@ -5,6 +5,15 @@ if wp core is-installed 2>/dev/null; then
 
     # Sync user roles.
     wp sync-user-roles sync
+
+    # Remove scheduled events for: wp_version_check, wp_update_plugins and wp_update_themes
+    wp cron event delete wp_version_check
+    wp cron event delete wp_update_plugins
+    wp cron event delete wp_update_themes
+
+    # Remove scheduled events for TotalPoll plugin that are not needed.
+    wp cron event delete totalpoll_weekly_environment
+    wp cron event delete totalpoll_daily_activity
 else
     # Fallback if WP is not installed.
     # This will happen during a first run on localhost.
---
+++ public/app/mu-plugins/moj-utils.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is a place for hooks that must be loaded very early.
+ */
+
+defined('ABSPATH') || exit;
+
+
+/**
+ * Prevent scheduling of unwanted TotalPoll cron hooks.
+ * 
+ * @see https://developer.wordpress.org/reference/hooks/pre_schedule_event/
+ */
+add_filter('pre_schedule_event', function ($pre, $event) {
+    // Block attempts to schedule unwanted TotalPoll cron hooks.
+    if (isset($event->hook) && in_array($event->hook, ['totalpoll_weekly_environment', 'totalpoll_daily_activity'], true)) {
+        return new \WP_Error('blocked_event', 'Scheduling ' . $event->hook . ' is disabled.');
+    }
+
+    return $pre;
+}, 10, 2);
