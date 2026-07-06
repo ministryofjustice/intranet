1bb53869f667f5b75696d489c8f4a7dc4cab9304
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2026-07-06 16:24:45 +0100

    Remove selectivizr (CSS3 selectors for legacy IE)

--- public/app/themes/clarity/inc/enqueue.php
+++ public/app/themes/clarity/inc/enqueue.php
@@ -25,7 +25,6 @@ function enqueue_clarity_scripts(): void
     wp_enqueue_script('jquery');
 
     wp_enqueue_script('popup', mix_asset('/js/magnific-popup.js'), array('jquery'), null, true);
-    wp_enqueue_script('selectivizr', mix_asset('/js/selectivizr-min.js'), null, true);
 }
 
 /**
