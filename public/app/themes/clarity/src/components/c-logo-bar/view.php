26c2ac797f6168bcec8083dd176361fea96b0d07
earthlingdavey <15802017+earthlingdavey@users.noreply.github.com>
2026-09-01 09:31:57 +0100

    Wales Office - add custom logo bar label (Welsh translation) (#1137)

--- public/app/themes/clarity/src/components/c-logo-bar/view.php
+++ public/app/themes/clarity/src/components/c-logo-bar/view.php
@@ -30,7 +30,7 @@ if (!empty($header_logo)) {
             <img class="c-logo-bar__logo" aria-hidden="true" src="<?= esc_url($logo) ?>" alt="" />
             <!--  We hide the full header if the user hasn't selected an agency  -->
             <!--  Default to 'Ministry of Justice' in the logo bar in this case  -->
-            <span class="agency-title l-half-section"><?= $simpleHeader ? 'Ministry of Justice' : $activeAgency['label'] ?></span>
+            <span class="agency-title l-half-section"><?= $simpleHeader ? 'Ministry of Justice' : ($activeAgency['logo_bar_label'] ?? $activeAgency['label']) ?></span>
             </a>
         </div>
 
